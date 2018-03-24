13d6c1bd5f5a987aa7f80675ef6d4dd459560e2b
mergwyn <gary@theclarkhome.com>
2018-03-24 15:20:24 +0000

    remove old code

--- site/profile/manifests/ddclient.pp
+++ site/profile/manifests/ddclient.pp
@@ -1,24 +1,25 @@
 #
 
 class profile::ddclient {
-  package { 'ddclient': ensure => installed }
+  include ddclient
+  # package { 'ddclient': ensure => installed }
 
-  file { '/etc/ddclient.conf':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0600',
-    require => Package['ddclient'],
-    notify  => Service['ddclient'],
-    source  => 'puppet:///modules/profile/ddclient/ddclient.conf',
-  }
+  #file { '/etc/ddclient.conf':
+    #ensure  => file,
+    #owner   => root,
+    #group   => root,
+    #mode    => '0600',
+    #require => Package['ddclient'],
+    #notify  => Service['ddclient'],
+    #source  => 'puppet:///modules/profile/ddclient/ddclient.conf',
+  #}
 
-  service { 'ddclient':
-    ensure    => running,
-    subscribe => [
-      Package['ddclient'],
-      File['/etc/ddclient.conf']
-    ],
-  }
+  #service { 'ddclient':
+    #ensure    => running,
+    #subscribe => [
+      #Package['ddclient'],
+      #File['/etc/ddclient.conf']
+    #],
+  #}
 }
 # vim: sw=2:ai:nu expandtab
