0e08f6cf152dcf4fb623c86b84688849ea8b6148
mergwyn <gary@theclarkhome.com>
2018-04-26 13:46:13 +0100

    get lxd snap from repo and fix lint

--- site/profile/manifests/lxd_host.pp
+++ site/profile/manifests/lxd_host.pp
@@ -8,18 +8,30 @@ class profile::lxd_host {
     require => Package['lxd'],
   }
 
+  # lxd snap related commands
+  vcsrepo { '/opt/code/lxdsnap':
+      ensure   => latest,
+      provider => git,
+      require  => [ Package['git'] ],
+      source   => 'https://github.com/nsyntych/lxdsnap.git',
+      revision => 'master',
+  }
+  #TODO run make
+
+  # now lxdsnap crontab related jobs
   file { '/usr/local/bin/lxdsnap_cron':
     ensure  => present,
     content => '
 #!/bin/sh
-/usr/src/lxdsnap/venv/bin/python /usr/src/lxdsnap/snap.py 2>&1 | logger -t lxdsnap
+/usr/src/lxdsnap/venv/bin/python /usr/src/lxdsnap/snap.py 2>&1 | 
+  logger -t lxdsnap
 status=$?
 if [ $? -ne 0 ]
 then
 	logger -t lxdsnap -s "lxdsnap returned $status"
 fi
 ',
-    mode    => 0555,
+    mode    => '0555',
   } ->
   cron { 'lxdsnap':
     command => '/usr/local/bin/lxdsnap_cron',
@@ -30,7 +42,7 @@ fi
 
   file { 'usr/local/bin/upgrade_lxd':
     ensure => present,
-    mode   => 0555,
+    mode   => '0555',
   }
 }
 # vim: sw=2:ai:nu expandtab
