9e157f56a52b47ca59834e48c0b86efc602de5f8
mergwyn <gary@theclarkhome.com>
2018-04-27 16:14:51 +0100

    remove letsencrypt for now to get nginx working

--- site/profile/manifests/reverse_proxy.pp
+++ site/profile/manifests/reverse_proxy.pp
@@ -3,17 +3,19 @@
 class profile::reverse_proxy {
 
   # sort out certificates first
-  class { '::letsencrypt':
-    configure_epel => true,
-    email          => hiera('defaults::adminemail'),
-  }
+  #class { '::letsencrypt':
+  #  config => {
+  #    email  => hiera('defaults::adminemail'),
+  #    server => 'https://acme-staging.api.letsencrypt.org/directory'
+  #  }
+  #}
 
-  $::dir='/usr/share/nginx/html'
-  $::domain = $::facts['domain']
-  letsencrypt::certonly { 'home':
-    domains       => [ $::facts['fqdn'] ],
-    plugin        => 'webroot',
-    webroot_paths => ["${::dir}/${::hostname}"],
+  #$dir='/usr/share/nginx/html'
+  $domain = $::facts['domain']
+  #letsencrypt::certonly { 'home':
+  #  domains       => [ $::facts['fqdn'] ],
+  #  plugin        => 'webroot',
+  #  webroot_paths => ["${dir}/${::hostname}"],
     #domains => [
     #  $facts['fqdn'],
     #  "echo.$domain",
@@ -28,44 +30,40 @@ class profile::reverse_proxy {
     #  "$dir/tango",
     #  $dir/vpn"
     #],
-    manage_cron   => true,
+    #manage_cron   => true,
     #cron_before_command => 'service nginx stop',
     #cron_success_command => '/bin/systemctl reload nginx.service',
     #suppress_cron_output => true,
-  }
+  #}
 
   # now for the reverse proxy
   class { 'nginx': }
 
   # now for servers - these can be moved to hiera
-  nginx::resource::server { "foxtrot.${::domain}":
+  nginx::resource::server { "foxtrot.${domain}":
     listen_port => 80,
-    proxy       => "http://foxtrot.${::domain}:80",
+    proxy       => "http://foxtrot.${domain}:80",
   }
-  nginx::resource::server { "${::hostname}.${::domain}":
+  nginx::resource::server { "webmin.${domain}":
     listen_port => 80,
-    proxy       => "http://${::hostname}.${::domain}:80",
+    proxy       => "http://${::hostname}.${domain}:10000",
   }
-  nginx::resource::server { "webmin.${::domain}":
+  nginx::resource::server { "zulu.${domain}":
     listen_port => 80,
-    proxy       => "http://${::hostname}.${::domain}:10000",
+    proxy       => "http://zulu.${domain}:80",
   }
-  nginx::resource::server { "zulu.${::domain}":
-    listen_port => 80,
-    proxy       => "http://zulu.${::domain}:80",
-  }
-  nginx::resource::server { "tango.${::domain}":
+  nginx::resource::server { "tango.${domain}":
     listen_port => 5050,
-    proxy       => "http://tango.${::domain}:5050",
+    proxy       => "http://tango.${domain}:5050",
   }
-  nginx::resource::server { "tango.${::domain}":
-    listen_port => 8080,
-    proxy       => "http://tango.${::domain}:8080",
-  }
-  nginx::resource::server { "tango.${::domain}":
-    listen_port => 8080,
-    proxy       => "http://tango.${::domain}:8080",
-  }
+  #nginx::resource::server { "tango.${domain}":
+  #  listen_port => 8080,
+  #  proxy       => "http://tango.${domain}:8080",
+  #}
+  #nginx::resource::server { "tango.${domain}":
+  #  listen_port => 8080,
+  #  proxy       => "http://tango.${domain}:8080",
+  #}
 
   
   # Finally tidy up pound
