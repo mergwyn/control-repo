5fa6a5b6df42e94f513b0b5dc9bf2f78c484cc42
mergwyn <gary@theclarkhome.com>
2018-04-27 12:27:33 +0100

    Add some more proxy hosts

--- site/profile/manifests/reverse_proxy.pp
+++ site/profile/manifests/reverse_proxy.pp
@@ -3,18 +3,32 @@
 class profile::reverse_proxy {
 
   # sort out certificates first
-  class { ::letsencrypt:
+  class { '::letsencrypt':
     configure_epel => true,
     email          => hiera('defaults::adminemail'),
   }
 
-  $domain = $facts['domain']
+  $::dir='/usr/share/nginx/html'
+  $::domain = $::facts['domain']
   letsencrypt::certonly { 'home':
-    domains => [ $facts['fqdn'] ],
-    plugin  => 'webroot',
-    webroot_paths => ["/usr/share/nginx/html/$hostname"],
-    #domains => [ $facts['fqdn'], "echo.$domain", "foxtrot.$domain", "tango.$domain",  "vpn.$domain" ],
-    manage_cron => true,
+    domains       => [ $::facts['fqdn'] ],
+    plugin        => 'webroot',
+    webroot_paths => ["${::dir}/${::hostname}"],
+    #domains => [
+    #  $facts['fqdn'],
+    #  "echo.$domain",
+    #  "foxtrot.$domain",
+    #  "tango.$domain",
+    #  "vpn.$domain"
+    #],
+    #webroot_paths => [
+    #  "$dir/$hostname",
+    #  "$dir/echo",
+    #  "$dir/foxtrot",
+    #  "$dir/tango",
+    #  $dir/vpn"
+    #],
+    manage_cron   => true,
     #cron_before_command => 'service nginx stop',
     #cron_success_command => '/bin/systemctl reload nginx.service',
     #suppress_cron_output => true,
@@ -24,20 +38,42 @@ class profile::reverse_proxy {
   class { 'nginx': }
 
   # now for servers - these can be moved to hiera
-  nginx::resource::server { 'foxtrot.theclarkhome.com':
+  nginx::resource::server { "foxtrot.${::domain}":
     listen_port => 80,
-    proxy       => 'http://foxtrot.theclarkhome.com:80',
-    #access_log  => '/var/log/nginx/foxtrot_access.log',
-    #error_log   => '/var/log/nginx/foxtrot_error.log',
+    proxy       => "http://foxtrot.${::domain}:80",
+  }
+  nginx::resource::server { "${::hostname}.${::domain}":
+    listen_port => 80,
+    proxy       => "http://${::hostname}.${::domain}:80",
+  }
+  nginx::resource::server { "webmin.${::domain}":
+    listen_port => 80,
+    proxy       => "http://${::hostname}.${::domain}:10000",
+  }
+  nginx::resource::server { "zulu.${::domain}":
+    listen_port => 80,
+    proxy       => "http://zulu.${::domain}:80",
+  }
+  nginx::resource::server { "tango.${::domain}":
+    listen_port => 5050,
+    proxy       => "http://tango.${::domain}:5050",
+  }
+  nginx::resource::server { "tango.${::domain}":
+    listen_port => 8080,
+    proxy       => "http://tango.${::domain}:8080",
+  }
+  nginx::resource::server { "tango.${::domain}":
+    listen_port => 8080,
+    proxy       => "http://tango.${::domain}:8080",
   }
 
   
   # Finally tidy up pound
-  Package { "pound":             ensure => absent }
+  Package { 'pound':             ensure => absent }
   file { '/etc/pound/pound.cfg': ensure => absent, }
   file { '/etc/pound':           ensure => absent, }
   file { '/etc/default/pound':   ensure  => absent, }
-	
+  
 }
 #
 # vim: sw=2:ai:nu expandtab
