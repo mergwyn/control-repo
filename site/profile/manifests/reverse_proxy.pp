bddac69303f198a2db84416d26f4a17a3a3c8b24
mergwyn <gary@theclarkhome.com>
2018-04-27 11:02:23 +0100

    Remove letsencrypt

--- site/profile/manifests/reverse_proxy.pp
+++ site/profile/manifests/reverse_proxy.pp
@@ -2,22 +2,6 @@
 
 class profile::reverse_proxy {
 
-  # sort out certificates first
-  class { ::letsencrypt:
-    configure_epel => true,
-    email          => hiera('defaults::adminemail'),
-  }
-
-  $domain = $facts['domain']
-  letsencrypt::certonly { 'home':
-    domains => [ $facts['fqdn'] ],
-    #domains => [ $facts['fqdn'], "echo.$domain", "foxtrot.$domain", "tango.$domain",  "vpn.$domain" ],
-    manage_cron => true,
-    #cron_before_command => 'service nginx stop',
-    #cron_success_command => '/bin/systemctl reload nginx.service',
-    #suppress_cron_output => true,
-  }
-
   # now for the reverse proxy
   package { "pound": ensure => installed }
 
