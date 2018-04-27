75798de7cd0d2d2d10b505ac2f5836f38de3d508
mergwyn <gary@theclarkhome.com>
2018-04-27 17:44:28 +0100

    Initial reverse proxy config complete

--- site/profile/manifests/reverse_proxy.pp
+++ site/profile/manifests/reverse_proxy.pp
@@ -40,6 +40,10 @@ class profile::reverse_proxy {
   class { 'nginx': }
 
   # now for servers - these can be moved to hiera
+  nginx::resource::server { "${::hostname}.${domain}":
+    listen_port => 80,
+    proxy       => "http://${::hostname}.${domain}:80",
+  }
   nginx::resource::server { "foxtrot.${domain}":
     listen_port => 80,
     proxy       => "http://foxtrot.${domain}:80",
@@ -52,19 +56,56 @@ class profile::reverse_proxy {
     listen_port => 80,
     proxy       => "http://zulu.${domain}:80",
   }
-  nginx::resource::server { "tango.${domain}":
+  # tango related
+  nginx::resource::server { "couchpototo.tango.${domain}":
     listen_port => 5050,
+    server_name => [ "tango.${domain}" ],
     proxy       => "http://tango.${domain}:5050",
   }
-  #nginx::resource::server { "tango.${domain}":
-  #  listen_port => 8080,
-  #  proxy       => "http://tango.${domain}:8080",
-  #}
-  #nginx::resource::server { "tango.${domain}":
-  #  listen_port => 8080,
-  #  proxy       => "http://tango.${domain}:8080",
-  #}
-
+  nginx::resource::server { "sabnzdb.tango.${domain}":
+    listen_port => 8080,
+    server_name => [ "tango.${domain}" ],
+    proxy       => "http://tango.${domain}:8080",
+  }
+  nginx::resource::server { "sonarr.tango.${domain}":
+    listen_port => 8989,
+    server_name => [ "tango.${domain}" ],
+    proxy       => "http://tango.${domain}:8989",
+  }
+  nginx::resource::server { "transmission.tango.${domain}":
+    listen_port => 9091,
+    server_name => [ "tango.${domain}" ],
+    proxy       => "http://tango.${domain}:9091",
+  }
+  #
+  nginx::resource::upstream { 'plex_upstream':
+    members => [ "tango.${domain}:32400", ],
+  }
+  nginx::resource::server { "plex.tango.${domain}":
+    listen_port => 32400,
+    server_name => [ "tango.${domain}" ],
+    access_log  => 'off',
+    locations   => {
+      '/web' => {
+        location            => '/web',
+        proxy               => "http://tango.${domain}:32400",
+        proxy_buffering     => 'off',
+        proxy_redirect      => 'off',
+        proxy_http_version  => '1.1',
+        proxy_set_header    => [
+          'X-Forwarded-For $proxy_add_x_forwarded_for',
+          'Upgrade $http_upgrade',
+          'Connection $http_connection',
+          'X-Real-IP $remote_addr',
+          'Host $http_host',
+        ],
+        location_cfg_append => {
+    #      'if ($http_x_plex_device_name = "")' => '{rewrite ^/$ https://$http_host/web/index.html}',
+          'proxy_cookie_path' => '/web/ /',
+        },
+      },
+    },
+  }
   
   # Finally tidy up pound
   #Package { 'pound':             ensure => absent }
