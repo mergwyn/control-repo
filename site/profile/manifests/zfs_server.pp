b36c417023b3f470417d0e8b584daa753942ec1b
mergwyn <gary@theclarkhome.com>
2018-05-09 19:40:15 +0100

    add support for zfs grub files

--- site/profile/manifests/zfs_server.pp
+++ site/profile/manifests/zfs_server.pp
@@ -3,29 +3,47 @@
 
 class profile::zfs_server {
 
-  file { "/usr/local/bin/zfs_report.sh":
+  # monthly report
+  file { '/usr/local/bin/zfs_report.sh':
+    ensure => present,
+    source => 'puppet:///modules/profile/zfs/zfs_report.sh',
+    mode   => '0775',
+  }
+  file { '/etc/cron.monthly/zfs-montly-report.sh': ensure  => absent, }
+  file { '/etc/cron.monthly/zfs-montly-report':
     ensure  => present,
-    source  => "puppet:///modules/profile/zfs/zfs_report.sh",
+    content => "#!/bin/sh\n/usr/local/bin/zfs_report.sh\n",
     mode    => '0775',
   }
-  file { "/etc/cron.monthly/zfs-montly-report.sh":
-    ensure  => present,
-    content  => "#!/bin/sh\n/usr/local/bin/zfs_report.sh\n",
-    mode    => '0775',
-  }
-  #file { "/etc/zabbix/zabbix_agentd.d/zfs-auto.conf.conf": ensure => absent }
-  #file { "/etc/zabbix/zabbix_agentd.d/zfs-health.conf.conf": ensure => absent }
 
+  # grub menu generation
+  file { '/etc/grub.d/42_zfs_select':
+    ensure => present,
+    source => 'puppet:///modules/profile/zfs/42_zfs_select',
+    mode   => '0755',
+  }
+  file { '/etc/grub.d/43_zfs_snap':
+    ensure => present,
+    source => 'puppet:///modules/profile/zfs/43_zfs_snap',
+    mode   => '0755',
+  }
+  file { '/etc/cron.daily/grubupdate':
+    ensure  => present,
+    content => "#!/bin/bash\n\n update-grub 2>&1 | logger -t update-grub -i\n"
+    mode    => '0555',
+  }
+
+  # zabbix support
   zabbix::userparameters { 'zfs-auto':
-    source => "puppet:///modules/profile/zfs/zfs-auto.conf",
+    source => 'puppet:///modules/profile/zfs/zfs-auto.conf',
   }
   zabbix::userparameters { 'zfs-health':
-    content => 'UserParameter=zpool.health[*],sudo zpool list -H -o health $1',
+    content => "UserParameter=zpool.health[*],sudo zpool list -H -o health \${1}\n",
   }
 
-  file { "/etc/sudoers.d/zabbix-zfs":
+  file { '/etc/sudoers.d/zabbix-zfs':
     ensure  => present,
-    content  => "
+    content => "
 Defaults:zabbix	!requiretty
 zabbix	ALL=(root)	NOPASSWD:	/sbin/zpool
 zabbix	ALL=(root)	NOPASSWD:	/sbin/zfs
