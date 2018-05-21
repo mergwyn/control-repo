c2bc1734a7d0b30606db0a88cf518ee1e464007b
mergwyn <gary@theclarkhome.com>
2018-05-21 15:36:25 +0100

    First pass at supporting ssh keys for backupps

--- site/profile/manifests/backuppc_client.pp
+++ site/profile/manifests/backuppc_client.pp
@@ -25,5 +25,33 @@ class profile::backuppc_client {
     ensure => absent,
   }
   package { 'rsync': ensure => installed }
+
+  # backuppc ssh keys
+  $system_account        = hiera("defaults::system_user")
+  $system_home_directory = hiera("defaults::system_home_dir")
+  $backuppc_hostname     = hiera("defaults::backuppc_server")
+
+  file { "${system_home_directory}/.ssh":
+    ensure => 'directory',
+    mode   => '0700',
+    owner  => $system_account,
+    group  => $system_account,
+  }
+  Ssh_authorized_key <<| tag == "backuppc_${backuppc_hostname}" |>> {
+    ensure  => present,
+    user    => $system_account,
+    require => File["${system_home_directory}/.ssh"]
+  }
+
+#  if $facts['networking']['fqdn'] != $backuppc_hostname {
+#    @@sshkey { $facts['networking']['fqdn']:
+#      ensure => $ensure,
+#      type   => 'ssh-rsa',
+#      key    => $facts['ssh']['rsa']['key'],
+#      tag    => "backuppc_sshkeys_${backuppc_hostname}",
+#    }
+#  }
 }
+
+
 # vim: sw=2:ai:nu expandtab
