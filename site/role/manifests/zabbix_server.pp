4e5b49761b820255365aa1c17da50fd38dd876cc
mergwyn <gary@theclarkhome.com>
2018-05-24 10:51:09 +0100

    correct config by adding domain_sso and samba

--- site/role/manifests/zabbix_server.pp
+++ site/role/manifests/zabbix_server.pp
@@ -2,5 +2,7 @@
 
 class role::zabbix_server {
   include profile::base  # All roles should have the base profile
+  include profile::samba_member
+  include profile::domain_sso
   include profile::zabbix_server
 }
