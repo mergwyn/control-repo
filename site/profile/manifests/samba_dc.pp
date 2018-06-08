f1c7f8915c1549fb3882b2fa12b2f0b808505919
mergwyn <gary@theclarkhome.com>
2018-06-08 16:55:45 +0100

    correct location of clean script

--- site/profile/manifests/samba_dc.pp
+++ site/profile/manifests/samba_dc.pp
@@ -22,7 +22,7 @@ class profile::samba_dc {
     source => 'puppet:///modules/profile/backuppc/S30samba_backup',
     mode   => '0555',
   }
-  file { "${preuser}/P30samba_clean":
+  file { "${posuser}/P30samba_clean":
     ensure => present,
     source => 'puppet:///modules/profile/backuppc/P30samba_clean',
     mode   => '0555',
