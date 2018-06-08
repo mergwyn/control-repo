e8a4170a74a13d5eb2dfdcebdccd479f58f625de
mergwyn <gary@theclarkhome.com>
2018-06-08 16:41:29 +0100

    Run samba backup as part of backuppc run

--- site/profile/manifests/samba_dc.pp
+++ site/profile/manifests/samba_dc.pp
@@ -3,11 +3,32 @@
 
 class profile::samba_dc {
 
-    package { 'unison': }
-    file { '/etc/cron.daily/samba4-backup':
-      content => "#!/bin/sh\n/var/lib/samba/sysvol/theclarkhome.com/scripts/samba4_backup\n",
-      mode    => '0755',
-    }
+  package { 'unison': }
+  file { '/etc/cron.daily/samba4-backup':
+    content => "#!/bin/sh\n/var/lib/samba/sysvol/theclarkhome.com/scripts/samba4_backup\n",
+    mode    => '0755',
+  }
+
+  # support for backup as part of backuppc run
+
+  $scripts='/etc/backuppc/scripts/'
+  $preuser="${scripts}DumpPreUser/"
+  $postuser="${scripts}DumpPostUser/"
+  file {[$preuser, $postuser]:
+    ensure  => directory,
+    recurse => true,
+  }
+
+  file { "${preuser}/S30samba_backup":
+    ensure => present,
+    source => 'puppet:///modules/profile/backuppc/S30samba_backup',
+    mode   => '0555',
+  }
+  file { "${postuser}/P30samba_clean":
+    ensure => present,
+    source => 'puppet:///modules/profile/backuppc/P30samba_clean',
+    mode   => '0555',
+  }
     #class { '::samba::dc':
     #}
     #class { '::samba::dc':
