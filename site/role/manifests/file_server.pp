baee37f387d2bf425a223832ae99b9aa3b71351a
mergwyn <gary@theclarkhome.com>
2018-07-05 17:11:40 +0100

    remove samba member

--- site/role/manifests/file_server.pp
+++ site/role/manifests/file_server.pp
@@ -2,7 +2,7 @@
 
 class role::file_server {
   include profile::base  # All roles should have the base profile
-  include profile::samba_member
+  #include profile::samba_member
   include profile::domain_sso
   include profile::nfs_server
   include profile::web_server
