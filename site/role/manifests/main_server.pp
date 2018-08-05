49737200ed559ec5abf83ad188024999ab6984fb
mergwyn <gary@theclarkhome.com>
2018-08-05 18:19:45 +0100

    add samba config

--- site/role/manifests/main_server.pp
+++ site/role/manifests/main_server.pp
@@ -3,7 +3,7 @@
 class role::main_server {
   include profile::base  # All roles should have the base profile
   include profile::samba_member
-  #include profile::domain_sso
+  include profile::domain_sso
   include profile::nfs_server
   include profile::web::webdav
   include profile::web::munki
