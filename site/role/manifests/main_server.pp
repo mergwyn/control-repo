9dc950122f5ca59bdb578b694a0198112d048b2e
mergwyn <gary@theclarkhome.com>
2018-08-05 09:58:00 +0100

    refactor and add photos

---
+++ site/role/manifests/main_server.pp
@@ -0,0 +1,11 @@
+#
+
+class role::main_server {
+  include profile::base  # All roles should have the base profile
+  #include profile::samba_member
+  include profile::domain_sso
+  include profile::nfs_server
+  include profile::web_server
+  include profile::photos
+}
+# vim: sw=2:ai:nu expandtab
