4566c0d735cc11eea078f7ae0d9bce1f7155fd5b
mergwyn <gary@theclarkhome.com>
2018-08-06 12:52:42 +0100

    checkin

---
+++ site/profile/lib/facter/jackett_ver.rb
@@ -0,0 +1,19 @@
+require 'facter'
+
+# Default for non-Linux nodes
+#
+Facter.add(:jacket_ver) do
+    setcode do
+        nil
+    end
+end
+
+# Linux
+#
+Facter.add(:jacket_ver) do
+    confine :kernel  => :linux
+    setcode do
+        Facter::Util::Resolution.exec("wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}'")
+    end
+end
+
