1004ee898acfcce88cc6330786302ebd6c87e0c9
mergwyn <gary@theclarkhome.com>
2018-08-12 14:08:32 +0100

    Add mailman support

---
+++ site/profile/manifests/mailman_rss.pp
@@ -0,0 +1,49 @@
+#
+# TODO: sudo configuration
+
+class profile::mailman_rss (
+    $codedir = '/opt/code',
+    $bindir  = '/usr/local/bin',
+    $htmldi  = '/usr/share/nginx/html',
+  ) {
+
+  include profile::git
+
+  file { [ "${htmldir}/rss" ]:
+    ensure => directory,
+  }
+
+  $target="${codedir}/mailman-archive-scraper"
+
+  # install commands
+  vcsrepo { $target:
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/philgyford/mailman-archive-scraper',
+    revision => 'master',
+    require  => Package['git'],
+    notify   => Exec['install_requirements', 'chmod']
+  }
+  exec { 'install_requirements':
+    path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/usr/local/bin'],
+    command     => 'pip install -r requirements.txt',
+    refreshonly => true
+  }
+  exec { 'chmod':
+    path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/usr/local/bin'],
+    command     => "chmod a+x ${target}/MailmanArchiveScraper.py",
+    refreshonly => true
+  }
+
+  # set up cron
+  file { '/etc/cron.hourly/mailman-rss':
+    ensure  => present,
+    mode    => '0555',
+    content => "#!/bin/sh\nexec /usr/bin/python ${target}/MailmanArchiveScraper.py ${target}/zfs*.cfg\n",
+  }
+
+  file { "${target}/zfs-discuss.cfg":
+    source => 'puppet:///modules/profile/zfs/zfs-discuss.cfg',
+  }
+}
+# vim: sw=2:ai:nu expandtab
