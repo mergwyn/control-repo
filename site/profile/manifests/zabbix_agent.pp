9d90d1e75482e3e640686fd553f33303b224cc0c
mergwyn <gary@theclarkhome.com>
2018-09-03 19:46:18 +0100

    Convert scripts to userparameters

--- site/profile/manifests/zabbix_agent.pp
+++ site/profile/manifests/zabbix_agent.pp
@@ -13,37 +13,33 @@ class profile::zabbix_agent {
     mode    => '0440',
   }
 
-  file { "$dir/discovery_disks.perl":
-    ensure  => present,
-    source  => "puppet:///modules/profile/zabbix_agent/discovery_disks.perl",
-    mode    => '0555',
+  zabbix::userparameters { "discovery_disks.perl":
+    script      => "puppet:///modules/profile/zabbix_agent/discovery_disks.perl",
+    script_dir  => ${dir},
   }
   zabbix::userparameters { 'disk_autodiscovery':
     source => 'puppet:///modules/profile/zabbix_agent/disk_autodiscovery.conf',
   }
 
-  file { "$dir/discovery_processes.sh":
-    ensure  => present,
-    source  => "puppet:///modules/profile/zabbix_agent/discovery_processes.sh",
-    mode    => '0555',
+  zabbix::userparameters { "discovery_processes.sh":
+    script      => "puppet:///modules/profile/zabbix_agent/discovery_processes.sh",
+    script_dir  => ${dir},
   }
   zabbix::userparameters { 'process_autodiscovery':
     content => "UserParameter=custom.proc.discovery_perl,/etc/zabbix/scripts/discovery_processes.sh\n"
   }
 
-  file { "$dir/discovery_tcp_services.perl":
-    ensure  => present,
-    source  => "puppet:///modules/profile/zabbix_agent/discovery_tcp_services.perl",
-    mode    => '0555',
+  zabbix::userparameters { "discovery_tcp_services.perl":
+    script      => "puppet:///modules/profile/zabbix_agent/discovery_tcp_services.perl",
+    script_dir  => ${dir},
   }
   zabbix::userparameters { 'service_autodiscovery_tcp':
     content => "UserParameter=custom.services.tcp.discovery_perl,/etc/zabbix/scripts/discovery_tcp_services.perl\n",
   }
 
-  file { "$dir/discovery_udp_services.perl":
-    ensure  => present,
-    source  => "puppet:///modules/profile/zabbix_agent/discovery_udp_services.perl",
-    mode    => '0555',
+  zabbix::userparameters { "discovery_udp_services.perl":
+    script      => "puppet:///modules/profile/zabbix_agent/discovery_udp_services.perl",
+    script_dir  => ${dir},
   }
   zabbix::userparameters { 'service_autodiscovery_udp':
     content => "UserParameter=custom.services.udp.discovery_perl,/etc/zabbix/scripts/discovery_udp_services.perl\n",
