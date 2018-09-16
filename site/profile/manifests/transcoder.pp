6af486f39774dff53437a86022162b6caf55d3fa
mergwyn <gary@theclarkhome.com>
2018-09-16 18:45:45 +0100

    Add humaxscan service

--- site/profile/manifests/transcoder.pp
+++ site/profile/manifests/transcoder.pp
@@ -22,5 +22,28 @@ class profile::transcoder {
     minute      => 5,
     hour        => '0-18',
   }
+
+
+  #setup systemd entry
+  systemd::unit_file { 'humaxscan.service':
+    enable  => true,
+    active  => true,
+    content => "[Unit]
+  Description=Scan for presence of Humax PVR and notify plex
+  After=multi-user.target
+
+  [Service]
+  Restart=on-failure
+  RestartSec=5
+  Type=simple
+  ExecStart=/${codedir}/bin/humaxscan
+  TimeoutStopSec=20
+  StartLimitInterval=60s
+  StartLimitBurst=3
+
+  [Install]
+  WantedBy=multi-user.target
+  "
+    }
 }
 # vim: sw=2:ai:nu expandtab
