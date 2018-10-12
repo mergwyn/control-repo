9f10974d464341ab85bb0891b48756d536a23ca6
mergwyn <gary@theclarkhome.com>
2018-10-12 18:20:19 +0100

    convert back to snap

--- site/profile/manifests/lxd_host.pp
+++ site/profile/manifests/lxd_host.pp
@@ -15,7 +15,7 @@ class profile::lxd_host {
     notify  => Exec['snap.lxd.daemon.service'],
   }
   Exec { 'snap.lxd.daemon.service':
-    command => '/usr/bin/snap run --command=reload lxd.daemon'
+    command => '/usr/bin/snap run --command=reload lxd.daemon',
     require => Package['lxd'],
   }
 
