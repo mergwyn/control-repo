dc85297e90eacb127acaba810470ae15f67d40c0
mergwyn <gary@theclarkhome.com>
2018-10-22 14:36:02 +0100

    correct mono repo

--- site/profile/manifests/mono.pp
+++ site/profile/manifests/mono.pp
@@ -3,9 +3,10 @@
 
 class profile::mono () {
 
+  $distro= $::facts['os']['distro']['codename']
   apt::source { 'mono':
     location => 'deb https://download.mono-project.com/repo/ubuntu',
-    release  => "stable-$::facts['os']['distro']['codename']",
+    release  => "stable-$distro",
     repos    => 'main',
     key      => {
       'id'     => '3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF',
