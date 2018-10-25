0f9ca257303ec2443ae9e5dc3424412b0ee4f82b
mergwyn <gary@theclarkhome.com>
2018-10-25 13:08:17 +0100

    add nfs-common and refact transcoder into media subdir

---
+++ site/profile/manifests/media/downloader.pp
@@ -0,0 +1,10 @@
+#
+
+class profile::media::downloader () {
+
+  #TODO insert dependency on mono into module
+  include profile::mono
+  include ::jackett
+  include ::radarr
+}
+# vim: sw=2:ai:nu expandtab
