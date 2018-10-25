60e26b62f725359d1be64f62838c143c76735c45
mergwyn <gary@theclarkhome.com>
2018-10-25 17:24:14 +0100

    Disable tvheadend whilst there are repository issues

--- site/profile/manifests/tvheadend.pp
+++ site/profile/manifests/tvheadend.pp
@@ -1,12 +1,12 @@
 #
 
 class profile::tvheadend {
-  class{'::tvheadend':
+  #class{'::tvheadend':
     #release        => 'stable-4.2',
     #admin_password => 'L1nahswf.ve',
     #user           => 'media',
     #group          => '513',
-  } 
+  #} 
 }
 
 # vim: sw=2:ai:nu expandtab
