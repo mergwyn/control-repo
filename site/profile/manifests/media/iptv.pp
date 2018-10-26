0646355b5118b8f5732b6c904dc9646f812fb5f5
mergwyn <gary@theclarkhome.com>
2018-10-26 10:48:07 +0100

    refactor tvheadend in media

--- site/profile/manifests/media/iptv.pp
+++ site/profile/manifests/media/iptv.pp
@@ -1,12 +1,17 @@
 #
 
-class profile::tvheadend {
+class profile::media::iptv {
+  $packages = [ 'curl', 'socat' ]
+  package { $packages: ensure => present }
+
   #class{'::tvheadend':
     #release        => 'stable-4.2',
     #admin_password => 'L1nahswf.ve',
     #user           => 'media',
     #group          => '513',
   #} 
+  #TODO cron
+  #TODO tvhproxy
 }
 
 # vim: sw=2:ai:nu expandtab
