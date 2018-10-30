f6fff89e63d3138f52e461c041659caa28dcf20b
mergwyn <gary@theclarkhome.com>
2018-10-30 11:29:42 +0000

    Add sickbeard automator into transcoder

--- site/profile/manifests/media/sickbeard_automator.pp
+++ site/profile/manifests/media/sickbeard_automator.pp
@@ -3,7 +3,7 @@ class profile::media::sickbeard_automator {
 
   $codedir='/opt/'
   $target="${codedir}/sickbeard_mp4_automator"
-  $scriptdir = ${codedir}/scripts"
+  $scriptdir = "${codedir}/scripts"
   $packages = [ 'nfs-common' ]
 
   include profile::git
