6f10e538137e7c31612b91038bb796424d11a69d
mergwyn <gary@theclarkhome.com>
2018-11-03 09:50:14 +0000

    Remove tidy up of apt pinning

--- site/profile/manifests/puppet/agent.pp
+++ site/profile/manifests/puppet/agent.pp
@@ -9,10 +9,6 @@ class profile::puppet::agent {
     package_version => $serverversion,
   }
 
-  file { '/etc/apt/preferences.d/00-puppet.pref':
-    ensure  => absent,
-    #content => "# /etc/apt/preferences.d/00-puppet.pref\nPackage: puppet puppet-common\nPin: version 3.8*\nPin-Priority: 501\n"
-  }
   file { '/etc/profile.d/puppet.sh':
     ensure  => present,
     content => "# Expand the PATH to include puppet binaries\nPATH=\$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin\n"
