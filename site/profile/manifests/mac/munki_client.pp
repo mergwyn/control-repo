27b1ad48b8eeebf36e2b140118f779ced4f54ac9
mergwyn <gary@theclarkhome.com>
2018-11-07 16:49:59 +0000

    Update cert location

--- site/profile/manifests/mac/munki_client.pp
+++ site/profile/manifests/mac/munki_client.pp
@@ -14,7 +14,7 @@ class profile::mac::munki_client {
     mode    => '0640',
     owner   => root,
     group   => wheel,
-    source  => '/etc/puppet/ssl/certs/ca.pem',
+    source  => "file://${puppet_confdir}/ssl/ca.pem",
     require => File['/Library/Managed Installs/certs/'],
   }
  
@@ -22,7 +22,7 @@ class profile::mac::munki_client {
     mode    => '0640',
     owner   => root,
     group   => wheel,
-    source  => "/etc/puppet/ssl/certs/${clientcert}.pem",
+    source  => "file://${puppet_confdir}/ssl/${clientcert}.pem",
     require => File['/Library/Managed Installs/certs/'],
   }
  
@@ -30,7 +30,7 @@ class profile::mac::munki_client {
     mode    => '0640',
     owner   => root,
     group   => wheel,
-    source  => "/etc/puppet/ssl/private_keys/${clientcert}.pem",
+    source  => "file://${puppet_confdir}/ssl/private_keys/${clientcert}.pem",
     require => File['/Library/Managed Installs/certs/'],
   }
 }
