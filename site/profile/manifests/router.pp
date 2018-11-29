c15745da8e3e7e3542cc1e391eccbe456f31c280
mergwyn <gary@theclarkhome.com>
2018-11-29 11:45:07 +0000

    Add job for iptv routes

--- site/profile/manifests/router.pp
+++ site/profile/manifests/router.pp
@@ -2,11 +2,22 @@
 
 class profile::router {
 
+  include cron
+  include profile::scripts
+  $codedir='/opt/scripts'
+
   $aptpackages = [
     'ufw',
-    #'openvpn-client',
+    'openvpn-client',
   ]
   package { $aptpackages: ensure   => present, }
+
+  cron::job { 'iptv-routes':
+    command     => "test -x ${codedir}/iptv/getroutes && ${codedir}/iptv/getroutes",
+    minute      => 51, 
+    hour        => '*/4',
+    environment => [ 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' ],
+  }
 
 }
 #
