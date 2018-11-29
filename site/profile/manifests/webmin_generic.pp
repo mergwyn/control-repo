c0e79f75ae13255906f35be7e6bc1d987f5cf421
mergwyn <gary@theclarkhome.com>
2018-11-29 10:14:15 +0000

    remove dependency on package for ini settings

--- site/profile/manifests/webmin_generic.pp
+++ site/profile/manifests/webmin_generic.pp
@@ -31,7 +31,6 @@ class profile::webmin_generic {
   }
 
   $package_updates_settings = {
-    'require'             => Package['webmin'],
     '' => {
       'sched_email'     => "$adminemail",
       'update_multiple' => '1',
