4867ea38b70e48c238dbdb1668bcbec0793d0ebd
mergwyn <gary@theclarkhome.com>
2018-12-02 10:06:45 +0000

    fix typo

--- site/profile/manifests/unattended_upgrades.pp
+++ site/profile/manifests/unattended_upgrades.pp
@@ -11,7 +11,7 @@ class profile::unattended_upgrades {
       'Zabbix:${distro_codename}',                      #lint:ignore:single_quote_string_with_variables
       'puppet:${distro_codename}',                      #lint:ignore:single_quote_string_with_variables
     ],
-    email   => hiera('defaults::adminemail'), },
+    email   => hiera('defaults::adminemail'),
   }
   file { '/etc/apt/apt.conf.d/50unattended-upgrades.ucf-dist': ensure  => absent, }
 }
