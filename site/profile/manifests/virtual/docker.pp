39519e852836fcb753b4637d1402fdd1cf810750
mergwyn <gary@theclarkhome.com>
2018-12-14 19:00:06 +0000

    Initial version

---
+++ site/profile/manifests/virtual/docker.pp
@@ -0,0 +1,38 @@
+#
+
+class profile::docker {
+
+  #include ::snapd
+  #$snappackages = [ 'frr' ]
+  #package { $snappackages:
+  #  ensure   => present,
+  #  provider => snap,
+  #}
+  # Docker
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+  apt::source { 'docker':
+    comment  => "Docker Repository",
+    location => 'https://download.docker.com/linux/ubuntu',
+    release  => $release,
+    repos    => 'docker',
+    key      => {
+      'id'     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+      'server' => 'https://download.docker.com/linux/ubuntu/gpg',
+    },
+  }
+  $aptpackages = [
+    #'apt-transport-https',
+    #'ca-certificates',
+    #'curl',
+    #'software-properties-common',
+    'docker_ce',
+  ]
+  package { $aptpackages: ensure   => present, }
+
+}
+#
+# vim: sw=2:ai:nu expandtab
