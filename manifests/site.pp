7b84015bd862a1e46ec611635dde3b393cee4551
mergwyn <gary@theclarkhome.com>
2018-12-24 16:28:36 +0000

    Move handling of role etc outside default node

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,78 +1,78 @@
 #
 
-node default {
+if defined('$facts') and defined('$trusted') {
+  if $trusted['extensions']['pp_role'] and !has_key($facts,'role') {
+    $role = $trusted['extensions']['pp_role']
+  }
+  if $trusted['extensions']['pp_environment'] and !has_key($facts,'env') {
+    $env = $trusted['extensions']['pp_environment']
+  }
+  if $trusted['extensions']['pp_datacenter'] and !has_key($facts,'datacenter') {
+    $datacenter = $trusted['extensions']['pp_datacenter']
+  }
+  if $trusted['extensions']['pp_zone'] and !has_key($facts,'zone') {
+    $zone = $trusted['extensions']['pp_zone']
+  }
+  if $trusted['extensions']['pp_application'] and !has_key($facts,'application') {
+    $application = $trusted['extensions']['pp_application']
+  }
 
-  if defined('$facts') and defined('$trusted') {
-    if $trusted['extensions']['pp_role'] and !has_key($facts,'role') {
-      $role = $trusted['extensions']['pp_role']
-    }
-    if $trusted['extensions']['pp_environment'] and !has_key($facts,'env') {
-      $env = $trusted['extensions']['pp_environment']
-    }
-    if $trusted['extensions']['pp_datacenter'] and !has_key($facts,'datacenter') {
-      $datacenter = $trusted['extensions']['pp_datacenter']
-    }
-    if $trusted['extensions']['pp_zone'] and !has_key($facts,'zone') {
-      $zone = $trusted['extensions']['pp_zone']
-    }
-    if $trusted['extensions']['pp_application'] and !has_key($facts,'application') {
-      $application = $trusted['extensions']['pp_application']
-    }
-
-    ### RESOURCE DEFAULTS
-    # Some resource defaults for Files and Execs
-    case $::kernel {
-      'Darwin': {
-        File {
-          owner => 'root',
-          group => 'wheel',
-          mode  => '0644',
-        }
-        Exec {
-          path => $::path,
-        }
+  ### RESOURCE DEFAULTS
+  # Some resource defaults for Files and Execs
+  case $::kernel {
+    'Darwin': {
+      File {
+        owner => 'root',
+        group => 'wheel',
+        mode  => '0644',
       }
-      'Windows': {
-        File {
-          owner => 'Administrator',
-          group => 'Administrators',
-          mode  => '0644',
-        }
-        Exec {
-          path => $::path,
-        }
-      }
-      default: {
-        File {
-          owner  => 'root',
-          group  => 'root',
-          mode   => '0644',
-          backup => false,              # Disable filebucket by default for all File resources:
-        }
-        Exec {
-          path => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
-        }
+      Exec {
+        path => $::path,
       }
     }
-    # Look up profiles
-    lookup('profiles', Array[String], 'unique', []).contain
-
-    case $::kernel {
-      'Linux': {
-        lookup('linux_profiles', Array[String], 'unique', []).contain
+    'Windows': {
+      File {
+        owner => 'Administrator',
+        group => 'Administrators',
+        mode  => '0644',
       }
-      'Windows': {
-        lookup('windows_profiles', Array[String], 'unique', []).contain
+      Exec {
+        path => $::path,
       }
-      'Darwin': {
-        lookup('darwin_profiles', Array[String], 'unique', []).contain
+    }
+    default: {
+      File {
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        backup => false,              # Disable filebucket by default for all File resources:
       }
-      default: {
+      Exec {
+        path => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
       }
     }
   }
+  # Look up profiles
+  lookup('profiles', Array[String], 'unique', []).contain
 
-  lookup('classes', Array[String], 'unique', []).include
+  case $::kernel {
+    'Linux': {
+      lookup('linux_profiles', Array[String], 'unique', []).contain
+    }
+    'Windows': {
+      lookup('windows_profiles', Array[String], 'unique', []).contain
+    }
+    'Darwin': {
+      lookup('darwin_profiles', Array[String], 'unique', []).contain
+    }
+    default: {
+    }
+  }
+}
+
+lookup('classes', Array[String], 'unique', []).include
+
+node default {
 
 }
 # vim: sw=2:ai:nu expandtab
