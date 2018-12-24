e50acd920457be15de8d8eba99f7fb8fe2da0f38
mergwyn <gary@theclarkhome.com>
2018-12-24 16:37:52 +0000

    remove complication role code

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,21 @@
 #
 
-if defined('$facts') and defined('$trusted') {
-  if $trusted['extensions']['pp_role'] and !has_key($facts,'role') {
-    $role = $trusted['extensions']['pp_role']
-  }
-  if $trusted['extensions']['pp_environment'] and !has_key($facts,'env') {
-    $env = $trusted['extensions']['pp_environment']
-  }
-  if $trusted['extensions']['pp_datacenter'] and !has_key($facts,'datacenter') {
-    $datacenter = $trusted['extensions']['pp_datacenter']
-  }
-  if $trusted['extensions']['pp_zone'] and !has_key($facts,'zone') {
-    $zone = $trusted['extensions']['pp_zone']
-  }
-  if $trusted['extensions']['pp_application'] and !has_key($facts,'application') {
-    $application = $trusted['extensions']['pp_application']
-  }
+#if defined('$facts') and defined('$trusted') {
+#  if $trusted['extensions']['pp_role'] and !has_key($facts,'role') {
+#    $role = $trusted['extensions']['pp_role']
+#  }
+#  if $trusted['extensions']['pp_environment'] and !has_key($facts,'env') {
+#    $env = $trusted['extensions']['pp_environment']
+#  }
+#  if $trusted['extensions']['pp_datacenter'] and !has_key($facts,'datacenter') {
+#    $datacenter = $trusted['extensions']['pp_datacenter']
+#  }
+#  if $trusted['extensions']['pp_zone'] and !has_key($facts,'zone') {
+#    $zone = $trusted['extensions']['pp_zone']
+#  }
+#  if $trusted['extensions']['pp_application'] and !has_key($facts,'application') {
+#    $application = $trusted['extensions']['pp_application']
+#  }
 
   ### RESOURCE DEFAULTS
   # Some resource defaults for Files and Execs
@@ -68,8 +68,8 @@ if defined('$facts') and defined('$trusted') {
     default: {
     }
   }
-}
-
+#}
+#
 lookup('classes', Array[String], 'unique', []).include
 
 node default {
