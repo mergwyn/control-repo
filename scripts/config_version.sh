83fe531009bb64eaa9e700fc4ded0de0c8d1215b
mergwyn <gary@theclarkhome.com>
2018-12-25 09:25:44 +0000

    sync with puppet template

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-ruby=/opt/puppetlabs/puppet/bin/ruby
-git=/usr/bin/git
 if [ -e $1/$2/.r10k-deploy.json ]
 then
-  ${ruby} $1/$2/scripts/code_manager_config_version.rb $1 $2
+  /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/code_manager_config_version.rb $1 $2
 elif [ -e /opt/puppetlabs/server/pe_version ]
 then
-  ${ruby} $1/$2/scripts/config_version.rb $1 $2
+  /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/config_version.rb $1 $2
 else
-  ${git} --version > /dev/null 2>&1 &&
-  ${git} --git-dir $1/$2/.git rev-parse HEAD ||
+  /usr/bin/git --version > /dev/null 2>&1 &&
+  /usr/bin/git --git-dir $1/$2/.git rev-parse HEAD ||
   date +%s
 fi
