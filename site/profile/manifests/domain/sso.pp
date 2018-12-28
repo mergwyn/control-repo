2e2d8a885d7fb2bd972528fde68ee98701dcc01d
mergwyn <gary@theclarkhome.com>
2018-12-28 18:22:24 +0000

    complete rename of samba classes

--- site/profile/manifests/domain/sso.pp
+++ site/profile/manifests/domain/sso.pp
@@ -4,8 +4,8 @@ class profile::domain::sso {
   $type = lookup( { 'name' => 'samba::dc::role', 'default_value' => 'member' } )
 
   case $type {
-    'member': { $require = 'profile::domain::member' }
-    default:  { $require = 'profile::domain::dc' }
+    'member': { $require = 'profile::samba::member' }
+    default:  { $require = 'profile::samba::dc' }
   }
   include $require
   #notify {"samba:dc:role $type for $::fqdn requires $require":}
