4fd34a69ffe75095ecfdddf48f9fca6791e1b500
mergwyn <gary@theclarkhome.com>
2019-01-05 13:41:37 +0000

    Move file creation to yaml

--- site/profile/manifests/base/linux.pp
+++ site/profile/manifests/base/linux.pp
@@ -2,39 +2,39 @@
 
 class profile::base::linux {
   # core stuff
-  $packages_present = [
-    'ubuntu-standard',
-    'update-notifier-common',
-    'landscape-common',
-    'vim',
-    'anacron',
-    'gpg',
-  ]
-  package { $packages_present: ensure => present, }
-  $packages_absent = [
-    'vim-tiny',
-    'mlocate'
-  ]
-  package { $packages_absent:   ensure => absent, }
-
-  file { '/etc/legal':                  ensure  => absent, }
-  file { '/etc/vim/vim.local':          ensure  => absent, }
-  file { '/etc/vim/vimrc.local':
-    ensure => present,
-    source => 'puppet:///modules/profile/vimrc.local',
-  }
-  file { '/etc/update-motd.d/10-help-text': ensure  => absent, }
-  file { '/etc/landscape/client.conf':
-    ensure  => present,
-    content => "[sysinfo]\nexclude_sysinfo_plugins = LandscapeLink\n",
-  }
-
-  if $::is_virtual == false or str2bool($::is_virtual) == false {
-    file { '/etc/udev/rules.d/11-media-by-label-auto-mount.rules':
-      ensure => present,
-      source => 'puppet:///modules/profile/11-media-by-label-auto-mount.rules',
-    }
-  }
+#  $packages_present = [
+#    'ubuntu-standard',
+#    'update-notifier-common',
+#    'landscape-common',
+#    'vim',
+#    'anacron',
+#    'gpg',
+#  ]
+#  package { $packages_present: ensure => present, }
+#  $packages_absent = [
+#    'vim-tiny',
+#    'mlocate'
+#  ]
+#  package { $packages_absent:   ensure => absent, }
+#
+#  file { '/etc/legal':                  ensure  => absent, }
+#  file { '/etc/vim/vim.local':          ensure  => absent, }
+#  file { '/etc/vim/vimrc.local':
+#    ensure => present,
+#    source => 'puppet:///modules/profile/vimrc.local',
+#  }
+#  file { '/etc/update-motd.d/10-help-text': ensure  => absent, }
+#  file { '/etc/landscape/client.conf':
+#    ensure  => present,
+#    content => "[sysinfo]\nexclude_sysinfo_plugins = LandscapeLink\n",
+#  }
+#
+#  if $::is_virtual == false or str2bool($::is_virtual) == false {
+#    file { '/etc/udev/rules.d/11-media-by-label-auto-mount.rules':
+#      ensure => present,
+#      source => 'puppet:///modules/profile/11-media-by-label-auto-mount.rules',
+#    }
+#  }
   host { $facts['networking']['hostname']:
     ensure => absent,
     ip     => '127.0.1.1',
