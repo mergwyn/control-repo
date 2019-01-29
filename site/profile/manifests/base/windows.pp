eca50f7fe6374e688da4c076fab3e766c6553d50
mergwyn <gary@theclarkhome.com>
2019-01-29 18:02:29 +0000

    remove vim

---
+++ site/profile/manifests/base/windows.pp
@@ -0,0 +1,19 @@
+#
+
+class profile::base::windows {
+  scheduled_task { 'GPO Backup':
+    command   => "$::system32\\WindowsPowerShell\\v1.0\\powershell.exe",
+    arguments => '-ExecutionPolicy RemoteSigned \\foxtrot\backup\GPO\GPOBackup.ps1',
+    working_dir => "\\foxtrot\backup\${trusted['hostname']}\GPO\",
+    enabled   => 'true',
+    trigger   => [
+      {
+        schedule   => 'daily',
+        start_time => '02:30'
+        user       => 'theclarkhome\\gary',
+      }
+    ],
+  }
+}
+
+# vim: sw=2:ai:nu expandtab
