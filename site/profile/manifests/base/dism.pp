e99eacb1e5302c6f306e8deee95e97e3988a0cbd
mergwyn <gary@theclarkhome.com>
2019-02-13 15:56:03 +0000

    Add generic dism module

---
+++ site/profile/manifests/base/dism.pp
@@ -0,0 +1,26 @@
+#
+# Dynamically create Puppet File resources using the Puppet built-in
+# 'create_resources' function.
+#
+
+class profile::base::dism (
+  Hash           $objects  = {},
+  Optional[Hash] $defaults = {},
+) {
+
+  case $::kernel {
+    'windows': {
+      require dism
+    }
+    default: {
+    }
+  }
+  unless empty ($objects) {
+    validate_raw_constructor ($objects)
+    validate_hash ($defaults)
+    create_resources(dism, $objects, $defaults)
+  }
+
+}
+
+# vim: sw=2:ai:nu expandtab
