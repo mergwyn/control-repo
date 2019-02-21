b7be91884fa8c956d5688fc3356c1015207539c1
mergwyn <gary@theclarkhome.com>
2019-02-21 16:09:16 +0000

    Update to remove error message on nil value

--- site/profile/lib/facter/backuppc_pubkey_rsa.rb
+++ site/profile/lib/facter/backuppc_pubkey_rsa.rb
@@ -1,5 +1,6 @@
 Facter.add('backuppc_pubkey_rsa') do
   setcode do
+    backuppc_pubkey_rsa = nil
     sshkey_path ||= case Facter.value(:osfamily)
                     when 'RedHat'
                       '/var/lib/BackupPC/.ssh/id_rsa.pub'
@@ -11,6 +12,8 @@ Facter.add('backuppc_pubkey_rsa') do
 
     if File.exist?(sshkey_path)
       File.open(sshkey_path).read.split(' ')[1]
+    else
+      backuppc_pubkey_rsa
     end
   end
 end
