4fce69f62319fef734824664f4a8f054c5fb16dd
mergwyn <gary@theclarkhome.com>
2019-03-10 11:59:27 +0000

    remove munki config

--- site/profile/manifests/web/munki.pp
+++ site/profile/manifests/web/munki.pp
@@ -4,20 +4,7 @@ class profile::web::munki {
   include profile::web::nginx
 
   nginx::resource::server { 'munki_repo':,
-    server_name          => [ $::facts['networking']['fqdn'] ],
-    listen_port          => 80,
-    use_default_location => false,
-    locations            => {
-      '/munki_repo/' => {
-        server              => 'munki_repo',
-        location_alias      => '/usr/share/nginx/html/munki-repo/',
-        autoindex           => 'off',
-        location_cfg_append => {
-          auth_pam              => '"Restricted area"',
-          auth_pam_service_name => '"nginx"',
-        }
-      }
-    }
+    ensure => absent,
   }
 }
 
