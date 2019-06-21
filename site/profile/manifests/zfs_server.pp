689826d57baab3a6e19bda3a00909aa6f61f9a85
mergwyn <gary@theclarkhome.com>
2019-06-21 16:23:13 +0100

    Increae min arc size to 1/4

--- site/profile/manifests/zfs_server.pp
+++ site/profile/manifests/zfs_server.pp
@@ -160,7 +160,7 @@ class profile::zfs_server {
   kmod::option { 'zfs_arc_min':
     module => 'zfs',
     option => 'zfs_arc_min',
-    value  => $::facts['memory']['system']['total_bytes']/8,
+    value  => $::facts['memory']['system']['total_bytes']/4,
     notify => Exec['update_initramfs_all']
   }
   exec { 'update_initramfs_all':
