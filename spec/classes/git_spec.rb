f5577da4e25132198c3a08f081fb8cb885ab806e
mergwyn <gary@theclarkhome.com>
2019-07-13 17:43:15 +0100

    Fix lint error

--- spec/classes/git_spec.rb
+++ spec/classes/git_spec.rb
@@ -1,17 +1,11 @@
-# spec/classes/profile/git_spec.rb
 require 'spec_helper'
-describe 'profile::git' do
-  on_supported_os.each do |os, facts|
-    next unless facts[:kernel] == 'Linux'
-    context "on #{os}" do
-      let (:facts) {
-        facts.merge({
-          :clientcert => 'build',
-        })
-      }
-#      let(:pre_condition) { 'include profile::git' }
 
-      it { is_expected.to compile.with_all_deps }
+describe 'profile::git' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
     end
   end
 end
