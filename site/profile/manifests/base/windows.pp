db87798eb05a83fe8dc3a212f965a7f6c1cff07c
mergwyn <gary@theclarkhome.com>
2019-07-15 22:59:40 +0100

    Add password

--- site/profile/manifests/base/windows.pp
+++ site/profile/manifests/base/windows.pp
@@ -12,10 +12,12 @@ class profile::base::windows {
 #    norestart => true,
 #  }
   scheduled_task { 'GPO Backup':
-    command     => "${::system32}\\WindowsPowerShell\\v1.0\\powershell.exe",
+    command     => "powershell.exe",
     arguments   => "-ExecutionPolicy RemoteSigned \\\\${lookup('defaults::backup_server')}\\backup\\GPO\\GPOBackup.ps1",
     working_dir => "\\\\${lookup('defaults::backup_server')}\\backup\\${trusted['hostname']}\\GPO\\",
     enabled     => true,
+    user        => 'THECLARKHOME\\gary',
+    password    => hiera('secrets::gary'),
     trigger     => [
       {
         schedule   => 'daily',
