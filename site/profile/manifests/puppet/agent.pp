3d086d0db4bbc5fb5ffc08c27d176e1beafcd493
mergwyn <gary@theclarkhome.com>
2019-07-15 23:04:54 +0100

    Modify path to include /o/p/puppet/bin

--- site/profile/manifests/puppet/agent.pp
+++ site/profile/manifests/puppet/agent.pp
@@ -15,8 +15,8 @@ class profile::puppet::agent {
   package { 'puppetlabs-release-pc1': ensure => absent }
 
   file { '/etc/profile.d/puppet.sh':
-    ensure  => absent,
-    content => "# Expand the PATH to include puppet binaries\nPATH=\$PATH:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin\n"
+    ensure  => present,
+    content => "# Expand the PATH to include extra puppet binaries\nPATH=\$PATH:/opt/puppetlabs/puppet/bin\n"
   }
 }
 # vim: sw=2:ai:nu expandtab
