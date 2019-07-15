dc8e225075fbf1010698a7c2818849bd59e6413b
mergwyn <gary@theclarkhome.com>
2019-07-15 21:48:56 +0100

    Add support for creating scheduled task

--- site/profile/manifests/base/windows.pp
+++ site/profile/manifests/base/windows.pp
@@ -11,19 +11,18 @@ class profile::base::windows {
 #    ensure    => present,
 #    norestart => true,
 #  }
-#  scheduled_task { 'GPO Backup':
-#    command   => "$::system32\\WindowsPowerShell\\v1.0\\powershell.exe",
-#    arguments => '-ExecutionPolicy RemoteSigned \\foxtrot\backup\GPO\GPOBackup.ps1',
-#    working_dir => "\\foxtrot\backup\${trusted['hostname']}\GPO\",
-#    enabled   => 'true',
-#    trigger   => [
-#      {
-#        schedule   => 'daily',
-#        start_time => '02:30'
-#        user       => 'theclarkhome\\gary',
-#      }
-#    ],
-#  }
+  scheduled_task { 'GPO Backup':
+    command     => "${::system32}\\WindowsPowerShell\\v1.0\\powershell.exe",
+    arguments   => "-ExecutionPolicy RemoteSigned \\\\${lookup('defaults::backup_server')}\\backup\\GPO\\GPOBackup.ps1",
+    working_dir => "\\\\${lookup('defaults::backup_server')}\\backup\\${trusted['hostname']}\\GPO\\",
+    enabled     => true,
+    trigger     => [
+      {
+        schedule   => 'daily',
+        start_time => '02:30'
+      }
+    ],
+  }
 }
 
 # vim: sw=2:ai:nu expandtab
