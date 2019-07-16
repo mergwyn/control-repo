f3bc7502fe7edc6dee4084f10ba47201cf7ebe32
mergwyn <gary@theclarkhome.com>
2019-07-16 12:54:34 +0100

    rely on generic tasks solution

--- site/profile/manifests/base/windows.pp
+++ site/profile/manifests/base/windows.pp
@@ -11,20 +11,20 @@ class profile::base::windows {
 #    ensure    => present,
 #    norestart => true,
 #  }
-  scheduled_task { 'GPO Backup':
-    command     => 'powershell.exe',
-    arguments   => "-ExecutionPolicy RemoteSigned \\\\${lookup('defaults::backup_server')}\\backup\\GPO\\GPOBackup.ps1",
-    working_dir => "\\\\${lookup('defaults::backup_server')}\\backup\\${trusted['hostname']}\\GPO\\",
-    enabled     => true,
-    user        => "${lookup('defaults::workgroup')}\\backup",
-    password    => hiera('secrets::backup'),
-    trigger     => [
-      {
-        schedule   => 'daily',
-        start_time => '02:30'
-      }
-    ],
-  }
+#  scheduled_task { 'GPO Backup':
+#    command     => 'powershell.exe',
+#    arguments   => "-ExecutionPolicy RemoteSigned \\\\${lookup('defaults::backup_server')}\\backup\\GPO\\GPOBackup.ps1",
+#    working_dir => "\\\\${lookup('defaults::backup_server')}\\backup\\${trusted['hostname']}\\GPO\\",
+#    enabled     => true,
+#    user        => "${lookup('defaults::workgroup')}\\backup",
+#    password    => hiera('secrets::backup'),
+#    trigger     => [
+#      {
+#        schedule   => 'daily',
+#        start_time => '02:30'
+#      }
+#    ],
+#  }
 }
 
 # vim: sw=2:ai:nu expandtab
