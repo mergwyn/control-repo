1ad2ba91616f94892a82401fefd575235e823ff8
mergwyn <gary@theclarkhome.com>
2019-07-25 12:51:22 +0100

    convert to mod 'ghoneycutt-ssh'

--- site/profile/manifests/base/ssh_server.pp
+++ site/profile/manifests/base/ssh_server.pp
@@ -1,45 +1,46 @@
 #
 
 class profile::base::ssh_server {
-  class { 'ssh::server':
-    options              => {
-      'HostKey'                         => [
-        '/etc/ssh/ssh_host_rsa_key',
-        '/etc/ssh/ssh_host_dsa_key',
-        '/etc/ssh/ssh_host_ecdsa_key'
-      ],
-      'ChallengeResponseAuthentication' => 'no',
-      'GSSAPIAuthentication'            => 'yes',
-      'GSSAPICleanupCredentials'        => 'yes',
-      'GSSAPIKeyExchange'               => 'yes',
-      'HostbasedAuthentication'         => 'no',
-      'IgnoreRhosts'                    => 'yes',
-      'KerberosAuthentication'          => 'yes',
-      'KeyRegenerationInterval'         => '3600',
-      'LoginGraceTime'                  => '120',
-      'LogLevel'                        => 'INFO',
-      'PermitEmptyPasswords'            => 'no',
-      'PermitRootLogin'                 => 'yes',
-      'PermitUserEnvironment'           => 'yes',
-      'PrintLastLog'                    => 'yes',
-      'PrintMotd'                       => 'no',
-      'PubkeyAuthentication'            => 'yes',
-      'RhostsRSAAuthentication'         => 'no',
-      'RSAAuthentication'               => 'yes',
-      'ServerKeyBits'                   => '1024',
-      'StrictModes'                     => 'yes',
-      'SyslogFacility'                  => 'AUTH',
-      'TCPKeepAlive'                    => 'yes',
-      'UseDNS'                          => 'no',
-      'UsePAM'                          => 'yes',
-      'UsePrivilegeSeparation'          => 'yes',
-      'X11DisplayOffset'                => '10',
-      'X11Forwarding'                   => 'yes',
-    },
-  }
+#  class { 'ssh::server':
+#    options              => {
+#      'HostKey'                         => [
+#        '/etc/ssh/ssh_host_rsa_key',
+#        '/etc/ssh/ssh_host_dsa_key',
+#        '/etc/ssh/ssh_host_ecdsa_key'
+#      ],
+#      'ChallengeResponseAuthentication' => 'no',
+#      'GSSAPIAuthentication'            => 'yes',
+#      'GSSAPICleanupCredentials'        => 'yes',
+#      'GSSAPIKeyExchange'               => 'yes',
+#      'HostbasedAuthentication'         => 'no',
+#      'IgnoreRhosts'                    => 'yes',
+#      'KerberosAuthentication'          => 'yes',
+#      'KeyRegenerationInterval'         => '3600',
+#      'LoginGraceTime'                  => '120',
+#      'LogLevel'                        => 'INFO',
+#      'PermitEmptyPasswords'            => 'no',
+#      'PermitRootLogin'                 => 'yes',
+#      'PermitUserEnvironment'           => 'yes',
+#      'PrintLastLog'                    => 'yes',
+#      'PrintMotd'                       => 'no',
+#      'PubkeyAuthentication'            => 'yes',
+#      'RhostsRSAAuthentication'         => 'no',
+#      'RSAAuthentication'               => 'yes',
+#      'ServerKeyBits'                   => '1024',
+#      'StrictModes'                     => 'yes',
+#      'SyslogFacility'                  => 'AUTH',
+#      'TCPKeepAlive'                    => 'yes',
+#      'UseDNS'                          => 'no',
+#      'UsePAM'                          => 'yes',
+#      'UsePrivilegeSeparation'          => 'yes',
+#      'X11DisplayOffset'                => '10',
+#      'X11Forwarding'                   => 'yes',
+#    },
+#  }
   exec { 'client_keys':
     command => '/usr/bin/ssh-keygen -q -N "" -f /root/.ssh/id_rsa -t rsa',
     creates => '/root/.ssh/id_rsa',
   }
+  include ::ssh
 }
 # vim: nu sw=2 ai expandtab
