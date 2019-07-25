491cbb177ecd9bac86e60721fb532bce19b54822
mergwyn <gary@theclarkhome.com>
2019-07-25 17:40:17 +0100

    fix dir dependencies

--- site/profile/manifests/backuppc/client.pp
+++ site/profile/manifests/backuppc/client.pp
@@ -14,6 +14,7 @@ class profile::backuppc::client (
   file {[$scripts, $preuser, $postuser]:
     ensure  => directory,
     recurse => true,
+    require => File ['/etc/backupppc'],
   }
   file {[ "${scripts}/PreUser", "${scripts}/PostUser"]:
     ensure => absent,
