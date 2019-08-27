49635016a0dee55a1c14f9ae0d719f620587ffbb
mergwyn <gary@theclarkhome.com>
2019-08-27 13:13:19 +0100

    Add mounts

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -5,6 +5,7 @@ class profile::base {
   include profile::base::users
   include profile::base::files
   include profile::base::packages
+  include profile::base::mounts
 }
 
 # vim: sw=2:ai:nu expandtab
