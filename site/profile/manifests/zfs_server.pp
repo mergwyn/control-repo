8ac2a04c8ebaf5fe7bc7a17bdfe6da77c99281e6
mergwyn <gary@theclarkhome.com>
2019-09-06 20:43:39 +0100

    Add additional tuning settings

--- site/profile/manifests/zfs_server.pp
+++ site/profile/manifests/zfs_server.pp
@@ -171,9 +171,25 @@ class profile::zfs_server {
     value  => 'noop',
     notify => Exec['update_initramfs_all']
   }
+  # use the prefetch method
+  kmod::option { 'zfs_prefetch_disable':
+    module => 'zfs',
+    option => 'zfs_prefetch_disable',
+    value  => 0,
+    notify => Exec['update_initramfs_all']
+  }
+  # max write speed to l2arc
+  # tradeoff between write/read and durability of ssd (?)
+  # default : 8 * 1024 * 1024
+  # setting here : 500 * 1024 * 1024
+  kmod::option { 'l2arc_write_max':
+    module => 'zfs',
+    option => 'l2arc_write_max',
+    value  => 524288000,
+    notify => Exec['update_initramfs_all']
+  }
   exec { 'update_initramfs_all':
     command     => '/usr/sbin/update-initramfs -k all -u',
     refreshonly => true
   }
-
 }
