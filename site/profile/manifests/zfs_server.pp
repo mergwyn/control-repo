d4f89797ef4c22c3d1a5aa15d747ac6e784add18
mergwyn <gary@theclarkhome.com>
2020-05-03 14:48:33 +0100

    refactor for focal support

--- site/profile/manifests/zfs_server.pp
+++ site/profile/manifests/zfs_server.pp
@@ -36,106 +36,106 @@ class profile::zfs_server {
     mode    => '0775',
   }
 
-  # grub menu generation
-  file { '/etc/grub.d/42_zfs_select':
-    ensure => present,
-    source => 'puppet:///modules/profile/zfs/42_zfs_select',
-    mode   => '0755',
-  }
-  file { '/etc/grub.d/43_zfs_snap':
-    ensure => present,
-    source => 'puppet:///modules/profile/zfs/43_zfs_snap',
-    mode   => '0755',
-  }
-  file { '/etc/cron.daily/grubupdate':
-    ensure  => present,
-    content => "#!/bin/bash\n\n update-grub 2>&1 | logger -t update-grub -i\n",
-    mode    => '0555',
-  }
-
-  # zfs autosnap 
-  vcsrepo { "${codedir}/zfs-auto-snapshot":
-    ensure   => latest,
-    provider => git,
-    source   => 'https://github.com/zfsonlinux/zfs-auto-snapshot',
-    revision => 'master',
-    require  => Package['git', 'gawk'],
-  }
-  file { "${bindir}/zfs-auto-snapshot":
-    ensure  => present,
-    mode    => '0755',
-    source  => "file://${codedir}/zfs-auto-snapshot/src/zfs-auto-snapshot.sh",
-    require => Vcsrepo["${codedir}/zfs-auto-snapshot"],
-  }
-  file { '/sbin/zfs-auto-snapshot': ensure => absent, }
-
-  file { "${mandir}/man8": ensure => directory, }
-
-  file { "${mandir}/man8/zfs-auto-snapshot.8":
-    ensure  => present,
-    mode    => '0644',
-    source  => "file://${codedir}/zfs-auto-snapshot/src/zfs-auto-snapshot.8",
-    require => Vcsrepo["${codedir}/zfs-auto-snapshot"],
-  }
-
+#  # grub menu generation
+#  file { '/etc/grub.d/42_zfs_select':
+#    ensure => present,
+#    source => 'puppet:///modules/profile/zfs/42_zfs_select',
+#    mode   => '0755',
+#  }
+#  file { '/etc/grub.d/43_zfs_snap':
+#    ensure => present,
+#    source => 'puppet:///modules/profile/zfs/43_zfs_snap',
+#    mode   => '0755',
+#  }
+#  file { '/etc/cron.daily/grubupdate':
+#    ensure  => present,
+#    content => "#!/bin/bash\n\n update-grub 2>&1 | logger -t update-grub -i\n",
+#    mode    => '0555',
+#  }
+#
+#  # zfs autosnap 
+#  vcsrepo { "${codedir}/zfs-auto-snapshot":
+#    ensure   => latest,
+#    provider => git,
+#    source   => 'https://github.com/zfsonlinux/zfs-auto-snapshot',
+#    revision => 'master',
+#    require  => Package['git', 'gawk'],
+#  }
+#  file { "${bindir}/zfs-auto-snapshot":
+#    ensure  => present,
+#    mode    => '0755',
+#    source  => "file://${codedir}/zfs-auto-snapshot/src/zfs-auto-snapshot.sh",
+#    require => Vcsrepo["${codedir}/zfs-auto-snapshot"],
+#  }
+##  file { '/sbin/zfs-auto-snapshot': ensure => absent, }
+#
+#  file { "${mandir}/man8": ensure => directory, }
+#
+#  file { "${mandir}/man8/zfs-auto-snapshot.8":
+#    ensure  => present,
+#    mode    => '0644',
+#    source  => "file://${codedir}/zfs-auto-snapshot/src/zfs-auto-snapshot.8",
+#    require => Vcsrepo["${codedir}/zfs-auto-snapshot"],
+#  }
+#
   cron::job {'zfs-auto-snapshot':
     environment => [ 'PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"'],
-    command     => 'zfs-auto-snapshot --quiet --syslog --label=01 --keep=4  //',
+    command     => 'which zfs-auto-snapshot > /dev/null || exit 0 ; zfs-auto-snapshot --quiet --syslog --label=01 --keep=4  //',
     user        => 'root',
     minute      => '*/15',
   }
   file { '/etc/cron.hourly/zfs-auto-snapshot':
     ensure  => present,
     mode    => '0555',
-    content => "#!/bin/sh\nexec zfs-auto-snapshot --quiet --syslog --label=02 --keep=24 //\n",
+    content => "#!/bin/sh\nwhich zfs-auto-snapshot > /dev/null || exit 0\nexec zfs-auto-snapshot --quiet --syslog --label=02 --keep=24 //\n",
   }
   file { '/etc/cron.daily/zfs-auto-snapshot':
     ensure  => present,
     mode    => '0555',
-    content => "#!/bin/sh\nexec zfs-auto-snapshot --quiet --syslog --label=03 --keep=14 //\n",
+    content => "#!/bin/sh\nwhich zfs-auto-snapshot > /dev/null || exit 0\nexec zfs-auto-snapshot --quiet --syslog --label=03 --keep=14 //\n",
   }
   file { '/etc/cron.weekly/zfs-auto-snapshot':
     ensure  => present,
     mode    => '0555',
-    content => "#!/bin/sh\nexec zfs-auto-snapshot --quiet --syslog --label=04 --keep=8 //\n",
+    content => "#!/bin/sh\nwhich zfs-auto-snapshot > /dev/null || exit 0\nexec zfs-auto-snapshot --quiet --syslog --label=04 --keep=8 //\n",
   }
   file { '/etc/cron.monthly/zfs-auto-snapshot':
     ensure  => present,
     mode    => '0555',
-    content => "#!/bin/sh\nexec zfs-auto-snapshot --quiet --syslog --label=05 --keep=12 //\n",
+    content => "#!/bin/sh\nwhich zfs-auto-snapshot > /dev/null || exit 0\nexec zfs-auto-snapshot --quiet --syslog --label=05 --keep=12 //\n",
   }
 
-  # beadm boot environments
-
-  # lxd snap related commands
-  package { 'gawk': }
-  vcsrepo { "${codedir}/beadm":
-      ensure   => latest,
-      provider => git,
-      source   => 'https://github.com/TemptorSent/beadm',
-      revision => 'master',
-      require  => Package['git', 'gawk'],
-  }
-  file { "${bindir}/beadm":
-    ensure => present,
-    mode   => '0555',
-    source => "file://${codedir}/beadm/beadm",
-  }
-  file { "${mandir}/man1": ensure => directory, }
-  file { "${mandir}/man1/beadm.1":
-    ensure => present,
-    mode   => '0644',
-    source => "file://${codedir}/beadm/beadm.1",
-  }
-
-  file { '/etc/default/beadm.conf':
-    ensure => absent,
-  }
-  file { '/etc/beadm.conf':
-    ensure  => present,
-    mode    => '0555',
-    content => "#\nGRUB=YES\n",
-  }
+#  # beadm boot environments
+#
+#  # lxd snap related commands
+#  package { 'gawk': }
+#  vcsrepo { "${codedir}/beadm":
+#      ensure   => latest,
+#      provider => git,
+#      source   => 'https://github.com/TemptorSent/beadm',
+#      revision => 'master',
+#      require  => Package['git', 'gawk'],
+#  }
+#  file { "${bindir}/beadm":
+#    ensure => present,
+#    mode   => '0555',
+#    source => "file://${codedir}/beadm/beadm",
+#  }
+#  file { "${mandir}/man1": ensure => directory, }
+#  file { "${mandir}/man1/beadm.1":
+#    ensure => present,
+#    mode   => '0644',
+#    source => "file://${codedir}/beadm/beadm.1",
+#  }
+#
+#  file { '/etc/default/beadm.conf':
+#    ensure => absent,
+#  }
+#  file { '/etc/beadm.conf':
+#    ensure  => present,
+#    mode    => '0555',
+#    content => "#\nGRUB=YES\n",
+#  }
 
   # zabbix support
   zabbix::userparameters { 'zfs-auto':
@@ -152,46 +152,46 @@ class profile::zfs_server {
     content => 'zabbix	ALL=(root)	NOPASSWD:	/sbin/zfs',
   }
 
-  # set kernel parameters
-  kmod::option { 'zfs_arc_max':
-    module => 'zfs',
-    option => 'zfs_arc_max',
-    #value  => $::facts['memory']['system']['total_bytes']*7/10,
-    value  => 0,
-    notify => Exec['update_initramfs_all'],
-  }
-  kmod::option { 'zfs_arc_min':
-    module => 'zfs',
-    option => 'zfs_arc_min',
-    #value  => $::facts['memory']['system']['total_bytes']*3/10,
-    value  => 0,
-    notify => Exec['update_initramfs_all'],
-  }
-  kmod::option { 'zfs_vdev_scheduler':
-    module => 'zfs',
-    option => 'zfs_vdev_scheduler',
-    value  => 'noop',
-    notify => Exec['update_initramfs_all'],
-  }
-  # use the prefetch method
-  kmod::option { 'zfs_prefetch_disable':
-    module => 'zfs',
-    option => 'zfs_prefetch_disable',
-    value  => 0,
-    notify => Exec['update_initramfs_all'],
-  }
-  # max write speed to l2arc
-  # tradeoff between write/read and durability of ssd (?)
-  # default : 8 * 1024 * 1024
-  # setting here : 500 * 1024 * 1024
-  kmod::option { 'l2arc_write_max':
-    module => 'zfs',
-    option => 'l2arc_write_max',
-    value  => 524288000,
-    notify => Exec['update_initramfs_all'],
-  }
-  exec { 'update_initramfs_all':
-    command     => '/usr/sbin/update-initramfs -k all -u',
-    refreshonly => true,
-  }
+#  # set kernel parameters
+#  kmod::option { 'zfs_arc_max':
+#    module => 'zfs',
+#    option => 'zfs_arc_max',
+#    #value  => $::facts['memory']['system']['total_bytes']*7/10,
+#    value  => 0,
+#    notify => Exec['update_initramfs_all'],
+#  }
+#  kmod::option { 'zfs_arc_min':
+#    module => 'zfs',
+#    option => 'zfs_arc_min',
+##    #value  => $::facts['memory']['system']['total_bytes']*3/10,
+#    value  => 0,
+#    notify => Exec['update_initramfs_all'],
+#  }
+#  kmod::option { 'zfs_vdev_scheduler':
+#    module => 'zfs',
+#    option => 'zfs_vdev_scheduler',
+#    value  => 'noop',
+#    notify => Exec['update_initramfs_all'],
+#  }
+#  # use the prefetch method
+#  kmod::option { 'zfs_prefetch_disable':
+#    module => 'zfs',
+#    option => 'zfs_prefetch_disable',
+#    value  => 0,
+#    notify => Exec['update_initramfs_all'],
+#  }
+#  # max write speed to l2arc
+#  # tradeoff between write/read and durability of ssd (?)
+#  # default : 8 * 1024 * 1024
+#  # setting here : 500 * 1024 * 1024
+#  kmod::option { 'l2arc_write_max':
+#    module => 'zfs',
+#    option => 'l2arc_write_max',
+#    value  => 524288000,
+#    notify => Exec['update_initramfs_all'],
+#  }
+#  exec { 'update_initramfs_all':
+#    command     => '/usr/sbin/update-initramfs -k all -u',
+#    refreshonly => true,
+#  }
 }
