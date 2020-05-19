12a87c0d1d541b3525f160c349bb850ea7176745
mergwyn <gary@theclarkhome.com>
2020-05-19 10:41:53 +0100

    correct nginx commands for letsencrypt

--- site/profile/manifests/certificate_server.pp
+++ site/profile/manifests/certificate_server.pp
@@ -9,8 +9,7 @@ class profile::certificate_server (
     plugin               => 'nginx',
     domains              => $domains,
     manage_cron          => true,
-    cron_before_command  => 'service nginx stop',
-    cron_success_command => '/bin/systemctl reload nginx.service',
+    cron_success_command => 'systemctl reload nginx',
     suppress_cron_output => true,
   }
 }
