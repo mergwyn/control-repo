ee9ec5f36afb9fe4a083013ea3ba169d6ba03454
mergwyn <gary@theclarkhome.com>
2020-06-01 12:21:38 +0100

    zabbix module does not support nginx (yet)

--- site/profile/manifests/zabbix/server.pp
+++ site/profile/manifests/zabbix/server.pp
@@ -3,15 +3,15 @@
 class profile::zabbix::server {
 
   # additional packages needed for ubuntu 16.04 and zabbix 3
-  package { [ 'php-xml', 'php-mbstring', 'php-bcmath' ] : }
+  #package { [ 'php-xml', 'php-mbstring', 'php-bcmath' ] : }
   #package { [ 'snmp', 'snmp-builder' ] : }
 
-  class { 'apache': mpm_module => 'prefork', }
-  include apache::mod::php
+  #class { 'apache': mpm_module => 'prefork', }
+  #include apache::mod::php
 
   include profile::mysql::server
+  package { [ 'zabbix-server-mysql', 'zabbix-nginx-conf' ] : }
 
-  class { 'zabbix': }
+  #class { 'zabbix': }
 
 }
-# vim: sw=2:ai:nu expandtab
