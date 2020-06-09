5414ed3924c64b3dda279cf7292b7271cd33866a
mergwyn <gary@theclarkhome.com>
2020-06-09 19:17:21 +0100

    initial pass of unit testing role classes

---
+++ spec/classes/role/media_server_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'role::media_server' do
+  on_supported_os.each do |os, os_facts|
+  next unless os_facts[:osfamily] == 'Debian'
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
