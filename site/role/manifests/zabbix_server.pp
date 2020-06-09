4e3383b1848b5592ecc67e989f7a5e3c940692c6
mergwyn <gary@theclarkhome.com>
2020-06-09 19:21:45 +0100

    fix typo

--- site/role/manifests/zabbix_server.pp
+++ site/role/manifests/zabbix_server.pp
@@ -1,6 +1,6 @@
 #
 class role::zabbix_server {
-  include profile::profile::baseline  # All roles should have the base profile
+  include profile::platform::baseline  # All roles should have the base profile
   include profile::domain::sso
   include profile::zabbix::server
   include profile::zabbix::agent
