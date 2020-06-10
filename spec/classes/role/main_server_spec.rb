6cf128b4db8e5f62b6c3dd0e03ea44bfcde055d6
mergwyn <gary@theclarkhome.com>
2020-06-10 16:30:54 +0100

    update fixtures and refesh test spec format

--- spec/classes/role/main_server_spec.rb
+++ spec/classes/role/main_server_spec.rb
@@ -2,11 +2,21 @@ require 'spec_helper'
 
 describe 'role::main_server' do
   on_supported_os.each do |os, os_facts|
-    next unless os_facts[:osfamily] == 'Debian'
-    context "on #{os}" do
-      let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+    next unless os_facts[:osfamily] == 'Debian'
+
+    context "on #{os}" do
+      # add these two lines in a single test block to enable puppet and hiera debug mode
+      # Puppet::Util::Log.level = :debug
+      # Puppet::Util::Log.newdestination(:console)
+
+      let(:facts) { os_facts }
+      let(:trusted_facts) { { 'pp_role' => '/main_server' } }
+
+      # Comment out to display all available resources easily
+      it { pp catalogue.resources }
+
+      it { is_expected.to compile.with_all_deps }
     end
   end
 end
