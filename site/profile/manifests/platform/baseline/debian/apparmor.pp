f1396e1a2ae18977e47c55b2d5154d13edfc34e6
mergwyn <gary@theclarkhome.com>
2020-06-11 14:55:44 +0100

    refactor apparmor

---
+++ site/profile/manifests/platform/baseline/debian/apparmor.pp
@@ -0,0 +1,11 @@
+#
+class profile::platform::baseline::debian::apparmor {
+  package { 'apparmor': }
+
+  service { 'apparmor':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['apparmor']
+  }
+
+}
