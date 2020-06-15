66219c6b048e3dbc36845892c42a470ac0e4e3f7
mergwyn <gary@theclarkhome.com>
2020-06-15 15:44:06 +0100

    Create darwin packages manifest

--- site/profile/manifests/platform/baseline/darwin.pp
+++ site/profile/manifests/platform/baseline/darwin.pp
@@ -4,5 +4,6 @@ class profile::platform::baseline::darwin {
 
   include profile::platform::baseline::users::darwin
   include profile::platform::baseline::darwin::brew
+  include profile::platform::baseline::darwin::packages
 
 }
