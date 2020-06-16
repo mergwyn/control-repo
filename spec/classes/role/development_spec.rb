97d9af83fcbe7b7117b0d201384ed11c1110068c
mergwyn <gary@theclarkhome.com>
2020-06-16 16:17:45 +0100

    Add development role

---
+++ spec/classes/role/development_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'role::development' do
+  on_supported_os.each do |os, os_facts|
+    next unless os_facts[:osfamily] == 'Debian'
+
+    context "on #{os}" do
+      # add these two lines in a single test block to enable puppet and hiera debug mode
+      # Puppet::Util::Log.level = :debug
+      # Puppet::Util::Log.newdestination(:console)
+
+      let(:facts) { os_facts }
+      let(:trusted_facts) { { 'pp_role' => 'development' } }
+      let(:node) { 'unittest.theclarkhome.com' }
+
+      # Comment out to display all available resources easily
+      # it { pp catalogue.resources }
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
