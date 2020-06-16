a7774ce5834ef4cc4cc40aea4dec113961360ac7
mergwyn <gary@theclarkhome.com>
2020-06-16 09:17:45 +0100

    Move brew user setup to brew manifest

--- site/profile/manifests/platform/baseline/darwin/brew.pp
+++ site/profile/manifests/platform/baseline/darwin/brew.pp
@@ -8,8 +8,25 @@ class profile::platform::baseline::darwin::brew {
     creates => '/usr/bin/git',
   }
 
+  $home = '/Users/brew'
+
+  user {'brew':
+    gid        => 'admin',
+    password   => lookup('secrets::brew'),
+    iterations => 86956,
+    salt       => 'b78fbae626c563458942fea9b35f160ab02274e8e1c6b2403b9c7c93785a3915',
+    home       => $home,
+  }
+
+  file { $home:
+    ensure  => directory,
+    owner   => 'brew',
+    group   => 'admin',
+    require => User[brew],
+  }
+
   class { 'homebrew':
-    user         => 'gary',
+    user         => 'brew',
     group        => 'admin',
     multiuser    => true,
     github_token => lookup('secrets::github::homebrew'),
@@ -18,4 +35,9 @@ class profile::platform::baseline::darwin::brew {
       User['brew'],
     ],
   }
+
+  sudo::conf { 'brew':
+    content => 'gary ALL=(brew) NOPASSWD: /usr/local/bin/brew,/bin/bash',
+  }
+
 }
