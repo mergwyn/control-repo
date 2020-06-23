73b3e68274c54dc47cd9c5e0479020de0083caa9
mergwyn <gary@theclarkhome.com>
2020-06-23 10:12:31 +0100

    Add initial timemachine support

---
+++ site/profile/manifests/platform/baseline/darwin/timemachine.pp
@@ -0,0 +1,16 @@
+#
+
+class profile::platform::baseline::darwin::timemachine (
+  Integer $quota = 262144, # 256GB
+) {
+
+# TODO: tmutil setdestination afp://user[:pass]@host/share
+# TODO: tmutil setexclusion [-p] <item> (check with 'tmutil isexcluded')
+
+  exec {'set timemachine quota':
+    path    => $facts['path'],
+    unless  => "test $(defaults read /Library/Preferences/com.apple.TimeMachine MaxSize 2>/dev/null) -eq ${quota} ]",
+    command => "defaults write /Library/Preferences/com.apple.TimeMachine MaxSize ${quota}",
+  }
+
+}
