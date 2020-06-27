4b9c74720b3f8029720f7b6b8f27d0e865a173cd
mergwyn <gary@theclarkhome.com>
2020-06-27 09:23:05 +0100

    Add value to exec resource name

--- site/profile/manifests/platform/baseline/darwin/timemachine.pp
+++ site/profile/manifests/platform/baseline/darwin/timemachine.pp
@@ -7,7 +7,7 @@ class profile::platform::baseline::darwin::timemachine (
 # TODO: tmutil setdestination afp://user[:pass]@host/share
 # TODO: tmutil setexclusion [-p] <item> (check with 'tmutil isexcluded')
 
-  exec {'set timemachine quota':
+  exec {"set timemachine quota to ${quota}":
     path    => $facts['path'],
     onlyif  => "test `defaults read /Library/Preferences/com.apple.TimeMachine MaxSize 2>/dev/null` -ne ${quota}",
     command => "defaults write /Library/Preferences/com.apple.TimeMachine MaxSize ${quota}",
