ffbddb2d86dad8765bef56b83081c3bd8b3cfa43
mergwyn <gary@theclarkhome.com>
2020-07-09 11:14:03 +0100

    Move unattended_upgrades to manifest

--- site/profile/manifests/platform/baseline/debian.pp
+++ site/profile/manifests/platform/baseline/debian.pp
@@ -6,12 +6,12 @@ class profile::platform::baseline::debian {
   include profile::platform::baseline::debian::apparmor
   include profile::platform::baseline::debian::packages
   include profile::platform::baseline::debian::hosts
+  include profile::platform::baseline::debian::unattended_upgrades
 
   include profile::os::debian::base
   include profile::base::mounts
   include profile::base::ssh_server
   include profile::base::mail_client
-  include profile::base::unattended_upgrades
   include profile::base::avahi
   #include profile::service::webmin
   #include profile::app::backuppc::client
---
+++ site/profile/manifests/platform/baseline/debian/unattended_upgrades.pp
@@ -0,0 +1,25 @@
+# @summary unattended upgrade settings for debian
+#
+
+class profile::platform::baseline::debian::unattended_upgrades {
+  include apt
+  class {'::unattended_upgrades':
+    update  => '1',
+    auto    => {
+      remove => true,
+    },
+    origins => [
+      '${distro_id}:${distro_codename}',           #lint:ignore:single_quote_string_with_variables
+      '${distro_id}:${distro_codename}-security',  #lint:ignore:single_quote_string_with_variables
+      'Jamie Cameron:stable',
+      'Zabbix:${distro_codename}',                 #lint:ignore:single_quote_string_with_variables
+      'puppet:${distro_codename}',                 #lint:ignore:single_quote_string_with_variables
+    ],
+    mail    => {
+      'to'            => lookup('defaults::adminemail'),
+      'only_on_error' => false,
+    }
+  }
+
+  file { '/etc/apt/apt.conf.d/50unattended-upgrades.ucf-dist': ensure  => absent, }
+}
