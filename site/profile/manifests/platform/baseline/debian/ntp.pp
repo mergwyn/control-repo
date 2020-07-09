640f2d58379ccfb71d7b6fa1f26f60920f078a00
mergwyn <gary@theclarkhome.com>
2020-07-09 16:03:21 +0100

    Complete refactor of ntp

--- site/profile/manifests/platform/baseline/debian/ntp.pp
+++ site/profile/manifests/platform/baseline/debian/ntp.pp
@@ -1,28 +1,23 @@
-# @summary Wrapper for ntp support
-
 class profile::platform::baseline::debian::ntp (
   Optional[Array[String]] $servers = lookup('defaults::time_servers')
-  ) {
+) {
 
   $local_clock = '127.127.1.0'
   $network_servers = $servers + lookup('ntp::servers')
 
-# Build array for restrict using the combined network_servers array
-  $restrict = [
+  $restrict_default = [
     'default kod nomodify notrap nopeer mssntp',
     $local_clock,
   ]
 
-  $network_servers.each |String $server| {
-    $restrict = $restrict + [ "${server}   mask 255.255.255.255    nomodify notrap nopeer noquery" ]
-  }
+  $restrict_args = ' mask 255.255.255.255 nomodify notrap nopeer noquery'
+  $restrict = regsubst($network_servers, '$', $restrict_args)
 
   class { 'ntp':
     ntpsigndsocket    => '/var/lib/samba/ntp_signd/',
-    servers           => [ $local_clock ] + $network_servers,
     preferred_servers => $servers,
-    burst             => true,
+    servers           => [ $local_clock ] + $network_servers,
+    restrict          => $restrict_default + $restrict,
     fudge             => [ "${local_clock} stratum 10" ],
-    restrict          => $restrict,
   }
 }
