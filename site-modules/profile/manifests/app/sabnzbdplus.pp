33aa2cf9c896f19a59d5f43f53a3417677713262
mergwyn <gary@theclarkhome.com>
2020-07-17 18:41:27 +0100

    Add systemd  unit file and remove sysvinit

--- site-modules/profile/manifests/app/sabnzbdplus.pp
+++ site-modules/profile/manifests/app/sabnzbdplus.pp
@@ -17,9 +17,37 @@ class profile::app::sabnzbdplus (
     ensure  => present,
   }
 
-# Run as a different user
-  $service = 'sabnzbdplus@.service'
-  $service_user = "sabnzbdplus@${user}.service"
+# TODO this will be replaced in time by installed version from package
+  $service = 'sabnzbd@.service'
+  systemd::unit_file { $service:
+    enable  => false,
+    active  => false,
+    content => @("EOT"),
+      [Unit]
+      Description=SABnzbd binary newsreader
+      Documentation=https://sabnzbd.org/wiki/
+      Wants=network-online.target
+      After=network-online.target
+
+      [Service]
+      Environment="PYTHONIOENCODING=utf-8"
+      #ExecStart=/opt/sabnzbd/SABnzbd.py --logging 1 --browser 0
+      ExecStart=/usr/bin/sabnzbdplus --logging 1 --browser 0
+      User=%I
+      Type=simple
+      Restart=on-failure
+
+      [Install]
+      WantedBy=multi-user.target
+      | EOT
+  }
+# remove sysvinit files ready to replace with systemd
+  file { [ '/etc/init.d/sabnzbdplus', '/etc/default/sabnzbdplus' ]:
+    ensure => absent,
+  }
+
+# Keep this separate to allow wait for 
+  $service_user = "sabnzbds@${user}.service"
 
   systemd::dropin_file { 'wait-ssd.conf':
       unit    => $service,
