24cf3e80597fac07b3b8293248bea3067c1372df
mergwyn <gary@theclarkhome.com>
2020-07-20 16:17:46 +0100

    ntp only for physical machines

--- site-modules/profile/manifests/platform/baseline/debian/ntp.pp
+++ site-modules/profile/manifests/platform/baseline/debian/ntp.pp
@@ -4,22 +4,28 @@ class profile::platform::baseline::debian::ntp (
   Optional[Array[String]] $servers = lookup('defaults::time_servers')
 ) {
 
-  $local_clock = '127.127.1.0'
-  $network_servers = $servers + lookup('ntp::servers')
+# ntp for physical machines only
+  if $facts['virtual'] != 'physical' {
+    class { 'ntp': ensure => absent }
+  }
+  else {
+    $local_clock = '127.127.1.0'
+    $network_servers = $servers + lookup('ntp::servers')
 
-  $restrict_default = [
-    'default kod nomodify notrap nopeer mssntp',
-    $local_clock,
-  ]
+    $restrict_default = [
+      'default kod nomodify notrap nopeer mssntp',
+      $local_clock,
+    ]
 
-  $restrict_args = ' mask 255.255.255.255 nomodify notrap nopeer noquery'
-  $restrict = regsubst($network_servers, '$', $restrict_args)
+    $restrict_args = ' mask 255.255.255.255 nomodify notrap nopeer noquery'
+    $restrict = regsubst($network_servers, '$', $restrict_args)
 
-  class { 'ntp':
-    ntpsigndsocket    => '/var/lib/samba/ntp_signd/',
-    preferred_servers => $servers,
-    servers           => [ $local_clock ] + $network_servers,
-    restrict          => $restrict_default + $restrict,
-    fudge             => [ "${local_clock} stratum 10" ],
+    class { 'ntp':
+      ntpsigndsocket    => '/var/lib/samba/ntp_signd/',
+      preferred_servers => $servers,
+      servers           => [ $local_clock ] + $network_servers,
+      restrict          => $restrict_default + $restrict,
+      fudge             => [ "${local_clock} stratum 10" ],
+    }
   }
 }
