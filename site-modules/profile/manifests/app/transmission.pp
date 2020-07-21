fe24e479028eef3e82f5eca3c9396beee7400eee
mergwyn <gary@theclarkhome.com>
2020-07-21 10:12:09 +0100

    correct service name

--- site-modules/profile/manifests/app/transmission.pp
+++ site-modules/profile/manifests/app/transmission.pp
@@ -6,7 +6,7 @@ class profile::app::transmission  {
 # TODO: settings
   include ::transmission
 
-  $service = 'transmission-daemon'
+  $service = 'transmission-daemon.service'
   systemd::dropin_file { 'transmission-sssd-wait.conf':
       unit    => $service,
       content => @("EOT"/),
