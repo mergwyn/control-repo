def747af5d80ad147034419af253792d47ce14c3
mergwyn <gary@theclarkhome.com>
2020-07-21 10:59:09 +0100

    Refactor radarr and add dropin file

---
+++ site-modules/profile/manifests/app/radarr.pp
@@ -0,0 +1,19 @@
+# @summary radarr movie downloader
+#
+
+class profile::app::radarr  {
+
+# TODO: settings
+  include ::radarr
+
+  $service = 'radarr.service'
+  systemd::dropin_file { 'radarr-sssd-wait.conf':
+      unit    => $service,
+      content => @("EOT"/),
+                 [Unit]
+                 After=nss-user-lookup.target
+                 | EOT
+      notify  => Service[$service],
+  }
+
+}
