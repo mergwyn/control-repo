c34f580026c7e32cb16cbc9f3a9bbd3f086e3da3
mergwyn <gary@theclarkhome.com>
2020-07-22 15:34:54 +0100

    Add create of nginx runtimedir

--- site-modules/profile/manifests/web/nginx.pp
+++ site-modules/profile/manifests/web/nginx.pp
@@ -5,5 +5,14 @@ class profile::web::nginx {
   # TODO: move to hiera
   include nginx
   package { [ 'fcgiwrap' ]: }
+
+  $service_name = 'nginx.service'
+  systemd::unit_file { $service_name:
+    content => "[Service]\nRuntimeDirectory=nginx\n"
+  }
+  ~> service { $service_name:
+    ensure => 'running',
+  }
+
 }
 
