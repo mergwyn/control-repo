b817c77b27a68b582010d703516fe42cc8507b6e
mergwyn <gary@theclarkhome.com>
2020-07-22 15:44:25 +0100

    fix nginx startup, and other dropping fixes (#53)
    * add After=nss-user-lookup.target
    
    * Add dropin file and refactor tranmission
    
    * Make sure dropin files are unique
    
    * correct service name
    
    * Fix typo
    
    * Refactor radarr and add dropin file
    
    * Add create of nginx runtimedir
    
    * Add create of nginx runtimedir

--- site-modules/profile/manifests/web/nginx.pp
+++ site-modules/profile/manifests/web/nginx.pp
@@ -5,5 +5,18 @@ class profile::web::nginx {
   # TODO: move to hiera
   include nginx
   package { [ 'fcgiwrap' ]: }
+
+  $service_name = 'nginx.service'
+  $service = 'nginx.service'
+  systemd::dropin_file { 'nginx-runtime.conf':
+      unit    => $service_name,
+      content => @("EOT"/),
+                 [Service]
+                 RuntimeDirectory=nginx
+                 | EOT
+  } ~> service { $service_name:
+    ensure => 'running',
+  }
+
 }
 
