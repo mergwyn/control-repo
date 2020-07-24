26e42b5f0d1fc600c0edb9d142ec4a0f4a3d458c
mergwyn <gary@theclarkhome.com>
2020-07-24 17:27:53 +0100

    refactor mono

--- site-modules/profile/manifests/media/downloader.pp
+++ site-modules/profile/manifests/media/downloader.pp
@@ -3,13 +3,12 @@
 class profile::media::downloader () {
 
   #TODO insert dependency on mono into module
-  include profile::media::mono
+  include profile::app::mono
   include ::jackett
-  include profile::media::sonarr
+  include profile::app::sonarr
   include profile::app::couchpotato
   include profile::app::sabnzbdplus
   include profile::app::sonarr
   include profile::app::transmission
   include profile::app::radarr
 }
-# vim: sw=2:ai:nu expandtab
