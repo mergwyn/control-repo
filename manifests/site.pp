7d3a6a01a5f2a159cda928dc6887e6384cf4885e
mergwyn <gary@theclarkhome.com>
2020-08-11 17:28:23 +0100

    Fix typo

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ lookup('classes', Array[String], 'deep', []).contain
 if $trusted['extensions']['pp_role'] {
   include "role::${$trusted['extensions']['pp_role']}"
 } else {
-  warn('pp_role is not set')
+  warning('pp_role is not set')
 }
 
 node default {
