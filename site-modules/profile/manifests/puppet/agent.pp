26af5393a77f8f7e24d8754bc74a993b089898c6
mergwyn <gary@theclarkhome.com>
2020-10-03 18:29:42 +0100

    Remove workaround for sshkey issue
    Does the file need to be removed?

--- site-modules/profile/manifests/puppet/agent.pp
+++ site-modules/profile/manifests/puppet/agent.pp
@@ -13,11 +13,11 @@ class profile::puppet::agent {
     version  => "${ver[0]}*"
   }
 # TODO: remove workaround
-  apt::pin { 'puppet-6.16':
-    priority => 999,
-    packages => 'puppet-agent',
-    version  => '6.16.*'
-  }
+#  apt::pin { 'puppet-6.16':
+#    priority => 999,
+#    packages => 'puppet-agent',
+#    version  => '6.16.*'
+# }
   package { 'puppetlabs-release-pc1': ensure => absent }
 
   file { '/etc/profile.d/puppet.sh':
