4a1ca81875a0c8e360b093e232c4035dd4faa864
mergwyn <gary@theclarkhome.com>
2020-10-13 18:59:06 +0100

    handle arrays properly

--- site-modules/profile/manifests/app/dhcpd/vpnhost.pp
+++ site-modules/profile/manifests/app/dhcpd/vpnhost.pp
@@ -2,15 +2,15 @@
 
 define profile::app::dhcpd::vpnhost (
   Stdlib::MAC                    $mac         = undef,
-  Array[Stdlib::IP::Address::V4] $router      = '192.168.11.251',
-  Array[Stdlib::IP::Address::V4] $nameservers = '192.168.11.251',
+  Array[Stdlib::IP::Address::V4] $router      = [ '192.168.11.251' ],
+  Array[Stdlib::IP::Address::V4] $nameservers = [ '192.168.11.251' ],
   ) {
 
   dhcp::host { $title:
     mac     => $mac,
     options => {
-      routers             => $router ,
-      domain-name-servers => $nameservers,
+      routers             => join($router,',') ,
+      domain-name-servers => join($nameservers,','),
     }
   }
 }
