ff8ab8ba2c32d36565aa188365b85c947fc0f743
mergwyn <gary@theclarkhome.com>
2020-10-14 11:17:22 +0100

    Keep india on fixed ip

--- site-modules/profile/manifests/app/dhcpd/vpnhost.pp
+++ site-modules/profile/manifests/app/dhcpd/vpnhost.pp
@@ -1,12 +1,14 @@
 #
 
 define profile::app::dhcpd::vpnhost (
-  Stdlib::MAC             $mac     = undef,
-  Stdlib::IP::Address::V4 $gateway = '192.168.11.251'
+  Stdlib::MAC                      $mac     = undef,
+  Optional[Stdlib::IP::Address::V4] $ip      = undef,
+  Stdlib::IP::Address::V4          $gateway = '192.168.11.251'
   ) {
 
   dhcp::host { $title:
     mac     => $mac,
+    ip      => $ip,
     options => {
       routers             => $gateway,
       domain-name-servers => $gateway,
