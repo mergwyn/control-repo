9f05827f5d563b15fcf2ed753d91f8956b44f76a
mergwyn <gary@theclarkhome.com>
2020-11-28 08:59:08 +0000

    Fix path

--- site-modules/profile/manifests/platform/baseline/debian/logrotate.pp
+++ site-modules/profile/manifests/platform/baseline/debian/logrotate.pp
@@ -5,7 +5,7 @@ class profile::platform::baseline::debian::logrotate {
 
   $confdir = '/etc/logrotate.d'
   exec { 'remove compression':
-    path    => 'default path',
+    path    => $facts['path'],
     onlyif  => "grep -ql '[[:space:]]compress' ${confdir}/*",
     command => "sed -i -e '/[[:space:]]compress/d'  \$(grep -l '[[:space:]]compress' ${confdir}/*)",
   }
