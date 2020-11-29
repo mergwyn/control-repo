ef31e32063b498512047e3bd7085211f8587d58a
mergwyn <gary@theclarkhome.com>
2020-11-29 13:19:55 +0000

    Use incldue rather than package

--- site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
+++ site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
@@ -9,7 +9,7 @@ class profile::platform::baseline::debian::sysctl {
 
   case $facts['virtual'] {
     'physical': {
-      package { 'sysctl': }
+      include systctl
       sysctl::configuration { 'vm.swappiness':  value => '1', }
       sysctl::configuration { 'vm.dirty_ratio': value => '15', }
     }
