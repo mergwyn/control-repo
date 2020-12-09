86f397b7dda98abd2cb4dbebcedec27339cb98bc
mergwyn <gary@theclarkhome.com>
2020-12-09 17:27:39 +0000

    Work around voxpupuli/puppet-nginx#1372

--- site-modules/profile/manifests/app/nginx.pp
+++ site-modules/profile/manifests/app/nginx.pp
@@ -8,16 +8,21 @@ class profile::app::nginx {
 
   $service_name = 'nginx.service'
   $service = 'nginx.service'
+  # TODO WORKAROUND 1: https://github.com/voxpupuli/puppet-nginx/issues/1372#issuecomment-611736052
+  #
+  # This implements Workaround 1 for nginx failing to start on boot because /run/nginx
+  # doesn't exist. Once that issue has been resolved, we should remove this
   systemd::dropin_file { 'nginx-runtime.conf':
       unit    => $service_name,
       content => @("EOT"/),
                  [Service]
                  RuntimeDirectory=nginx
-                 ExecStartPre=-/bin/chmod -R 755 /run/nginx
                  | EOT
   } ~> service { $service_name:
     ensure => 'running',
   }
-
+  # TODO WORKAROUND 2: Make sure puppet keeps the file mode to 0700 for /run/nginx
+  File <| title == '/run/nginx/client_body_temp' or title == '/run/nginx/proxy_temp' |> {
+    mode => '0700',
+  }
 }
-
