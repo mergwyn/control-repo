5adeaa48ded09d91ba24b4a45d871f3c75986401
mergwyn <gary@theclarkhome.com>
2020-12-23 16:24:05 +0000

    rename zoomus to zoom

--- site-modules/profile/manifests/platform/baseline/darwin/packages.pp
+++ site-modules/profile/manifests/platform/baseline/darwin/packages.pp
@@ -3,26 +3,13 @@
 class profile::platform::baseline::darwin::packages {
 
 
-# packages to be installed
   Package {
     provider => homebrew,
     ensure   => latest,
   }
 
-  #package { 'amazon-music': }
-  package { 'git': }
-  package { 'firefox': }
-  package { 'aerial': }
-  package { 'skype': }
-  package { 'zoomus': }
-  package { 'vlc': }
-  package { 'keeweb': }
-  package { 'unison': }
-  package { 'puppetlabs/puppet/puppet-agent-6': }
-  package { 'homebrew/cask-drivers/sonos':      }
-  package { 'plex': }
-
-
+# packages to be removed
+  package { 'zoomus':                 ensure => absent }
   package { 'dropbox':                ensure => absent }
   package { 'google-chrome':          ensure => absent }
   package { 'amazon-photos':          ensure => absent }
@@ -30,5 +17,20 @@ class profile::platform::baseline::darwin::packages {
   package { 'onedrive':               ensure => absent }
   package { 'profilecreator':         ensure => absent }
   package { 'plex-media-server':      ensure => absent }
+
+# packages to be installed
+  #package { 'amazon-music': }
+  package { 'git': }
+  package { 'firefox': }
+  package { 'aerial': }
+  package { 'skype': }
+  package { 'zoom': }
+  package { 'vlc': }
+  package { 'keeweb': }
+  package { 'unison': }
+  package { 'puppetlabs/puppet/puppet-agent-6': }
+  package { 'homebrew/cask-drivers/sonos':      }
+  package { 'plex': }
+
 
 }
