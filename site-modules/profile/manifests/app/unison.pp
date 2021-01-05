c7765f361c49eab355582e2ebd069f7eab384847
mergwyn <gary@theclarkhome.com>
2021-01-05 11:09:55 +0000

    Add ensur> file

--- site-modules/profile/manifests/app/unison.pp
+++ site-modules/profile/manifests/app/unison.pp
@@ -4,41 +4,46 @@ class profile::app::unison {
 
   case $facts['os']['family'] {
     'Darwin': {
-      file { '/usr/local/share/com.theclarkhome.prefsync.plist':
-        source => 'puppet:///modules/profile/mac/com.theclarkhome.prefsync.plist',
-      }
-      file { '/usr/local/share/com.theclarkhome.logoutwatcher.plist':
-        source => 'puppet:///modules/profile/mac/com.theclarkhome.logoutwatcher.plist',
-      }
-      file { '/usr/local/share/Home.prf':
-        source => 'puppet:///modules/profile/mac/Home.prf',
-      }
-      file { '/usr/local/share/default.prf':
-        source => 'puppet:///modules/profile/mac/default.prf',
-      }
-      file { '/usr/local/share/Preferences.prf':
-        source => 'puppet:///modules/profile/mac/Preferences.prf',
-      }
-      file { '/usr/local/share/common.prf':
-        source => 'puppet:///modules/profile/mac/common.prf',
-      }
-      file { '/usr/local/bin/UnisonHomeSync.sh':
-        source => 'puppet:///modules/profile/mac/UnisonHomeSync.sh',
-        mode   => '0775',
-      }
-      file { '/usr/local/bin/SetupHomeSync.sh':
-        source => 'puppet:///modules/profile/mac/SetupHomeSync.sh',
-        mode   => '0775',
-      }
-      file { '/usr/local/bin/logoutwatcher.sh':
-        source => 'puppet:///modules/profile/mac/logoutwatcher.sh',
-        mode   => '0775',
-      }
-      file { '/etc/newsyslog.d/unison.conf':
-        source => 'puppet:///modules/profile/mac/unison.conf',
-      }
-      file { '/Library/Logs/unison.log':
-        mode => '0666',
+      file {
+        default:
+          ensure => file,
+        ;
+        '/usr/local/share/com.theclarkhome.prefsync.plist':
+          source => 'puppet:///modules/profile/mac/com.theclarkhome.prefsync.plist',
+        ;
+        '/usr/local/share/com.theclarkhome.logoutwatcher.plist':
+          source => 'puppet:///modules/profile/mac/com.theclarkhome.logoutwatcher.plist',
+        ;
+        '/usr/local/share/Home.prf':
+          source => 'puppet:///modules/profile/mac/Home.prf',
+        ;
+        '/usr/local/share/default.prf':
+          source => 'puppet:///modules/profile/mac/default.prf',
+        ;
+        '/usr/local/share/Preferences.prf':
+          source => 'puppet:///modules/profile/mac/Preferences.prf',
+        ;
+        '/usr/local/share/common.prf':
+          source => 'puppet:///modules/profile/mac/common.prf',
+        ;
+        '/usr/local/bin/UnisonHomeSync.sh':
+          source => 'puppet:///modules/profile/mac/UnisonHomeSync.sh',
+          mode   => '0775',
+        ;
+        '/usr/local/bin/SetupHomeSync.sh':
+          source => 'puppet:///modules/profile/mac/SetupHomeSync.sh',
+          mode   => '0775',
+        ;
+        '/usr/local/bin/logoutwatcher.sh':
+          source => 'puppet:///modules/profile/mac/logoutwatcher.sh',
+          mode   => '0775',
+        ;
+        '/etc/newsyslog.d/unison.conf':
+          source => 'puppet:///modules/profile/mac/unison.conf',
+        ;
+        '/Library/Logs/unison.log':
+          mode => '0666',
+        ;
       }
     }
     'Debian': {
