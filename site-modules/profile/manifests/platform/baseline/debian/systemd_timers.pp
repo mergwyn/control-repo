eb1fccc965c47ee8f54d0f69204ed025ce0e6155
mergwyn <gary@theclarkhome.com>
2021-01-19 15:08:32 +0000

    Add basic cron like timers for systemd

---
+++ site-modules/profile/manifests/platform/baseline/debian/systemd_timers.pp
@@ -0,0 +1,48 @@
+# @summary cron style standard timers for systemd
+#
+
+class profile::platform::baseline::debian::systemd_timers {
+
+  if $facts['os']['family'] != 'Debian' {
+    fail("${name} can only be called on Debian")
+  }
+  systemd::timer{'hourly.timer':
+    active        => true,
+    enable        => true,
+    timer_content => @("EOT"/),
+                     [Timer]
+                     OnCalendar=hourly
+                     RandomizedDelaySec=30m
+                     | EOT
+  }
+
+  systemd::timer{'daily.timer':
+    active        => true,
+    enable        => true,
+    timer_content => @("EOT"/),
+                     [Timer]
+                     OnCalendar=daily
+                     RandomizedDelaySec=30m
+                     | EOT
+  }
+
+  systemd::timer{'weekly.timer':
+    active        => true,
+    enable        => true,
+    timer_content => @("EOT"/),
+                     [Timer]
+                     OnCalendar=weekly
+                     RandomizedDelaySec=30m
+                     | EOT
+  }
+
+  systemd::timer{'monthly.timer':
+    active        => true,
+    enable        => true,
+    timer_content => @("EOT"/),
+                     [Timer]
+                     OnCalendar=monthly
+                     RandomizedDelaySec=30m
+                     | EOT
+  }
+}
