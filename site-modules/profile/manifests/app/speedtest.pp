03213167a224e9da712623a047f1a12466c97001
mergwyn <gary@theclarkhome.com>
2021-01-20 16:09:12 +0000

    Add support for speedtest

---
+++ site-modules/profile/manifests/app/speedtest.pp
@@ -0,0 +1,25 @@
+# @summary gather speedtest data into abbix
+
+class profile::app::speedtest {
+
+  package { 'speedtest-cli': }
+
+  $datafile = '/opt/puppetlabs/puppet/cache/speedtest.json'
+  $speedtest = '/usr/bin/speedtest-cli'
+
+  cron::job {'speedtest':
+    environment => [ 'PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"'],
+    command     => "test -x ${speedtest} && ${speedtest} --json > ${datafile}",
+    minute      => '53',
+    hour        => '*/12',
+  }
+
+  include sudo
+  sudo::conf { 'speedtest':
+    content => 'zabbix  ALL=(root)      NOPASSWD:       /bin/cat'
+  }
+
+  zabbix::userparameters { 'speedtest':
+    content => "UserParameter=speedtest.data.[*],sudo cat ${datafile}\n"
+  }
+}
--- site-modules/role/manifests/iptv_server.pp
+++ site-modules/role/manifests/iptv_server.pp
@@ -3,6 +3,7 @@ class role::iptv_server {
   include profile::platform::baseline  # All roles should have the base profile
   include profile::app::sssd
   include profile::app::iptv
+  include profile::app::speedtest
   include profile::app::zabbix::agent
   include profile::app::backuppc::client
 }
