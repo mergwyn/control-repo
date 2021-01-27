a935763b390208877bba6f824c392c85834035b0
mergwyn <gary@theclarkhome.com>
2021-01-27 12:18:33 +0000

    Add vpnless router

--- site-modules/profile/manifests/infrastructure/router.pp
+++ site-modules/profile/manifests/infrastructure/router.pp
@@ -2,15 +2,88 @@
 
 class profile::infrastructure::router (
   Boolean $iptv = true,
-  Enum['openvpn','nordvpn'] $type = 'openvpn',
+  Enum['openvpn','nordvpn','none'] $type = 'none',
 ) {
 
+  $aptpackages = [
+    'ufw',
+  ]
+  package { $aptpackages: ensure   => present, }
+
+
+  # Enable UFW
+  exec { 'ufw-enable':
+    command => 'ufw --force enable',
+    unless  => 'ufw status | grep -q "Status: active"',
+    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+    require => Package['ufw']
+  }
+
+  # Define service
+  service { 'ufw':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['ufw']
+  }
+
+  # Disable IPv6
+  shellvar { 'IPV6':
+    ensure    => present,
+    value     => 'no',
+    target    => '/etc/default/ufw',
+    uncomment => true,
+    notify    => Service['ufw'],
+    require   => Package['ufw']
+  }
+
+  # Set forward policy
+  shellvar { 'DEFAULT_FORWARD_POLICY':
+    ensure    => present,
+    value     => 'ACCEPT',
+    target    => '/etc/default/ufw',
+    uncomment => true,
+    notify    => Service['ufw'],
+    require   => Package['ufw']
+  }
+
+# Add forward rule
+  shellvar { 'net/ipv4/ip_forward':
+    ensure    => present,
+    target    => '/etc/ufw/sysctl.conf',
+    value     => '1',
+    uncomment => true,
+  }
+
+#TODO
+  $nat_rule = "
+# nat Table rules
+*nat
+:POSTROUTING ACCEPT [0:0]
+ 
+# Forward traffic from eth1 through eth0.
+-A POSTROUTING -s 10.0.125.0/24 -o eth1 -j MASQUERADE
+ 
+# don't delete the 'COMMIT' line or these nat table rules won't be processed
+COMMIT
+"
+  file_line { 'ufw-ipv6':
+    line    => $nat_rule,
+    match   => '^IPV6=',
+    path    => '/etc//ufw/before.rules',
+    notify  => Service['ufw'],
+    require => Package['ufw']
+  }
+
+
+
   case $type  {
-    'openvpn': { include profile::infrastructure::router::openvpn }
+    'none':    { }
+    'nordvpn': { include profile::infrastructure::router::nordvpn }
     'nordvpn': { include profile::infrastructure::router::nordvpn }
     default:   { fail("${type} is not supported") }
   }
 
+# TODO remove this
   if $iptv {
     include cron
     include profile::app::scripts
