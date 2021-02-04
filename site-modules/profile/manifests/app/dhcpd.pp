ab2c1f5067a17a6c7c7c19a1d270184821c5762c
mergwyn <gary@theclarkhome.com>
2021-02-04 12:05:20 +0000

    Reinstate hardcoded shared network with 10.58.0 network

--- site-modules/profile/manifests/app/dhcpd.pp
+++ site-modules/profile/manifests/app/dhcpd.pp
@@ -20,7 +20,7 @@ class profile::app::dhcpd (
     dnssearchdomains   => lookup('defaults::dns::search'),
     default_lease_time => 14400,
     extra_config       => [
-      #'include "/etc/dhcp/dhcpd.shared";',
+      'include "/etc/dhcp/dhcpd.shared";',
       'include "/etc/dhcp/dhcpd.samba_ddns";',
     ],
   }
@@ -42,24 +42,28 @@ class profile::app::dhcpd (
                     }
 
                     option subnet-mask 255.255.255.0;
-                    option routers 192.168.11.254;
+                    option routers 192.168.11.1;
+                  }
+                  subnet 10.58.0.0 netmask 255.255.0.0 {
+                    pool
+                    {
+                      failover peer "dhcp-failover";
+                      deny dynamic bootp clients;
+                      range 10.58.0.100 10.58.0.199;
+                    }
+
+                    option subnet-mask 255.255.0.0;
+                    option routers 10.58.0.1;
                   }
                 }
                 | EOT
   }
 
-  dhcp::pool { lookup('defaults::network'):
-    network  => lookup('defaults::network'),
-    mask     => '255.255.255.0',
-    range    => [ "${lookup('defaults::subnet')}.100 ${lookup('defaults::subnet')}.199" ],
-    gateway  => lookup('defaults::gateway'),
-    failover => 'dhcp-failover',
-  }
-#  dhcp::pool { '10.58.0.0':
-#    network  => '10.58.0.0',
-#    mask     => '255.255.0.0',
-#    range    => [ '10.58.0.100', '10.58.0.199' ],
-#    gateway  => '10.58.10.1',
+#  dhcp::pool { lookup('defaults::network'):
+#    network  => lookup('defaults::network'),
+#    mask     => '255.255.255.0',
+#    range    => [ "${lookup('defaults::subnet')}.100 ${lookup('defaults::subnet')}.199" ],
+#    gateway  => lookup('defaults::gateway'),
 #    failover => 'dhcp-failover',
 #  }
   if ($role and $peer_address) {
@@ -77,10 +81,10 @@ class profile::app::dhcpd (
   #dhcp::host { 'romeo':   mac => '00:16:3e:fb:dc:5e', ip => '192.168.11.250', }
 
 # Hosts that just need names,
-  dhcp::host { 'switch1':               mac => '00:8e:f2:59:c7:98' }
-  dhcp::host { 'switch2':               mac => 'a0:40:a0:71:7e:ce' }
-  dhcp::host { 'DELLA3F95F':            mac => '08:00:37:a3:f9:5f' }
-  dhcp::host { 's685ip':                mac => '00:01:e3:9a:f9:c1' }
+  #dhcp::host { 'switch1':               mac => '00:8e:f2:59:c7:98' }
+  #dhcp::host { 'switch2':               mac => 'a0:40:a0:71:7e:ce' }
+  #dhcp::host { 'DELLA3F95F':            mac => '08:00:37:a3:f9:5f' }
+  #dhcp::host { 's685ip':                mac => '00:01:e3:9a:f9:c1' }
   dhcp::host { 'humax':                 mac => '80:1f:02:21:a1:74' }
   dhcp::host { 'lounge-sonos-playbar':  mac => '00:0e:58:bc:b4:dc' }
   dhcp::host { 'lounge-sonos-play1-ls': mac => '00:0e:58:c9:f0:9a' }
