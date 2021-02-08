a03fe91c98c1a2dbca599f406b83b85dd10cf227
mergwyn <gary@theclarkhome.com>
2021-02-08 15:58:45 +0000

    Refine rules

--- site-modules/profile/manifests/infrastructure/router.pp
+++ site-modules/profile/manifests/infrastructure/router.pp
@@ -14,26 +14,30 @@ class profile::infrastructure::router (
   firewall {'100 FORWARD for eth0':
     chain   => 'FORWARD',
     source  => '10.58.0.0/16',
+    proto   => 'all',
     iniface => 'eth0',
-    jump    => 'ACCEPT',
+    action  => 'accept',
   }
   firewall {'110 FORWARD for eth0.1':
     chain   => 'FORWARD',
     source  => '192.168.11.0/24',
+    proto   => 'all',
     iniface => 'eth0.1',
-    jump    => 'ACCEPT',
+    action  => 'accept',
   }
   firewall {'200 nat for eth0':
-    table   => 'nat',
-    chain   => 'POSTROUTING',
-    iniface => 'eth0',
-    jump    => 'MASQUERADE',
+    table    => 'nat',
+    chain    => 'POSTROUTING',
+    outiface => 'eth0',
+    proto    => 'all',
+    jump     => 'MASQUERADE',
   }
   firewall {'210 nat for eth0.1':
-    table   => 'nat',
-    chain   => 'POSTROUTING',
-    iniface => 'eth0.1',
-    jump    => 'MASQUERADE',
+    table    => 'nat',
+    chain    => 'POSTROUTING',
+    outiface => 'eth0.1',
+    proto    => 'all',
+    jump     => 'MASQUERADE',
   }
 
   case $type  {
