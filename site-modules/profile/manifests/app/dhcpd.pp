bc385f04a3d08f8db5c6bc524a07c818833d568c
mergwyn <gary@theclarkhome.com>
2021-02-10 10:54:30 +0000

    Add 10.58 subnet

--- site-modules/profile/manifests/app/dhcpd.pp
+++ site-modules/profile/manifests/app/dhcpd.pp
@@ -44,17 +44,17 @@ class profile::app::dhcpd (
                     option subnet-mask 255.255.255.0;
                     option routers 192.168.11.1;
                   }
-#                  subnet 10.58.0.0 netmask 255.255.0.0 {
-#                    pool
-#                    {
-#                      failover peer "dhcp-failover";
-#                      deny dynamic bootp clients;
-#                      range 10.58.0.100 10.58.0.199;
-#                    }
-#
-#                    option subnet-mask 255.255.0.0;
-#                    option routers 10.58.0.1 10.58.0.2;
-#                  }
+                  subnet 10.58.0.0 netmask 255.255.0.0 {
+                    pool
+                    {
+                      failover peer "dhcp-failover";
+                      deny dynamic bootp clients;
+                      range 10.58.0.100 10.58.0.199;
+                    }
+
+                    option subnet-mask 255.255.0.0;
+                    option routers 10.58.0.1;
+                  }
                 }
                 | EOT
   }
