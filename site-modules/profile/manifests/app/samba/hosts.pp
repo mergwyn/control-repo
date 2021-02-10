09fb6c2d5d7da6331405b6bf0237582d07c22b10
mergwyn <gary@theclarkhome.com>
2021-02-10 12:22:15 +0000

    remove status host dns entries

--- site-modules/profile/manifests/app/samba/hosts.pp
+++ site-modules/profile/manifests/app/samba/hosts.pp
@@ -4,9 +4,9 @@ class profile::app::samba::hosts(
   $subnet =  lookup('defaults::subnet'),
 ) {
 # TODO read from hiera or exported resource?
-  profile::app::samba::dnsentry { 'romeo':   ipaddress => "${subnet}.1" }
-  profile::app::samba::dnsentry { 'golf':    ipaddress => "${subnet}.11" }
-  profile::app::samba::dnsentry { 'foxtrot': ipaddress => "${subnet}.12" }
-  profile::app::samba::dnsentry { 'juliet':  ipaddress => "${subnet}.251" }
-  profile::app::samba::dnsentry { 'victor':  ipaddress => "${subnet}.252" }
+#  profile::app::samba::dnsentry { 'romeo':   ipaddress => "${subnet}.1" }
+#  profile::app::samba::dnsentry { 'golf':    ipaddress => "${subnet}.11" }
+#  profile::app::samba::dnsentry { 'foxtrot': ipaddress => "${subnet}.12" }
+#  profile::app::samba::dnsentry { 'juliet':  ipaddress => "${subnet}.251" }
+#  profile::app::samba::dnsentry { 'victor':  ipaddress => "${subnet}.252" }
 }
