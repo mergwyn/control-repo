a3483a95dfbbacfc809a244fb36d2358a940a8ea
mergwyn <gary@theclarkhome.com>
2021-03-03 15:38:04 +0000

    Remove iptables config

--- site-modules/profile/manifests/infrastructure/router.pp
+++ site-modules/profile/manifests/infrastructure/router.pp
@@ -5,40 +5,40 @@ class profile::infrastructure::router (
   Enum['openvpn','nordvpn','none'] $type = 'none',
 ) {
 
-  include sysctl
-  sysctl::configuration { 'net.ipv4.ip_forward':  value => '1', }
-
-  include firewall
-  resources { 'firewall': purge => true, }
-
-  firewall {'100 FORWARD for eth0':
-    chain   => 'FORWARD',
-    source  => '10.58.0.0/16',
-    proto   => 'all',
-    iniface => 'eth0',
-    action  => 'accept',
-  }
-  firewall {'110 FORWARD for eth0.1':
-    chain   => 'FORWARD',
-    source  => '192.168.11.0/24',
-    proto   => 'all',
-    iniface => 'eth0.1',
-    action  => 'accept',
-  }
-  firewall {'200 nat for eth0':
-    table    => 'nat',
-    chain    => 'POSTROUTING',
-    outiface => 'eth0',
-    proto    => 'all',
-    jump     => 'MASQUERADE',
-  }
-  firewall {'210 nat for eth0.1':
-    table    => 'nat',
-    chain    => 'POSTROUTING',
-    outiface => 'eth0.1',
-    proto    => 'all',
-    jump     => 'MASQUERADE',
-  }
+#  include sysctl
+#  sysctl::configuration { 'net.ipv4.ip_forward':  value => '1', }
+#
+#  include firewall
+#  resources { 'firewall': purge => true, }
+#
+#  firewall {'100 FORWARD for eth0':
+#    chain   => 'FORWARD',
+#    source  => '10.58.0.0/16',
+#    proto   => 'all',
+#    iniface => 'eth0',
+#    action  => 'accept',
+#  }
+#  firewall {'110 FORWARD for eth0.1':
+#    chain   => 'FORWARD',
+#    source  => '192.168.11.0/24',
+#    proto   => 'all',
+#    iniface => 'eth0.1',
+#    action  => 'accept',
+#  }
+#  firewall {'200 nat for eth0':
+#    table    => 'nat',
+#    chain    => 'POSTROUTING',
+#    outiface => 'eth0',
+#    proto    => 'all',
+#    jump     => 'MASQUERADE',
+#  }
+#  firewall {'210 nat for eth0.1':
+#    table    => 'nat',
+#    chain    => 'POSTROUTING',
+#    outiface => 'eth0.1',
+#    proto    => 'all',
+#    jump     => 'MASQUERADE',
+#  }
 
   case $type  {
     'none':    { }
