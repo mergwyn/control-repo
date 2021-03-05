85a820d4f0ea3387f0c780b1a99701433c6219c1
mergwyn <gary@theclarkhome.com>
2021-03-05 11:34:20 +0000

    Change zones

--- site-modules/profile/manifests/infrastructure/router.pp
+++ site-modules/profile/manifests/infrastructure/router.pp
@@ -1,50 +1,83 @@
 #
 
+# In order from least trusted to most trusted, the predefined zones within firewalld are:
+
+# * drop:
+#   The lowest level of trust. All incoming connections are dropped
+#   without reply and only outgoing connections are possible.
+# * block:
+#   Similar to the above, but instead of simply dropping connections,
+#   incoming requests are rejected with an icmp-host-prohibited or
+#   icmp6-adm-prohibited message.
+# * public:
+#   Represents public, untrusted networks. You don’t trust other
+#   computers but may allow selected incoming connections on a
+#   case-by-case basis.
+# * external:
+#   External networks in the event that you are using the firewall as
+#   your gateway. It is configured for NAT masquerading so that your
+#   internal network remains private but reachable.
+# * internal:
+#   The other side of the external zone, used for the internal portion of
+#   a gateway. The computers are fairly trustworthy and some additional
+#   services are available.
+# * dmz:
+#   Used for computers located in a DMZ (isolated computers that will
+#   not have access to the rest of your network). Only certain incoming
+#   connections are allowed.
+# * work:
+#   Used for work machines. Trust most of the computers in the network.
+#   A few more services might be allowed.
+# * home:
+#   A home environment. It generally implies that you trust most of the
+#   other computers and that a few more services will be accepted.
+# * trusted:
+#   Trust all of the machines in the network. The most open of the
+#   available options and should be used sparingly.
+
 class profile::infrastructure::router (
   Boolean $iptv = true,
   Enum['privatvpn','none'] $type = 'none',
 ) {
 
   class {'firewalld':
-    default_zone         => 'internal',
-    default_service_zone => 'public',
-    default_port_zone    => 'public',
+# TODO add hashes for configuration
   }
 
-# Attach interface to internal zone
-  firewalld_zone {'internal':
+# Attach interface to home zone
+  firewalld_zone {'home':
     interfaces => [$facts['networking']['primary']],
     masquerade => true,
   }
 
 # Lockdown services
-  firewalld_service {'Disallow SSH in the public Zone':
+  firewalld_service {'Disallow SSH in the external Zone':
     ensure  => absent,
-    zone    => 'public',
+    zone    => 'external',
     service => 'ssh',
   }
-  firewalld_service {'Disallow dhcpv6-client in the public Zone':
+  firewalld_service {'Disallow dhcpv6-client in the external Zone':
     ensure  => absent,
-    zone    => 'public',
+    zone    => 'external',
     service => 'dhcpv6-client',
   }
 
 # TODO move this to app modules and sonsume exported resources
-  firewalld_rich_rule {'Allow plex to plexserver from public zone':
+  firewalld_rich_rule {'Allow plex to plexserver from external zone':
     ensure  => present,
-    zone    => 'public',
+    zone    => 'external',
     dest    => '10.58.0.30',
     service => 'plex',
     action  => 'accept',
   }
-  firewalld_service {'Allow http in the public Zone':
+  firewalld_service {'Allow http in the external Zone':
     ensure  => present,
-    zone    => 'public',
+    zone    => 'external',
     service => 'http',
   }
-  firewalld_service {'Allow https in the public Zone':
+  firewalld_service {'Allow https in the external Zone':
     ensure  => present,
-    zone    => 'public',
+    zone    => 'external',
     service => 'https',
   }
 
