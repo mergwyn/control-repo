8976c13b25abccd928947fd9106b70599eed3e04
mergwyn <gary@theclarkhome.com>
2021-03-08 15:25:12 +0000

    Disable ipv6

--- site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
+++ site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
@@ -1,7 +1,9 @@
 # @summary sysctl settings
 #
 
-class profile::platform::baseline::debian::sysctl {
+class profile::platform::baseline::debian::sysctl (
+  Boolean $ipv6 = false
+) {
 
   if $facts['os']['family'] != 'Debian' {
     fail("${name} can only be called on Debian")
@@ -9,8 +11,11 @@ class profile::platform::baseline::debian::sysctl {
 
   case $facts['virtual'] {
     'physical': {
-      sysctl{ 'vm.swappiness':  ensure => present, value => '1', }
-      sysctl{ 'vm.dirty_ratio': ensure => present, value => '15', }
+      sysctl{ 'vm.swappiness':                      ensure => present, value => '1', }
+      sysctl{ 'vm.dirty_ratio':                     ensure => present, value => '15', }
+      sysctl{ 'net.ipv6.conf.all.disable_ipv6':     ensure => present, value => $ipv6, }
+      sysctl{ 'net.ipv6.conf.default.disable_ipv6': ensure => present, value => $ipv6, }
+      sysctl{ 'net.ipv6.conf.lo.disable_ipv6':      ensure => present, value => $ipv6, }
     }
     default: {
     }
