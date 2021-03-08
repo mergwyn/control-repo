a9bc6fffbdefd10b702c41589be8a556e1d28555
mergwyn <gary@theclarkhome.com>
2021-03-08 16:02:53 +0000

    Initial refactoring of router classes in app::openvpn

---
+++ site-modules/profile/manifests/app/openvpn.pp
@@ -0,0 +1,159 @@
+# @summary openvpn ttunnel
+#
+# In order from least trusted to most trusted, the predefined zones within firewalld are:
+#
+# * drop:
+#   The lowest level of trust. All incoming connections are dropped
+#   without reply and only outgoing connections are possible.
+# * block:
+#   Similar to the above, but instead of simply dropping connections,
+#   incoming requests are rejected with an icmp-host-prohibited or
+#   icmp6-adm-prohibited message.
+# * public:
+#   Represents public, untrusted networks. You don’t trust other
+#   computers but may allow selected incoming connections on a
+#   case-by-case basis.
+# * external:
+#   External networks in the event that you are using the firewall as
+#   your gateway. It is configured for NAT masquerading so that your
+#   internal network remains private but reachable.
+# * internal:
+#   The other side of the external zone, used for the internal portion of
+#   a gateway. The computers are fairly trustworthy and some additional
+#   services are available.
+# * dmz:
+#   Used for computers located in a DMZ (isolated computers that will
+#   not have access to the rest of your network). Only certain incoming
+#   connections are allowed.
+# * work:
+#   Used for work machines. Trust most of the computers in the network.
+#   A few more services might be allowed.
+# * home:
+#   A home environment. It generally implies that you trust most of the
+#   other computers and that a few more services will be accepted.
+# * trusted:
+#   Trust all of the machines in the network. The most open of the
+#   available options and should be used sparingly.
+
+class profile::app::openvpn (
+  String[1] $lan = $facts['networking']['primary'],
+  String[1] $wan = 'eth1',
+  String[1] $vpn = 'tun0',
+) {
+
+  $aptpackages = [
+    'openvpn',
+    'unzip',
+    'ca-certificates',
+  ]
+  package { $aptpackages: ensure   => present, }
+
+# TODO install https://github.com/jonathanio/update-systemd-resolved
+
+###### Firewall setup
+  firewalld_zone {'external':
+    interfaces => ['tun0'],
+  }
+
+  firewalld_service {'Allow openvpn in the external Zone':
+    ensure  => present,
+    zone    => 'external',
+    service => 'openvpn',
+  }
+
+  class {'firewalld':
+    purge_direct_rules  => true,
+    purge_direct_chains => true,
+  }
+
+  firewalld_zone { 'home':
+    interfaces       => [$lan],
+    purge_rich_rules => true,
+    purge_services   => true,
+    purge_ports      => true,
+  }
+  firewalld_zone { 'public':
+    interfaces       => [$wan],
+    purge_rich_rules => true,
+    purge_services   => true,
+    purge_ports      => true,
+  }
+  firewalld_zone { 'external':
+    interfaces       => [$vpn],
+    purge_rich_rules => true,
+    purge_services   => true,
+    purge_ports      => true,
+  }
+
+# TODO move this to app modules and sonsume exported resources
+  firewalld_rich_rule {'Allow plex to plexserver from external zone':
+    ensure  => present,
+    zone    => 'external',
+    dest    => '10.58.0.30',
+    service => 'plex',
+    action  => 'accept',
+  }
+
+# home zone services and ports
+  firewalld_service {'Allow dns in the home Zone':
+    ensure  => present,
+    zone    => 'home',
+    service => 'dns',
+  }
+  firewalld_service {'Allow http in the home Zone':
+    ensure  => present,
+    zone    => 'home',
+    service => 'http',
+  }
+  firewalld_service {'Allow https in the home Zone':
+    ensure  => present,
+    service => 'https',
+  }
+  firewalld_service {'Allow ssh in the home Zone':
+    ensure  => present,
+    zone    => 'home',
+    service => 'ssh',
+  }
+
+# External zone services and ports
+  firewalld_service {'Allow openvpn in the external Zone':
+    ensure  => present,
+    zone    => 'external',
+    service => 'openvpn',
+  }
+  firewalld_service {'Allow http in the external Zone':
+    ensure  => present,
+    zone    => 'external',
+    service => 'http',
+  }
+  firewalld_service {'Allow https in the external Zone':
+    ensure  => present,
+    zone    => 'external',
+    service => 'https',
+  }
+
+
+# public zone services and ports
+  firewalld_service {'Allow openvpn in the public Zone':
+    ensure  => present,
+    zone    => 'public',
+    service => 'openvpn',
+  }
+
+###### unbound setup
+#TODO unbound setup
+#server:
+#	interface: 10.58.0.3
+#	access-control: 10.58.0.0/16 allow
+#	Access-control: 127.0.0.0/8 allow
+#	do-not-query-localhost: no
+#	val-permissive-mode: yes
+#forward-zone:
+#	name: "."
+#	forward-addr: 127.0.0.53
+
+
+##### privat vpn setup
+#TODO privat vpn setup
+
+}
---
+++ site-modules/profile/manifests/app/openvpn/privat.pp
@@ -0,0 +1,28 @@
+#
+
+class profile::app::openvpn::privat {
+
+# TODO Install confia:  wget 'http://privatevpn.com/client/PrivateVPN-TUN.zip' -O $openvpn/PrivateVPN-TUN.zipg
+# TODO get credentials
+# TODO add port 1195
+  firewalld_port {'Open port 1195 in the external Zone':
+    ensure   => 'present',
+    zone     => 'external',
+    port     => 1195,
+    protocol => 'udp',
+  }
+# TODO setup config file
+#cat <<! >$openvpn/privat.conf
+#remote uk-lon.pvdata.host 1195 udp
+#config  "${openvpn}/PrivateVPN-TUN/UDP/PrivateVPN-UK-London 1-TUN-1194.ovpn"
+#auth-user-pass '/etc/openvpn/privat.auth'
+#comp-lzo no
+#script-security 2
+#up /etc/openvpn/update-resolv-conf # use systemd version
+#down /etc/openvpn/update-resolv-conf # use systemd version
+#down-pre
+#!
+
+# TODO setup local DNS for local domain
+
+}
