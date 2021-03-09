15006e7c705662816138a2511de8257e60377d93
mergwyn <gary@theclarkhome.com>
2021-03-09 11:40:19 +0000

    remove duplicate declarations

--- site-modules/profile/manifests/app/openvpn.pp
+++ site-modules/profile/manifests/app/openvpn.pp
@@ -66,16 +66,6 @@ class profile::app::openvpn (
 # TODO install https://github.com/jonathanio/update-systemd-resolved
 
 ###### Firewall setup
-  firewalld_zone {'external':
-    interfaces => ['tun0'],
-  }
-
-  firewalld_service {'Allow openvpn in the external Zone':
-    ensure  => present,
-    zone    => 'external',
-    service => 'openvpn',
-  }
-
   class {'firewalld':
     purge_direct_rules  => true,
     purge_direct_chains => true,
