700f557fea8ccfaf3f4a14f4cd0c6322556b0146
mergwyn <gary@theclarkhome.com>
2021-03-09 14:37:49 +0000

    Disable ipv6 on all hosts

--- site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
+++ site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
@@ -2,20 +2,21 @@
 #
 
 class profile::platform::baseline::debian::sysctl (
-  Boolean $ipv6 = false
+  Boolean $disable_ipv6 = true
 ) {
 
   if $facts['os']['family'] != 'Debian' {
     fail("${name} can only be called on Debian")
   }
 
+  sysctl{ 'net.ipv6.conf.all.disable_ipv6':     ensure => present, value => $disable_ipv6, }
+  sysctl{ 'net.ipv6.conf.default.disable_ipv6': ensure => present, value => $disable_ipv6, }
+  sysctl{ 'net.ipv6.conf.lo.disable_ipv6':      ensure => present, value => $disable_ipv6, }
+
   case $facts['virtual'] {
     'physical': {
       sysctl{ 'vm.swappiness':                      ensure => present, value => '1', }
       sysctl{ 'vm.dirty_ratio':                     ensure => present, value => '15', }
-      sysctl{ 'net.ipv6.conf.all.disable_ipv6':     ensure => present, value => $ipv6, }
-      sysctl{ 'net.ipv6.conf.default.disable_ipv6': ensure => present, value => $ipv6, }
-      sysctl{ 'net.ipv6.conf.lo.disable_ipv6':      ensure => present, value => $ipv6, }
     }
     default: {
     }
