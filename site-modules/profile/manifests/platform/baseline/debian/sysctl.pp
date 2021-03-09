2e27ef4b373db7963f0cd52f04ebfdafc09c59f2
mergwyn <gary@theclarkhome.com>
2021-03-09 14:43:52 +0000

    Revert to physical only

--- site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
+++ site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
@@ -9,14 +9,13 @@ class profile::platform::baseline::debian::sysctl (
     fail("${name} can only be called on Debian")
   }
 
-  sysctl{ 'net.ipv6.conf.all.disable_ipv6':     ensure => present, value => $disable_ipv6, }
-  sysctl{ 'net.ipv6.conf.default.disable_ipv6': ensure => present, value => $disable_ipv6, }
-  sysctl{ 'net.ipv6.conf.lo.disable_ipv6':      ensure => present, value => $disable_ipv6, }
-
   case $facts['virtual'] {
     'physical': {
       sysctl{ 'vm.swappiness':                      ensure => present, value => '1', }
       sysctl{ 'vm.dirty_ratio':                     ensure => present, value => '15', }
+      sysctl{ 'net.ipv6.conf.all.disable_ipv6':     ensure => present, value => $disable_ipv6, }
+      sysctl{ 'net.ipv6.conf.default.disable_ipv6': ensure => present, value => $disable_ipv6, }
+      sysctl{ 'net.ipv6.conf.lo.disable_ipv6':      ensure => present, value => $disable_ipv6, }
     }
     default: {
     }
