37d5bdd30df402471cf3cad4f32b1cc4222ff67f
mergwyn <gary@theclarkhome.com>
2021-03-09 13:34:56 +0000

    switch to using iteration

--- site-modules/profile/manifests/app/openvpn/forwards.pp
+++ site-modules/profile/manifests/app/openvpn/forwards.pp
@@ -6,41 +6,20 @@ class profile::app::openvpn::forwards {
   $proxy = '10.58.0.10'
 
 # TODO move this to app modules and sonsume exported resources
-  firewalld_rich_rule {'Forward plex from external zone':
-    ensure  => present,
-    zone    => 'external',
-    dest    => $plex,
-    service => 'plex',
-    action  => 'accept',
-  }
-
-  firewalld_rich_rule {'Forward transmission-client from external zone':
-    ensure  => present,
-    zone    => 'external',
-    dest    => $plex,
-    service => 'transmission-client',
-    action  => 'accept',
-  }
-
-  firewalld_rich_rule {'Forward http from external zone':
-    ensure  => present,
-    zone    => 'external',
-    dest    => $proxy,
-    service => 'http',
-    action  => 'accept',
-  }
-
-  firewalld_rich_rule {'Forward https from external zone':
-    ensure  => present,
-    zone    => 'external',
-    dest    => $proxy,
-    service => 'https',
-    action  => 'accept',
+  [ 'plex', 'transmission-client' ].each |String $service| {
+    firewalld_rich_rule {"Forward ${service} from external zone to $plex":
+      ensure  => present,
+      zone    => 'external',
+      dest    => $plex,
+      service => $service,
+      action  => 'accept',
+    }
   }
 
   #add_port_forward sonarr_8989 vpnfirewall 8989 $proxy 8989
   #add_port_forward radarr_7878 vpnfirewall 7878 $proxy 7878
   #add_port_forward sab_8080 vpnfirewall 8080 $proxy 8080
+  #add_port_forward http vpnfirewall 80 $proxy 443
   #add_port_forward https vpnfirewall 443 $proxy 443
 
 }
