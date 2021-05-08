d98eaad715e3e4048ce850cfe2b3af395f849e38
mergwyn <gary@theclarkhome.com>
2021-05-08 11:53:58 +0100

    Add iteration of vpn server array and fix lint error

--- site-modules/profile/manifests/app/keepalived.pp
+++ site-modules/profile/manifests/app/keepalived.pp
@@ -25,41 +25,32 @@ class profile::app::keepalived (
     track_interface   => [ $wan, $vpn], # optional, monitor these interfaces.
   }
 
+# Add virtual server for DNS
   keepalived::lvs::virtual_server { 'VPN_DNS':
-    ip_address          => $v_ip,
-    port                => 53,
-    delay_loop          => 6,
-    ha_suspend          => true,
-    lb_algo             => 'wrr',
-    lb_kind             => 'DR',
+    ip_address => $v_ip,
+    port       => 53,
+    delay_loop => 6,
+    ha_suspend => true,
+    lb_algo    => 'wrr',
+    lb_kind    => 'DR',
     # TODO remove? persistence_timeout => 0,
-    protocol            => 'TCP'
+    protocol   => 'TCP'
   }
 
-  keepalived::lvs::real_server { 'VPN_DNS_3':
-    virtual_server => 'VPN_DNS',
-    ip_address     => '10.58.0.3',
-    port           => 53,
-    options        => {
-      weight      => 1,
-      notify_down => "/sbin/ipvsadm -d -u ${v_ip}:53 -r 10.0.0.3:53",
-      notify_up   => "/sbin/ipvsadm -a -u ${v_ip}:53 -r 10.0.0.3:53 -g -w 1",
-      'TCP_CHECK' => {
-        connection_timeout => '3',
-      }
-    }
-  }
+  $virtual_servers = lookup('defaults::vpn::servers')
 
-  keepalived::lvs::real_server { 'VPN_DNS_4':
-    virtual_server => 'VPN_DNS',
-    ip_address     => '10.58.0.4',
-    port           => 53,
-    options        => {
-      weight      => 1,
-      notify_down => "/sbin/ipvsadm -d -u ${v_ip}:53 -r 10.0.0.4:53",
-      notify_up   => "/sbin/ipvsadm -a -u ${v_ip}:53 -r 10.0.0.4:53 -g -w 1",
-      'TCP_CHECK' => {
-        connection_timeout => '3',
+  $virtual_servers.each |Integer $index, String $real_ip| {
+    keepalived::lvs::real_server { "VPN_DNS_${index}":
+      virtual_server => 'VPN_DNS',
+      ip_address     => $real_ip,
+      port           => 53,
+      options        => {
+        weight      => 1,
+        notify_down => "/sbin/ipvsadm -d -u ${v_ip}:53 -r ${real_ip}:53",
+        notify_up   => "/sbin/ipvsadm -a -u ${v_ip}:53 -r ${real_ip}:53 -g -w 1",
+        'TCP_CHECK' => {
+          connection_timeout => '3',
+        }
       }
     }
   }
