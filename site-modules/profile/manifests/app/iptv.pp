4d2d25cf1a2214020b187ce52bcf600772def3e1
mergwyn <gary@theclarkhome.com>
2021-05-16 12:44:22 +0100

    Comment out get-channels cron job

--- site-modules/profile/manifests/app/iptv.pp
+++ site-modules/profile/manifests/app/iptv.pp
@@ -14,21 +14,22 @@ class profile::app::iptv {
 #    admin_password => lookup('secrets::tvheadend'),
 #  }
 
-  cron::job::multiple { 'xmltv':
-    jobs        => [
+# TODO replace with timer - cron job doesn't run correctly (curl error)
+#  cron::job::multiple { 'xmltv':
+#    jobs        => [
+##      {
+##        command => "test -x ${codedir}/iptv/get-epg && ${codedir}/iptv/get-epg",
+##        minute  => 30,
+##        hour    => '*',
+##      },
 #      {
-#        command => "test -x ${codedir}/iptv/get-epg && ${codedir}/iptv/get-epg",
-#        minute  => 30,
-#        hour    => '*',
+#        command => "test -x ${codedir}/iptv/get-channels && ${codedir}/iptv/get-channels",
+#        minute  => 20,
+#        hour    => '*/6',
 #      },
-      {
-        command => "test -x ${codedir}/iptv/get-channels && ${codedir}/iptv/get-channels",
-        minute  => 20,
-        hour    => '*/6',
-      },
-    ],
-    environment => [ 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' ],
-  }
+#    ],
+#    environment => [ 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' ],
+#  }
 
   #TODO xteve?
   include profile::app::nginx::xmltv
