bb313cf87c3b94b6dc176f2023d1d4919ee4c46e
mergwyn <gary@theclarkhome.com>
2021-07-30 15:57:30 +0100

    Swicth to saz-ssh

--- site-modules/profile/manifests/platform/baseline/debian/ssh.pp
+++ site-modules/profile/manifests/platform/baseline/debian/ssh.pp
@@ -2,19 +2,59 @@
 
 class profile::platform::baseline::debian::ssh {
 
-# TODO: check defaults for clean bionic install
+# The values below have been set for an Ubuntu 20.04 distribution
+# Values may need to be changed for different versions
   class { 'ssh':
-    ssh_hostbasedauthentication       => 'yes',
-    ssh_gssapiauthentication          => 'yes',
-    ssh_gssapidelegatecredentials     => 'yes',
-    ssh_enable_ssh_keysign            => 'yes',
-
-    sshd_gssapiauthentication         => 'yes',
-    sshd_gssapicleanupcredentials     => 'yes',
-    sshd_kerberos_authentication      => 'yes',
-    sshd_hostbasedauthentication      => 'yes',
-    sshd_config_permitemptypasswords  => 'no',
-    sshd_config_print_last_log        => 'yes',
-    sshd_config_permituserenvironment => 'yes',
+    storeconfigs_enabled => false,
+    client_options       => {
+      'PasswordAuthentication'    => 'yes',
+      'PubkeyAuthentication'      => 'yes',
+      'HostbasedAuthentication'   => 'yes',
+      'IdentityFile'              => [ '~/.ssh/id_rsa', '~/.ssh/id_dsa' ],
+      'Protocol'                  => '2',
+      'HashKnownHosts'            => 'yes',
+      'GlobalKnownHostsFile'      => '/etc/ssh/ssh_known_hosts',
+      'GSSAPIAuthentication'      => 'yes',
+      'GSSAPIDelegateCredentials' => 'yes',
+      'ForwardX11Trusted'         => 'yes',
+      'UseRoaming'                => 'no',
+      'SendEnv'                   => [
+        'LANG LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES',
+        'LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT',
+        'LC_IDENTIFICATION LC_ALL',
+      ],
+      'EnableSSHKeysign'          => 'yes',
+    },
+    server_options       => {
+      'LoginGraceTime'                  => '120',
+      'PermitRootLogin'                 => 'yes',
+      'HostbasedAuthentication'         => 'yes',
+      'IgnoreUserKnownHosts'            => 'no',
+      'IgnoreRhosts'                    => 'yes',
+      'PasswordAuthentication'          => 'yes',
+      'PermitEmptyPasswords'            => 'no',
+      'ChallengeResponseAuthentication' => 'yes',
+      'KerberosAuthentication'          => 'yes',
+      'GSSAPIAuthentication'            => 'yes',
+      'GSSAPICleanupCredentials'        => 'yes',
+      'UsePAM'                          => 'yes',
+      'AcceptEnv'                       => [
+        'LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES',
+        'LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT',
+        'LC_IDENTIFICATION LC_ALL',
+      ],
+      'AllowTcpForwarding'              => 'yes',
+      'X11Forwarding'                   => 'yes',
+      'X11UseLocalhost'                 => 'yes',
+      'PrintMotd'                       => 'yes',
+      'PrintLastLog'                    => 'yes',
+      'PermitUserEnvironment'           => 'yes',
+      'ClientAliveInterval'             => '0',
+      'ClientAliveCountMax'             => '3',
+      'UseDNS'                          => 'yes',
+      'Banner'                          => 'none',
+      'Subsystem'                       => 'sftp /usr/lib/openssh/sftp-server',
+    },
   }
+
 }
