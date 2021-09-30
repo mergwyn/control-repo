7653e62bbd97fccf318a1856be434120f8694145
mergwyn <gary@theclarkhome.com>
2021-09-30 13:02:18 +0100

    Add custom zabbox template installation

--- site-modules/profile/manifests/app/zabbix/server.pp
+++ site-modules/profile/manifests/app/zabbix/server.pp
@@ -19,4 +19,17 @@ class profile::app::zabbix::server {
     database_password => hiera('secrets::mysql'),
   }
 
+  [
+    'template_app_backuppc_active.xml',
+    'template_app_easeus_todo_backup_active.xml',
+    'template_app_speedtest_active.xml',
+    'template_app_xteve_active.xml',
+    'template_app_zfs_active.xml',
+    'template_module_processes_autodiscovery_active.xml',
+  ].each |String $template| {
+    zabbix::template { $template:
+      templ_source => "puppet:///modules/profile/zabbix/aerver/templates/${template}",
+    }
+  }
+
 }
