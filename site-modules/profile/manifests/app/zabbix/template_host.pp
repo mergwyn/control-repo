377ebff37d951d41ce38b89af7c7abd6782e1f35
mergwyn <gary@theclarkhome.com>
2021-10-17 14:52:02 +0100

    Use fqdn

--- site-modules/profile/manifests/app/zabbix/template_host.pp
+++ site-modules/profile/manifests/app/zabbix/template_host.pp
@@ -1,5 +1,7 @@
 # @summary link a template to a host using the zabbix api
 #
 define profile::app::zabbix::template_host {
-  @@zabbix_template_host { "${title}@${facts['hostname']}": ensure => 'present', }
+  @@zabbix_template_host { "${name}@${trusted['certname']}":
+    ensure => present,
+  }
 }
