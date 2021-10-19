25e5bffcee8b8f61b2ffe9c2200ef5d21a0f9575
mergwyn <gary@theclarkhome.com>
2021-10-19 12:56:50 +0100

    Adjust the way webdav is setup

--- site-modules/profile/manifests/app/nginx/webdav.pp
+++ site-modules/profile/manifests/app/nginx/webdav.pp
@@ -3,7 +3,7 @@
 class profile::app::nginx::webdav{
   include profile::app::nginx
 
-  if ! nginx::resource::server[$trusted['hostname']] {
+  if ! defined(nginx::resource::server[$trusted['hostname']]) {
     nginx::resource::server { $trusted['hostname']:
       listen_port          => 80,
       use_default_location => false,
