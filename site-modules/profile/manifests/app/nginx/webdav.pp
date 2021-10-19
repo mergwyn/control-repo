99cbbd27b5bfc01c3a7a0c1029d6f3d1da40fc46
mergwyn <gary@theclarkhome.com>
2021-10-19 12:55:27 +0100

    Adjust the way webdav is setup

--- site-modules/profile/manifests/app/nginx/webdav.pp
+++ site-modules/profile/manifests/app/nginx/webdav.pp
@@ -3,25 +3,24 @@
 class profile::app::nginx::webdav{
   include profile::app::nginx
 
-  nginx::resource::server { 'webdav':
-    server_name          => [ $::facts['networking']['fqdn'] ],
-    listen_port          => 80,
-    use_default_location => false,
-    locations            => {
-      '/webdav'       => {
-        server              => 'webdav',
-        autoindex           => 'on',
-        index_files         => [ '/index.cgi' ],
-        location_alias      => '/srv/webdav',
-        location_cfg_append => {
-          auth_pam              => '"Restricted area"',
-          auth_pam_service_name => '"nginx"',
-          dav_methods           => 'PUT DELETE MKCOL COPY MOVE',
-          dav_ext_methods       => 'PROPFIND OPTIONS',
-          dav_access            => 'group:rw all:r',
-          create_full_put_path  => 'on',
-        },
-      },
+  if ! nginx::resource::server[$trusted['hostname']] {
+    nginx::resource::server { $trusted['hostname']:
+      listen_port          => 80,
+      use_default_location => false,
+    }
+  }
+  nginx::resource::location { "webdav_${trusted['hostname']}":
+    location            => '/webdav',
+    autoindex           => 'on',
+    index_files         => [ '/index.cgi' ],
+    location_alias      => '/srv/webdav',
+    location_cfg_append => {
+      auth_pam              => '"Restricted area"',
+      auth_pam_service_name => '"nginx"',
+      dav_methods           => 'PUT DELETE MKCOL COPY MOVE',
+      dav_ext_methods       => 'PROPFIND OPTIONS',
+      dav_access            => 'group:rw all:r',
+      create_full_put_path  => 'on',
     },
   }
 }
