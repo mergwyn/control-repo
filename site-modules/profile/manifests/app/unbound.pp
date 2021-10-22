d9ebe1c38bb6d101a4e3878bddb2017d1a917bcb
mergwyn <gary@theclarkhome.com>
2021-10-22 16:33:19 +0100

    Add capability net_raw

--- site-modules/profile/manifests/app/unbound.pp
+++ site-modules/profile/manifests/app/unbound.pp
@@ -7,8 +7,8 @@
 #   (virtual) ip address of the VPN gateway
 #
 class profile::app::unbound (
-  #String[1]               $lan     = 'eth0',
-  #Stdlib::IP::Address::V4 $gateway = lookup('defaults::vpn_gateway'),
+  String[1]               $lan     = 'eth0',
+  Stdlib::IP::Address::V4 $gateway = lookup('defaults::vpn_gateway'),
 ) {
 
 # TODO investigate why ip_transparent caused permission error
@@ -17,14 +17,21 @@ class profile::app::unbound (
     ensure => absent,
     unit   => 'unbound.service',
   }
+
+  include profile::platform::baseline::debian::apparmor
+  file { '/etc/apparmor.d/local/usr.sbin.unbound':
+    ensure  => file,
+    notify  => Service['unbound', 'apparmor'],
+    content => 'capability net_raw,',
+  }
+
   service { 'systemd-resolved':
     ensure => 'stopped',
     enable => false,
   }
   -> class { 'unbound':
-    #interface              => [ $gateway, $facts['networking']['interfaces'][$lan]['ip'] ],
-    interface              => [ '0.0.0.0' ],
-    interface_automatic    => true,
+    interface              => [ $gateway, $facts['networking']['interfaces'][$lan]['ip'] ],
+    interface_automatic    => false,
     access                 => [ "${lookup('defaults::cidr')}", '127.0.0.0/8' ],
     do_not_query_localhost => false,
     val_permissive_mode    => true,
