2faf02db1d15867618f8341a1352319abdb67093
mergwyn <gary@theclarkhome.com>
2021-12-08 21:14:09 +0000

    start rpc_statd

--- site-modules/profile/manifests/platform/baseline/debian/nfs_server.pp
+++ site-modules/profile/manifests/platform/baseline/debian/nfs_server.pp
@@ -2,4 +2,8 @@
 #
 class profile::platform::baseline::debian::nfs_server {
   package { 'nfs-kernel-server': }
+  ->  service {'rpc-statd':
+        ensure => running,
+        enable => true,
+      }
 }
