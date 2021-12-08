60c872e7421574058e0e8a448849e8965ff1c6e4
mergwyn <gary@theclarkhome.com>
2021-12-08 19:55:43 +0000

    Use without-password to disable root login via password

--- site-modules/profile/manifests/platform/baseline/debian/ssh.pp
+++ site-modules/profile/manifests/platform/baseline/debian/ssh.pp
@@ -27,7 +27,7 @@ class profile::platform::baseline::debian::ssh {
     },
     server_options       => {
       'LoginGraceTime'                  => '120',
-      'PermitRootLogin'                 => 'yes',
+      'PermitRootLogin'                 => 'without-password',
       'HostbasedAuthentication'         => 'yes',
       'IgnoreUserKnownHosts'            => 'no',
       'IgnoreRhosts'                    => 'yes',
