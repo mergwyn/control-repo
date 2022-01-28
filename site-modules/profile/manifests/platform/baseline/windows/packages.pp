34eb15140f04d4df7a206ee6243e26cf3895677c
mergwyn <gary@theclarkhome.com>
2022-01-28 17:48:03 +0000

    split default windows packages

--- site-modules/profile/manifests/platform/baseline/windows/packages.pp
+++ site-modules/profile/manifests/platform/baseline/windows/packages.pp
@@ -11,18 +11,8 @@ class profile::platform::baseline::windows::packages {
   }
 
   package { 'rsat': install_options => [ { '-params' => '/AD /GP /DNS', } ] }
-  package { 'wsl-ubuntu-2004': }
   package { 'chocolateygui': }
-  package { 'firefox': }
-  package { 'keeweb': }
-  package { 'kitty': }
-  package { 'mp3gain-gui': }
-  package { 'mp3tag': }
   package { 'notepadplusplus': }
   package { 'puppet-agent': }
   package { 'zabbix-agent': }
-
-  package { 'wsl-ubuntu-1804': ensure => absent }
-  package { 'dropbox':         ensure => absent }
-
 }
