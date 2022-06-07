fae5b0fb9ba2697229c8b9f624952377d0b5ed4f
mergwyn <gary@theclarkhome.com>
2022-06-07 10:38:04 +0100

    Periodic update of dependencies (#107)
    * Periodic update of dependencies
    * Disable backuppc to fix disk error

--- site-modules/profile/manifests/app/backuppc/server.pp
+++ site-modules/profile/manifests/app/backuppc/server.pp
@@ -66,6 +66,7 @@ class profile::app::backuppc::server (
   -> Class['backuppc::server']
 
   class { 'backuppc::server':
+    service_enable             => false, # TODO remove workaound for disk error
     backuppc_password          => lookup('secrets::backuppc'),
     gzip_path                  => '/usr/bin/pigz',
     full_age_max               => 370,
