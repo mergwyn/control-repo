ce699ca1daebb36c5922655453b46e3cb462015e
mergwyn <gary@theclarkhome.com>
2022-11-01 10:25:16 +0000

    Use defined as a workaround for apparmor

--- site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
+++ site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
@@ -2,12 +2,14 @@
 class profile::platform::baseline::debian::apparmor {
 
 # TODO duplicated in docker module - work out solution
-  #package { 'apparmor': }
+  if !defined(Package['apparmor']) {
+    package { 'apparmor': }
+  }
 
-  #service { 'apparmor':
-  #  ensure  => 'running',
-  #  enable  => true,
-  #  require => Package['apparmor']
-  #}
+  service { 'apparmor':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['apparmor']
+  }
 
 }
