c18fefdacda2b7241b7ab26bc7c8f1b2cde46643
mergwyn <gary@theclarkhome.com>
2022-11-01 11:47:10 +0000

    Install docker-compose-plugin

--- site-modules/profile/manifests/platform/baseline/debian/virtual/docker.pp
+++ site-modules/profile/manifests/platform/baseline/debian/virtual/docker.pp
@@ -3,10 +3,13 @@
 class profile::platform::baseline::debian::virtual::docker {
 
   $docker_users = [ 'gary' ]
+  $additional_packages = [ 'docker-compose-plugin' ]
 
   class { 'docker':
     ensure       => present,
     docker_users => $docker_users,
   }
+  -> package { $additional_packages: }
+
 
 }
