3fcfa16b56cbd1c54f1cb1865f77b4da2308f21a
mergwyn <gary@theclarkhome.com>
2022-11-01 10:09:31 +0000

    Add docker module

--- site-modules/profile/manifests/platform/baseline/debian/virtual/docker.pp
+++ site-modules/profile/manifests/platform/baseline/debian/virtual/docker.pp
@@ -1,39 +1,12 @@
 #
 #
 class profile::platform::baseline::debian::virtual::docker {
-# See https://wiki.kobol.io/helios64/software/zfs/docker-zfs/
-  #include ::snapd
-  #$snappackages = [ 'frr' ]
-  #package { $snappackages:
-  #  ensure   => present,
-  #  provider => snap,
-  #}
-  # Docker
 
-#sudo add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) \
-#   stable"
+  $docker_users = [ 'gary' ]
 
-  apt::source { 'docker':
-    comment  => 'Docker Repository',
-    location => 'https://download.docker.com/linux/ubuntu',
-    #release  => $release,
-    repos    => 'docker',
-    key      => {
-      'id'     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
-      'server' => 'https://download.docker.com/linux/ubuntu/gpg',
-    },
+  clase { 'docker':
+    ensure       => present,
+    docker_users => $docker_users,
   }
-  $aptpackages = [
-    #'apt-transport-https',
-    #'ca-certificates',
-    #'curl',
-    #'software-properties-common',
-    'docker_ce',
-  ]
-  package { $aptpackages: ensure   => present, }
 
 }
-#
-# vim: sw=2:ai:nu expandtab
