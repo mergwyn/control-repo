f1d35e190fcf7c9bbda3afb6e63cbada38eef170
mergwyn <gary@theclarkhome.com>
2022-11-01 10:22:12 +0000

    apparmor is also declared in docker - comment out for now

--- site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
+++ site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
@@ -1,12 +1,13 @@
 #
 class profile::platform::baseline::debian::apparmor {
 
-  package { 'apparmor': }
+# TODO duplicated in docker module - work out solution
+  #package { 'apparmor': }
 
-  service { 'apparmor':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['apparmor']
-  }
+  #service { 'apparmor':
+  #  ensure  => 'running',
+  #  enable  => true,
+  #  require => Package['apparmor']
+  #}
 
 }
