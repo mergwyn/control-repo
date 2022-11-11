b25f1ad542d9a558ae884e470f513d3a0413369b
mergwyn <gary@theclarkhome.com>
2022-11-11 17:31:39 +0000

    TODO comment out apparmor due to clash with docker

--- site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
+++ site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
@@ -6,10 +6,10 @@ class profile::platform::baseline::debian::apparmor {
 #    package { 'apparmor': }
 #  }
 #
-#  service { 'apparmor':
-#    ensure  => 'running',
-#    enable  => true,
-#    require => Package['apparmor']
-#  }
+  service { 'apparmor':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['apparmor']
+  }
 
 }
