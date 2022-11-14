f99b063fff71860df7b47ac3a570ba83eca47471
mergwyn <gary@theclarkhome.com>
2022-11-14 08:59:41 +0000

    Apparmor is installed by default so not needed

--- site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
+++ site-modules/profile/manifests/platform/baseline/debian/apparmor.pp
@@ -1,15 +1,9 @@
 #
 class profile::platform::baseline::debian::apparmor {
 
-# TODO duplicated in docker module - work out solution
-#  if !defined(Package['apparmor']) {
-#    package { 'apparmor': }
-#  }
-#
   service { 'apparmor':
-    ensure  => 'running',
-    enable  => true,
-    #require => Package['apparmor']
+    ensure => 'running',
+    enable => true,
   }
 
 }
