884054cab693d95bab07902382004cac0762ef40
mergwyn <gary@theclarkhome.com>
2022-11-26 18:09:26 +0000

    Move docker apps to physical nodes

--- site-modules/role/manifests/iptv_server.pp
+++ site-modules/role/manifests/iptv_server.pp
@@ -1,11 +1,8 @@
 #
 class role::iptv_server {
   include profile::platform::baseline  # All roles should have the base profile
-  include profile::platform::baseline::debian::virtual::docker
 
   include profile::app::sssd
-  include profile::app::iptv
-  include profile::app::speedtest
   include profile::app::zabbix::agent
   include profile::app::backuppc::client
 }
--- site-modules/role/manifests/main_server.pp
+++ site-modules/role/manifests/main_server.pp
@@ -14,11 +14,13 @@ class role::main_server {
   include profile::app::backuppc::client
   include profile::app::backuppc::server
   include profile::app::gpo::clean
+  include profile::app::iptv
   include profile::app::nginx::webdav
   include profile::app::nginx::phonebook
   include profile::app::odrive
   include profile::app::photos
   include profile::app::samba::shares
+  include profile::app::speedtest
   include profile::app::timemachine
   include profile::app::transcoder
   include profile::app::unison
