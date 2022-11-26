023f4d7e6473837d894dae64d1b3ce5eb7ba27b0
mergwyn <gary@theclarkhome.com>
2022-11-26 18:01:59 +0000

    Tidy up fuse support

--- site-modules/profile/manifests/platform/baseline/debian/virtual/docker.pp
+++ site-modules/profile/manifests/platform/baseline/debian/virtual/docker.pp
@@ -4,11 +4,7 @@ class profile::platform::baseline::debian::virtual::docker {
 
 # TODO woraround for https://github.com/puppetlabs/puppetlabs-docker/issues/870
   if $facts['virtual'] == 'lxc' {
-#    package  { [ 'fuse-overlayfs' ]: }
-    $storage_driver = 'fuse-overlayfs'
-#    $storage_driver = 'vfs'
-  } else {
-    $storage_driver = 'zfs'
+    package  { [ 'fuse-overlayfs' ]: }
   }
 
   $docker_users = [ 'gary' ]
@@ -17,9 +13,7 @@ class profile::platform::baseline::debian::virtual::docker {
   class { 'docker':
     ensure         => present,
     docker_users   => $docker_users,
-    storage_driver => $storage_driver,
   }
   -> package { $additional_packages: }
-
 
 }
