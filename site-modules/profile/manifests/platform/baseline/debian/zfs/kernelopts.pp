24eb22fb8e66f4c6c59708427783ad43ba4f4836
mergwyn <gary@theclarkhome.com>
2022-12-14 11:24:49 +0000

    Make sure zfs_arc* are not floats

--- site-modules/profile/manifests/platform/baseline/debian/zfs/kernelopts.pp
+++ site-modules/profile/manifests/platform/baseline/debian/zfs/kernelopts.pp
@@ -1,21 +1,21 @@
 # @summary kernel options (mainly tuning)
 #
-class profile::platform::baseline::debian::zfs::kernelopts {
+class profile::platform::baseline::debian::zfs::kernelopts (
+  Integer[0,100] $arc_max_percent = 0,
+  Integer[0,100] $arc_min_percent = 0,
+) {
 
-# settings (percentage)
-  $max = 0.3
-  $min = 0.1
 
   kmod::option { 'zfs_arc_max':
     module => 'zfs',
     option => 'zfs_arc_max',
-    value  => $::facts['memory']['system']['total_bytes']*$max,
+    value  => Integer($::facts['memory']['system']['total_bytes']*$arc_max_percent/100),
     notify => Exec['update_initramfs_all'],
   }
-  kmod::option { 'zfs_arc_min':,
+  kmod::option { 'zfs_arc_min':
     module => 'zfs',
     option => 'zfs_arc_min',
-    value  => $::facts['memory']['system']['total_bytes']*$min,
+    value  => Integer($::facts['memory']['system']['total_bytes']*$arc_min_percent/100),
     notify => Exec['update_initramfs_all'],
   }
 #  kmod::option { 'zfs_vdev_scheduler':,
@@ -25,23 +25,23 @@ class profile::platform::baseline::debian::zfs::kernelopts {
 #    notify => Exec['update_initramfs_all'],
 #   use the prefetch method,
 #  }
-#  kmod::option { 'zfs_prefetch_disable':,
-#    module => 'zfs',
-#    option => 'zfs_prefetch_disable',
-#    value => 0,
-#    notify => Exec['update_initramfs_all'],
-#  },
+  kmod::option { 'zfs_prefetch_disable':
+    module => 'zfs',
+    option => 'zfs_prefetch_disable',
+    value  => 0,
+    notify => Exec['update_initramfs_all'],
+  }
 #   max write speed to l2arc,
 #   tradeoff between write/read and durability of ssd (?),
 #   default  => 8 * 1024 * 1024,
-  kmod::option { 'l2arc_write_max':,
+  kmod::option { 'l2arc_write_max':
     module => 'zfs',
     option => 'l2arc_write_max',
     value  => 500*1024*1024,
     notify => Exec['update_initramfs_all'],
   }
 
-  exec { 'update_initramfs_all':,
+  exec { 'update_initramfs_all':
     command     => '/usr/sbin/update-initramfs -k all -u',
     refreshonly => true,
   }
