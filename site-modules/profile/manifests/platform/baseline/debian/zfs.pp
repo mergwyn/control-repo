cbae1e677db1aea2db2a4e1fbfbad1a7a7bcc013
mergwyn <gary@theclarkhome.com>
2023-08-02 11:28:16 +0100

    Remove grub menu support for zfs boot menu

--- site-modules/profile/manifests/platform/baseline/debian/zfs.pp
+++ site-modules/profile/manifests/platform/baseline/debian/zfs.pp
@@ -4,7 +4,7 @@ class profile::platform::baseline::debian::zfs {
 
   include profile::platform::baseline::debian::zfs::autosnapshot
   include profile::platform::baseline::debian::zfs::default
-  include profile::platform::baseline::debian::zfs::grub
+# include profile::platform::baseline::debian::zfs::grub
   include profile::platform::baseline::debian::zfs::kernelopts
   include profile::platform::baseline::debian::zfs::reporting
   include profile::platform::baseline::debian::zfs::zabbix
