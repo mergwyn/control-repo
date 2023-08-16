5603a211295f9062f9cb8b8db3e8b271e98ef4e5
mergwyn <gary@theclarkhome.com>
2023-08-16 21:15:28 +0100

    attempt to remove pdk validate errors

--- site-modules/bootstrap/plans/windows.pp
+++ site-modules/bootstrap/plans/windows.pp
@@ -1,6 +1,5 @@
 # This is the structure of a simple plan. To learn more about writing
 # Puppet plans, see the documentation: http://pup.pt/bolt-puppet-plans
-
 # The summary sets the description of the plan that will appear
 # in 'bolt plan show' output. Bolt uses puppet-strings to parse the
 # summary and parameters from the plan.
