f5239be5d72df240992092dec0c9a81f171e2849
mergwyn <gary@theclarkhome.com>
2023-08-24 16:42:16 +0100

    First pass at adding microk8s settings

--- site-modules/role/manifests/k8s_server.pp
+++ site-modules/role/manifests/k8s_server.pp
@@ -3,6 +3,7 @@ class role::k8s_server {
   include profile::platform::baseline  # All roles should have the base profile
   include profile::platform::baseline::debian::zfs
   include profile::app::sssd
+  include profile::app::microk8s
   include profile::platform::baseline::debian::virtual::lxd
   include profile::app::zabbix::agent
   include profile::app::backuppc::client
