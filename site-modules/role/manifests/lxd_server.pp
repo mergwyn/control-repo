d6b1f17ed260eea0c4520fc7912b7ecc36e3aa23
mergwyn <gary@theclarkhome.com>
2023-08-30 08:31:17 +0100

    Add microk8s settings

--- site-modules/role/manifests/lxd_server.pp
+++ site-modules/role/manifests/lxd_server.pp
@@ -4,6 +4,7 @@ class role::lxd_server {
   include profile::platform::baseline::debian::zfs
   include profile::app::sssd
   include profile::platform::baseline::debian::virtual::lxd
+  include profile::app::microk8s
   include profile::app::zabbix::agent
   include profile::app::backuppc::client
   include profile::app::unison
