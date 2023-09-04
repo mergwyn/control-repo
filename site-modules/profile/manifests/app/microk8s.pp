a1abbaca13f0aeedc8a9edf9a210fa94cf7cca45
mergwyn <gary@theclarkhome.com>
2023-09-04 09:06:32 +0100

    Apply quic values for cloudflared tunnel

---
+++ site-modules/profile/manifests/app/microk8s.pp
@@ -0,0 +1,20 @@
+# @summary microk8s support
+#
+class profile::app::microk8s () {
+
+  if $facts['os']['family'] != 'Debian' {
+    fail("${name} can only be called on Debian")
+  }
+
+  sysctl{ 'net.core.rmem_max':
+    ensure => present,
+    value  => '2500000',
+    target => "/etc/sysctl.d/66 quic.conf",
+  }
+  sysctl{ 'net.core.wmem_max':
+    ensure => present,
+    value  => '2500000',
+    target => "/etc/sysctl.d/66 quic.conf",
+  }
+
+}
