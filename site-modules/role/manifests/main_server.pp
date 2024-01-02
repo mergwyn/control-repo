d3d97646141a27055854969f8d97172f1562574b
mergwyn <gary@theclarkhome.com>
2024-01-02 11:25:18 +0000

    Add kubernetes support (#119)
    * Add kubernetes support (and k3s)
    * Refactor Puppefile and .fixtures.yml to match published syntax
    * Add service settings
    * Remove some plex files from backup
    * Remove some downloader files from backup
    * Remove unison files from backup
    * Remove some mysql files from backup

--- site-modules/role/manifests/main_server.pp
+++ site-modules/role/manifests/main_server.pp
@@ -8,9 +8,9 @@ class role::main_server {
 
   include profile::app::sssd
   include profile::app::zabbix::agent
-  include profile::platform::baseline::debian::virtual::lxd
   include profile::platform::baseline::debian::autofs
-  include profile::app::microk8s
+  include profile::platform::baseline::debian::virtual::lxd
+  include profile::platform::baseline::debian::virtual::kubernetes
 
   include profile::app::backuppc::client
   include profile::app::backuppc::server
