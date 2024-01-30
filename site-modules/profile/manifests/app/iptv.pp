137c75d1e59aaea492bdb29008f5b3a8fbd542a4
mergwyn <gary@theclarkhome.com>
2024-01-30 12:41:56 +0000

    Add flag to enable/disable get_channels

--- site-modules/profile/manifests/app/iptv.pp
+++ site-modules/profile/manifests/app/iptv.pp
@@ -1,6 +1,8 @@
 #
 #
-class profile::app::iptv {
+class profile::app::iptv (
+  Boolean $enable = false
+) {
   include cron
   include profile::app::scripts
 
@@ -9,37 +11,41 @@ class profile::app::iptv {
   $packages = [ 'socat' ]
   package { $packages: ensure => present }
 
+  if $enable {
 # systemd timer to download channels
-  $adminemail = lookup('defaults::adminemail')
-  $_timer = @(EOT)
-    [Unit]
-    Description=Run get channels on boot and periodically
+    $adminemail = lookup('defaults::adminemail')
+    $_timer = @(EOT)
+      [Unit]
+      Description=Run get channels on boot and periodically
 
-    [Timer]
-    OnBootSec=20min
-    OnUnitActiveSec=6h
+      [Timer]
+      OnBootSec=20min
+      OnUnitActiveSec=6h
 
-    [Install]
-    WantedBy=timers.target
-    | EOT
+      [Install]
+      WantedBy=timers.target
+      | EOT
 
-  $_service = @("EOT")
-    [Unit]
-    Description=runs channel download
-    Wants=getchannels.timer
+    $_service = @("EOT")
+      [Unit]
+      Description=runs channel download
+      Wants=getchannels.timer
 
-    [Service]
-    Type=oneshot
-    ExecStart=/bin/bash -c '${codedir}/iptv/update-xteve-plex 2>&1 | /usr/bin/mailx -E -s "%N output root@%H" ${adminemail}'
+      [Service]
+      Type=oneshot
+      ExecStart=/bin/bash -c '${codedir}/iptv/update-xteve-plex 2>&1 | /usr/bin/mailx -E -s "%N output root@%H" ${adminemail}'
 
-    [Install]
-    WantedBy=multi-user.target
-    | EOT
+      [Install]
+      WantedBy=multi-user.target
+      | EOT
 
-  systemd::timer{'getchannels.timer':
-    timer_content   => $_timer,
-    service_content => $_service,
-    enable          => true,
+    systemd::timer {'getchannels.timer':
+      timer_content   => $_timer,
+      service_content => $_service,
+      enable          => true,
+    }
+  }  else {
+    systemd::timer { 'getchannels.timer': ensure => absent }
   }
 
   #TODO xteve?
