e777c5eb189a215c64b464048ad5666b6f143e5c
mergwyn <gary@theclarkhome.com>
2024-07-29 15:10:24 +0100

    Reinstate vrrp

--- site-modules/profile/manifests/app/keepalived/dns.pp
+++ site-modules/profile/manifests/app/keepalived/dns.pp
@@ -14,18 +14,18 @@ class profile::app::keepalived::dns (
 #  }
 
 # VRRP
-#  keepalived::vrrp::instance { 'VI_DNS':
-#    interface         => $lan,
-#    lvs_interface     => 'veth-dns',
-#    state             => 'BACKUP',
-#    virtual_router_id => 51,
-#    priority          => 150,
-#    auth_type         => 'PASS',
-#    auth_pass         => lookup('secrets::keepalived'),
-#    virtual_ipaddress => [ $v_ip ],
-##   track_interface   => [ $wan, "${vpn} weight 5"], # optional, monitor these interfaces.
-#    #track_script      => 'check_dns',
-#  }
+  keepalived::vrrp::instance { 'VI_DNS':
+    interface         => $lan,
+    lvs_interface     => 'veth-dns',
+    state             => 'BACKUP',
+    virtual_router_id => 51,
+    priority          => 150,
+    auth_type         => 'PASS',
+    auth_pass         => lookup('secrets::keepalived'),
+    virtual_ipaddress => [ $v_ip ],
+#   track_interface   => [ $wan, "${vpn} weight 5"], # optional, monitor these interfaces.
+    #track_script      => 'check_dns',
+  }
 
 # Add virtual server for DNS
   keepalived::lvs::virtual_server { 'VPN_DNS':
@@ -33,9 +33,8 @@ class profile::app::keepalived::dns (
     port       => 53,
     delay_loop => 6,
     ha_suspend => true,
-    lb_algo    => 'wrr',
+    lb_algo    => 'wlc',
     lb_kind    => 'DR',
-    # TODO remove? persistence_timeout => 0,
     protocol   => 'TCP'
   }
 
@@ -46,8 +45,8 @@ class profile::app::keepalived::dns (
       port           => 53,
       options        => {
         weight      => 1,
-        notify_down => "'/sbin/ipvsadm -d -u ${v_ip}:53 -r ${real_ip}:53'",
-        notify_up   => "'/sbin/ipvsadm -a -u ${v_ip}:53 -r ${real_ip}:53 -g -w 1'",
+#        notify_down => "'/sbin/ipvsadm -d -u ${v_ip}:53 -r ${real_ip}:53'",
+#        notify_up   => "'/sbin/ipvsadm -a -u ${v_ip}:53 -r ${real_ip}:53 -g -w 1'",
         'TCP_CHECK' => {
           connect_timeout => '3',
         }
