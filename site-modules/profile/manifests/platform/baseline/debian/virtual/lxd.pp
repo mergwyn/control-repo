ce466b56bde1bea63f4fdf53a6f0eb51989199a3
mergwyn <gary@theclarkhome.com>
2024-10-08 10:09:15 +0100

    Change lxd to default 'latest/stable'

--- site-modules/profile/manifests/platform/baseline/debian/virtual/lxd.pp
+++ site-modules/profile/manifests/platform/baseline/debian/virtual/lxd.pp
@@ -6,7 +6,7 @@ class profile::platform::baseline::debian::virtual::lxd {
 
   include ::snap
   package { 'lxd':
-    ensure   => latest,
+    #ensure   => latest,
     provider => snap,
   }
   exec { 'enable-criu':
