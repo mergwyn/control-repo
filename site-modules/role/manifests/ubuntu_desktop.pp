d6aa3c4a3ad598f6270a5a8eddb4e19b79c38053
mergwyn <gary@theclarkhome.com>
2024-12-31 14:03:22 +0000

    Add backup to ubuntu desktop

--- site-modules/role/manifests/ubuntu_desktop.pp
+++ site-modules/role/manifests/ubuntu_desktop.pp
@@ -3,5 +3,5 @@ class role::ubuntu_desktop {
   include profile::platform::baseline  # All roles should have the base profile
   include profile::app::sssd
   #include profile::app::zabbix::agent
-  #include profile::app::backuppc::client
+  include profile::app::backuppc::client
 }
