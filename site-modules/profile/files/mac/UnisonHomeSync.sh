a589594f5e62a4c24d2dbe1338162ec4af3ef883
mergwyn <gary@theclarkhome.com>
2025-10-12 16:22:34 +0100

    Delete unison log on startup

--- site-modules/profile/files/mac/UnisonHomeSync.sh
+++ site-modules/profile/files/mac/UnisonHomeSync.sh
@@ -2,6 +2,7 @@
 LOCAL="${HOME}"
 REMOTE="ssh://${LOGNAME}@foxtrot//home/${LOGNAME}"
 ARGS="-silent -ui text -repeat watch -terse"
+cp /dev/null ~/.unison/unison.log
 
 if [[ $(pmset -g ps | head -1) =~ "AC Power" ]]; then
   /opt/homebrew/bin/unison Home ${LOCAL} ${REMOTE} ${ARGS}
