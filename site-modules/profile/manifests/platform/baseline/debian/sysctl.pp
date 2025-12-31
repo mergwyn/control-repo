5272bae86633ac18db5de0690829a526821b4542
mergwyn <gary@theclarkhome.com>
2025-12-31 16:15:17 +0000

    Disable ip6 for br0 interface

--- site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
+++ site-modules/profile/manifests/platform/baseline/debian/sysctl.pp
@@ -15,6 +15,7 @@ class profile::platform::baseline::debian::sysctl (
       sysctl { 'net.ipv6.conf.all.disable_ipv6':     ensure => present, value => $disable_ipv6, }
       sysctl { 'net.ipv6.conf.default.disable_ipv6': ensure => present, value => $disable_ipv6, }
       sysctl { 'net.ipv6.conf.lo.disable_ipv6':      ensure => present, value => $disable_ipv6, }
+      sysctl { 'net.ipv6.conf.br0.disable_ipv6':     ensure => present, value => $disable_ipv6, }
     }
     default: {
     }
