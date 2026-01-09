b3fd098ea5a34c8729f2029315f0688251cafce4
mergwyn <gary@theclarkhome.com>
2026-01-09 16:14:12 +0000

    Fux typos in puppet_master role

--- site-modules/role/manifests/puppet_master.pp
+++ site-modules/role/manifests/puppet_master.pp
@@ -2,8 +2,8 @@
 class role::puppet_master {
   include profile::platform::baseline  # All roles should have the base profile
   include profile::openvox::server
-  puppetinclude profile::openvox::agent
-  puppetinclude profile::openvoe::dev
-  einclude profile::app::zabbix::agent
+  include profile::openvox::agent
+  include profile::openvox::dev
+  #include profile::app::zabbix::agent
   #include profile::app::backuppc::client
 }
