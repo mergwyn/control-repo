5bc25b1a3da113f7519046340156ad85c2ec51f4
mergwyn <gary@theclarkhome.com>
2026-01-09 14:34:29 +0000

    Convert roles to openvox

--- site-modules/role/manifests/main_server.pp
+++ site-modules/role/manifests/main_server.pp
@@ -4,7 +4,7 @@ class role::main_server {
   include profile::platform::baseline::debian::zfs
   include profile::platform::baseline::debian::nfs_server
 
-  include profile::puppet::dev
+  include profile::openvox::dev
 
   include profile::app::sssd
   #include profile::app::zabbix::agent
--- site-modules/role/manifests/puppet_master.pp
+++ site-modules/role/manifests/puppet_master.pp
@@ -1,9 +1,9 @@
 #
 class role::puppet_master {
   include profile::platform::baseline  # All roles should have the base profile
-  include profile::puppet::server
-  include profile::puppet::agent
-  include profile::puppet::dev
-  #include profile::app::zabbix::agent
+  include profile::openvox::server
+  puppetinclude profile::openvox::agent
+  puppetinclude profile::openvoe::dev
+  einclude profile::app::zabbix::agent
   #include profile::app::backuppc::client
 }
