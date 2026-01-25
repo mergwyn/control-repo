654a41808e9d0eb1a24a595ee77be11f6022d79a
mergwyn <gary@theclarkhome.com>
2026-01-25 16:56:20 +0000

    Refactor lxd and add support for automatic creation of remotes (#204)
    * Refactor lxd and add support for automatic creation of remotes
    
    * Ensure facter directories exist for lxd

--- site-modules/profile/manifests/platform/baseline/debian/virtual/lxd/facts.pp
+++ site-modules/profile/manifests/platform/baseline/debian/virtual/lxd/facts.pp
@@ -1,13 +1,31 @@
 #
 #
 class profile::platform::baseline::debian::virtual::lxd::facts {
+<<<<<<< HEAD
+  file { '/etc/puppetlabs/facter':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/etc/puppetlabs/facter/facts.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/etc/puppetlabs/facter'],
+  }
+
   file { '/etc/puppetlabs/facter/facts.d/lxd.yaml':
     ensure  => file,
+    owner   => 'root',
+    group   => 'root',
     mode    => '0644',
     content => @("EOF"),
       lxd:
         enabled: true
       | EOF
-    require => Package['lxd'],
+    require => File['/etc/puppetlabs/facter/facts.d'],
   }
 }
