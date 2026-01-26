811a20c23a0e1a676888db0e42dd260036986064
mergwyn <gary@theclarkhome.com>
2026-01-26 09:20:30 +0000

    Attempt to fix lxd remote config

--- site-modules/profile/manifests/app/lxd/remotes/owner.pp
+++ site-modules/profile/manifests/app/lxd/remotes/owner.pp
@@ -3,24 +3,15 @@
 class profile::app::lxd::remotes::owner (
   Integer $port = 8443,
 ) {
-  $lxd_hosts = puppetdb_query([
+  $remote_hosts = puppetdb_query([
     'from',
     'nodes',
     ['=', ['fact', 'lxd.enabled'], true],
   ])
+  .map |$n| { $n['certname'] }
+  .filter |$h| { $h != $facts['networking']['fqdn'] }
 
-  # collect all the host certs
-  File <<| tag == 'lxd_server_cert' |>>
-
-  $remote_hosts = puppetdb_query([
-    'from',
-    'nodes',
-    ['=', ['fact', 'lxd_remote_owner'], $facts['networking']['fqdn']],
-  ]).map |$n| {
-    $n['certname']
-  }.filter |$h| {
-    $h and $h != $facts['networking']['fqdn']
-  }
+  notice("LXD remote hosts discovered: ${remote_hosts}")
 
   class { 'profile::app::lxd::remotes':
     remote_hosts => $remote_hosts,
