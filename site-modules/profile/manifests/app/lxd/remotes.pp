4f1c542d72340c59a0e9eb781018756876fb532f
mergwyn <gary@theclarkhome.com>
2026-01-26 20:11:13 +0000

    Tidy up and debugging of lxd remote

--- site-modules/profile/manifests/app/lxd/remotes.pp
+++ site-modules/profile/manifests/app/lxd/remotes.pp
@@ -1,24 +1,20 @@
 #
-#
 class profile::app::lxd::remotes (
-  Array[String] $remote_hosts,
-  Integer       $port = 8443,
+  Array[Hash] $remote_hosts,
+  Integer     $port = 8443,
 ) {
   if $remote_hosts.empty {
     notice('No LXD remotes to configure')
   } else {
-    $remote_hosts.each |String $host| {
-      exec { "lxd-trust-cert-${host}":
-        path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-        command => "lxc config trust add /var/lib/lxd-certs/${host}.crt",
-        unless  => "lxc config trust list --format csv | grep -q ${host}",
-        require => Package['lxd'],
-      }
-      exec { "lxd-add-remote-${host}":
-        path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-        command => "lxc remote add ${host} https://${host}:${port} --accept-certificate",
-        unless  => "lxc remote list --format csv | cut -d, -f1 | grep -qx ${host}",
-        require => Exec["lxd-trust-cert-${host}"],
+    $remote_hosts.each |Hash $host| {
+      exec { "lxd-add-remote-${host['name']}":
+        path      => '/usr/bin:/usr/sbin:/bin:/sbin',
+        command   => "lxc remote add ${host['name']} https://${host['fqdn']}:${port} --accept-certificate",
+        unless    => "lxc remote list --format csv | cut -d, -f1 | grep -qx ${host['name']}",
+        onlyif    => "test -f /var/lib/lxd-certs/${host['fqdn']}.crt",
+        tries     => 5,
+        try_sleep => 10,
+        require   => Package['lxd'],
       }
     }
   }
--- site-modules/profile/manifests/app/lxd/remotes/owner.pp
+++ site-modules/profile/manifests/app/lxd/remotes/owner.pp
@@ -3,13 +3,25 @@
 class profile::app::lxd::remotes::owner (
   Integer $port = 8443,
 ) {
-  $remote_hosts = puppetdb_query([
-    'from',
-    'nodes',
-    ['=', ['fact', 'lxd.enabled'], true],
-  ])
-  .map |$n| { $n['certname'] }
-  .filter |$h| { $h != $facts['networking']['fqdn'] }
+
+  $nodes = pick(
+    puppetdb_query('nodes { facts.lxd.enabled = true }'),
+    []
+  )
+
+  $remote_hosts = ($nodes)
+    .filter |$n| { $n['certname'] }
+    .map |$n| {
+      $fqdn  = $n['certname'];
+      $short = $fqdn.split('.')[0];
+      {
+        'name' => $short,
+        'fqdn' => $fqdn,
+      }
+    }
+    .filter |$h| {
+      $h['fqdn'] != $facts['networking']['fqdn']
+    }
 
   notice("LXD remote hosts discovered: ${remote_hosts}")
 
