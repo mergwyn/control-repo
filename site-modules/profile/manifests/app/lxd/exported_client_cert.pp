c39f8df7f1ac35d2fd782ada37c405af6f2c2b1d
mergwyn <gary@theclarkhome.com>
2026-01-29 11:29:44 +0000

    Change deugging message

--- site-modules/profile/manifests/app/lxd/exported_client_cert.pp
+++ site-modules/profile/manifests/app/lxd/exported_client_cert.pp
@@ -10,7 +10,7 @@ define profile::app::lxd::exported_client_cert (
   ] $cert = undef,
 ) {
   if $cert == undef {
-    notify{"Skipping exported LXD cert for ${title} (no cert yet)": }
+    notify{"LXD trust not bootstrapped yet on ${facts['networking']['hostname']}":}
     return()
   }
 
