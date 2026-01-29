79229ec8833277cd25f2004d660dd6d15d03c3d1
mergwyn <gary@theclarkhome.com>
2026-01-29 15:24:53 +0000

    restructre to separate concerns

---
+++ site-modules/profile/manifests/app/lxd/exporter.pp
@@ -0,0 +1,10 @@
+#
+#
+class profile::app::lxd::exporter {
+  if $facts['lxd_client_cert'] {
+    @@profile::app::lxd::exported_client_cert { $facts['networking']['hostname']:
+      fqdn => $facts['networking']['fqdn'],
+      cert => Sensitive($facts['lxd_client_cert']),
+    }
+  }
+}
