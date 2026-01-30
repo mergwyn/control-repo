88722614fb3062f9d5f8a71d9e1329f3c22a4be2
mergwyn <gary@theclarkhome.com>
2026-01-30 12:46:52 +0000

    Used fixed path ruby fact creats and tighten tests in exporter

--- site-modules/profile/lib/facter/lxc_client_cert.rb
+++ site-modules/profile/lib/facter/lxc_client_cert.rb
@@ -2,7 +2,7 @@ Facter.add(:lxd_client_cert) do
   confine kernel: 'Linux'
 
   setcode do
-    cert_path = "/root/snap/lxd/common/config/client.crt"
+    cert_path = '/root/snap/lxd/common/config/client.crt'
     if File.readable?(cert_path)
       File.read(cert_path)
     else
