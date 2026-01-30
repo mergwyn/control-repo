5404c216089ec64d5444026cecbf419f79907de8
mergwyn <gary@theclarkhome.com>
2026-01-30 14:19:10 +0000

    Remove DEBUG statements in exporter

--- site-modules/profile/manifests/app/lxd/exporter.pp
+++ site-modules/profile/manifests/app/lxd/exporter.pp
@@ -1,11 +1,8 @@
 #
 class profile::app::lxd::exporter {
-  notify { 'DEBUG: profile::app::lxd::exporter compiled': }
-  notify { "DEBUG lxd_client_cert=${facts['lxd_client_cert']}": }
 
   if $facts['lxd_client_cert']
     and String($facts['lxd_client_cert']) != '' {
-    notify { 'DEBUG exporting LXD cert': }
 
     @@profile::app::lxd::exported_client_cert {
       $facts['networking']['hostname']:
