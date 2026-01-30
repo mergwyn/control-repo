3ad631805d0c8514603c0b15f1985bd3c6f2da72
mergwyn <gary@theclarkhome.com>
2026-01-30 12:40:29 +0000

    Debuuging commands and update the custom lxd cert fact

--- site-modules/profile/lib/facter/lxc_client_cert.rb
+++ site-modules/profile/lib/facter/lxc_client_cert.rb
@@ -1,8 +1,12 @@
-# modules/profile/lib/facter/lxd_client_cert.rb
 Facter.add(:lxd_client_cert) do
-  confine { File.exist?('/root/snap/lxd/common/config/client.crt') }
+  confine kernel: 'Linux'
 
   setcode do
-    File.read('/root/snap/lxd/common/config/client.crt')
+    cert_path = "/root/snap/lxd/common/config/client.crt"
+    if File.readable?(cert_path)
+      File.read(cert_path)
+    else
+      nil
+    end
   end
 end
--- site-modules/profile/manifests/app/lxd/exporter.pp
+++ site-modules/profile/manifests/app/lxd/exporter.pp
@@ -1,8 +1,12 @@
 #
-#
 class profile::app::lxd::exporter {
+  notify { 'DEBUG: profile::app::lxd::exporter compiled': }
+  notify { "DEBUG lxd_client_cert=${facts['lxd_client_cert']}": }
+
   if $facts['lxd_client_cert'] and $facts['lxd_client_cert'] != '' {
-    @@profile::app::lxd::exported_client_cert { $facts['networking']['hostname']:
+    notify { 'DEBUG exporting LXD cert': }
+
+    @@profile::app::lxd::exported_client_cert { "${facts['networking']['hostname']}-lxd":
       fqdn => $facts['networking']['fqdn'],
       cert => Sensitive($facts['lxd_client_cert']),
     }
