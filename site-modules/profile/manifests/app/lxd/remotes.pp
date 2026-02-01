8dd50109d811b56b747d33325e815636b6ea04e6
mergwyn <gary@theclarkhome.com>
2026-02-01 09:22:59 +0000

    Add consumers

--- site-modules/profile/manifests/app/lxd/remotes.pp
+++ site-modules/profile/manifests/app/lxd/remotes.pp
@@ -5,8 +5,17 @@ class profile::app::lxd::remotes {
   require profile::platform::baseline::debian::virtual::lxd
 
   if $facts.dig('lxd', 'manage_remotes') {
-    Profile::App::Lxd::Exported_client_cert <<|
+
+    $client_certs = Profile::App::Lxd::Exported_client_cert <<|
       certname != $trusted['certname']
     |>>
+
+    # Drop anything without a cert (paranoia + old exports)
+    $client_certs = $client_certs.filter |$k, $v| { $v['cert'] }
+
+    create_resources(
+      'profile::app::lxd::consume_client_cert',
+      $client_certs
+    )
   }
 }
