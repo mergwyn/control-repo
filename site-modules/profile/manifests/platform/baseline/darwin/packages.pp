762fb5461855c5437f8e8263069d7a093b045681
mergwyn <gary@theclarkhome.com>
2026-02-16 14:17:49 +0000

    Keep tap install separate from casks for homebrew

--- site-modules/profile/manifests/platform/baseline/darwin/packages.pp
+++ site-modules/profile/manifests/platform/baseline/darwin/packages.pp
@@ -2,4 +2,15 @@
 #
 class profile::platform::baseline::darwin::packages {
 # TODO add call to brew update and brew upgrade?
+
+  $taps = [
+    'git',
+    'unison',
+    'python@3.12',   # TODO convert to a fact
+    'python-tk@3.12',
+  ]
+  package { $taps:
+    provider => 'brew',
+    ensure   => 'present'
+  }
 }
