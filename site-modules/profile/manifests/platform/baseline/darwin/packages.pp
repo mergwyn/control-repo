25d4551ab0f5abef3dd525f1aae6dcb0bb9bdf81
mergwyn <gary@theclarkhome.com>
2026-02-16 14:23:00 +0000

    Fix paths in k8stools (#209)
    * Fix paths in k8stools
    * make managedmac reference master
    * Switch to my repo for puppet-homebrew
    * Update providers and switch to present for packages
    * move darwin packages to hiera

--- site-modules/profile/manifests/platform/baseline/darwin/packages.pp
+++ site-modules/profile/manifests/platform/baseline/darwin/packages.pp
@@ -1,36 +1,16 @@
 # @summary Packages for Darwin
 #
 class profile::platform::baseline::darwin::packages {
-  Package {
-    provider => brewcask,
-    ensure   => latest,
-  }
+# TODO add call to brew update and brew upgrade?
 
-# packages to be installed
-  #package { 'amazon-music': }
   $taps = [
     'git',
     'unison',
     'python@3.12',   # TODO convert to a fact
     'python-tk@3.12',
   ]
-  package { $taps: provider => 'brew' }
-
-  $puppetver = $facts['os']['macosx']['version']['major'] ? {
-    /10.13/ => '6',
-    default => '7',
+  package { $taps:
+    provider => 'brew',
+    ensure   => 'present'
   }
-
-  $casks = [
-    'firefox',
-    'aerial',
-    'skype',
-    'zoom',
-    'vlc',
-    'sonos',
-    'plex',
-    "puppetlabs/puppet/puppet-agent-${puppetver}",
-  ]
-
-  package { $casks: }
 }
--- site-modules/role/manifests/mac_laptop.pp
+++ site-modules/role/manifests/mac_laptop.pp
@@ -1,7 +1,7 @@
 #
 class role::mac_laptop {
   include profile::platform::baseline
-  include profile::platform::baseline::darwin::managedmac
+  #include profile::platform::baseline::darwin::managedmac
 
   include profile::app::unison
 }
