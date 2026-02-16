ad8e85efa1ba9aacfb83a08ba9d541268bd96d6b
mergwyn <gary@theclarkhome.com>
2026-02-16 17:21:17 +0000

    use full length keys and switch to .source files

--- site-modules/profile/manifests/app/k8s_tools/debian.pp
+++ site-modules/profile/manifests/app/k8s_tools/debian.pp
@@ -1,46 +1,73 @@
 #
 #
 class profile::app::k8s_tools::debian {
-
   $bin_dir = '/usr/local/bin'
 
   include apt
 
+  # --- Ensure keyring directory exists ---
+  file { '/usr/share/keyrings':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
   # --- Kubernetes key & repo ---
-  apt::key { 'kubernetes':
-    id     => '8D81803C0EBFCD88',
-    source => 'https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key',
+  file { '/usr/share/keyrings/kubernetes.gpg':
+    ensure  => file,
+    source  => 'https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key',
+    mode    => '0644',
+    require => File['/usr/share/keyrings'],
   }
 
   apt::source { 'kubernetes':
     location => 'https://pkgs.k8s.io/core:/stable:/v1.31/deb/',
-    release  => '/',
     repos    => ' ',
-    require  => Apt::Key['kubernetes'],
+    release  => '/',
+    key      => '/usr/share/keyrings/kubernetes.gpg',
+    include  => {
+      deb => true,
+      src => false,
+    },
   }
 
   # --- Helm key & repo ---
-  apt::key { 'helm':
-    id     => 'B1998361219BD9C9',
-    source => 'https://baltocdn.com/helm/signing.asc',
+  file { '/usr/share/keyrings/helm.gpg':
+    ensure  => file,
+    source  => 'https://baltocdn.com/helm/signing.asc',
+    mode    => '0644',
+    require => File['/usr/share/keyrings'],
   }
 
   apt::source { 'helm':
     location => 'https://baltocdn.com/helm/stable/debian/',
     repos    => 'all',
-    require  => Apt::Key['helm'],
+    release  => '/',
+    key      => '/usr/share/keyrings/helm.gpg',
+    include  => {
+      deb => true,
+      src => false,
+    },
   }
 
   # --- ArgoCD key & repo ---
-  apt::key { 'argocd':
-    id     => '7F2DF7AD',
-    source => 'https://apt.argoproj.io/key.gpg',
+  file { '/usr/share/keyrings/argocd.gpg':
+    ensure  => file,
+    source  => 'https://apt.argoproj.io/key.gpg',
+    mode    => '0644',
+    require => File['/usr/share/keyrings'],
   }
 
   apt::source { 'argocd':
     location => 'https://apt.argoproj.io/',
     repos    => 'stable',
-    require  => Apt::Key['argocd'],
+    release  => '/',
+    key      => '/usr/share/keyrings/argocd.gpg',
+    include  => {
+      deb => true,
+      src => false,
+    },
   }
 
   # --- Base packages (curl removed) ---
@@ -59,15 +86,13 @@ class profile::app::k8s_tools::debian {
   }
 
   # --- kubectl (k3s-aware) ---
-  # Symlink kubectl to k3s if k3s exists
   exec { 'symlink-kubectl-to-k3s':
     command => "/bin/ln -sf ${bin_dir}/k3s ${bin_dir}/kubectl",
-    creates => "${bin_dir}/kubectl",   # idempotent
+    creates => "${bin_dir}/kubectl",
     onlyif  => "/usr/bin/test -f ${bin_dir}/k3s",
     path    => ['/bin', '/usr/bin'],
   }
 
-  # Always install kubectl package; symlink overrides if k3s exists
   package { 'kubectl':
     ensure  => latest,
     require => Exec['apt_update'],
