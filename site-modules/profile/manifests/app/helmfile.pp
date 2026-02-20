4fa82397f37e1c0dec326ea837c5926eea0b4398
mergwyn <gary@theclarkhome.com>
2026-02-20 16:53:40 +0000

    Fix helmfile version cmd helmfile.pp

--- site-modules/profile/manifests/app/helmfile.pp
+++ site-modules/profile/manifests/app/helmfile.pp
@@ -9,6 +9,6 @@ class profile::app::helmfile (
     url         => "https://github.com/helmfile/helmfile/releases/download/v${version}/helmfile_${version}_linux_amd64.tar.gz",
     tarball     => true,
     tar_extract => 'helmfile',
-    version_cmd => 'helmfile --version',
+    version_cmd => 'helmfile version -o short',
   }
 }
