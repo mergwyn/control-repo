796002f5a3c70d7999ef7c630e8bb1711421ec11
mergwyn <gary@theclarkhome.com>
2026-02-20 15:08:22 +0000

    Change kubectx.pp to commit based version tracking

--- site-modules/profile/manifests/app/kubectx.pp
+++ site-modules/profile/manifests/app/kubectx.pp
@@ -1,14 +1,13 @@
 # @summary Install kubectx shell script with version stamping
 #
 class profile::app::kubectx (
-  String $version = '0.9.5', # renovate: datasource=github-releases depName=ahmetb/kubectx
+  String $version = 'f3c2d91', # renovate: datasource=github-commits depName=ahmetb/kubectx
 ) {
   profile::app::binary_install { 'kubectx':
     version     => $version,
     binary      => 'kubectx',
-    url         => "https://github.com/ahmetb/kubectx/releases/download/v${version}/kubectx_v${version}_linux_x86_64.tar.gz",
-    tarball     => true,
-    tar_extract => 'kubectx',
+    url         => "https://github.com/ahmetb/kubectx/raw/${version}/kubectx",
+    install_dir => '/usr/local/bin',
     stamp       => true,
   }
 }
