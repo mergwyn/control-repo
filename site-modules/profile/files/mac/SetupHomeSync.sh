95f44dbbd890380a29cc3145e8d30edeefb60ff9
mergwyn <gary@theclarkhome.com>
2026-03-08 09:42:08 +0000

    Tidy up homesync setup

--- site-modules/profile/files/mac/SetupHomeSync.sh
+++ site-modules/profile/files/mac/SetupHomeSync.sh
@@ -1,26 +1,28 @@
-#/bin/bash
-cd ${HOME}
+#!/usr/bin/env bash
+
+cd "${HOME}" || exit 1
 
 ensureDir()
 {
-	[[ -d ${1} ]] || mkdir -p ${1}
+	[[ -d ${1} ]] || mkdir -p "${1}"
 }
-ensureDir ${HOME}/.unison 
+ensureDir "${HOME}/.unison" 
+
 for file in Home.prf Preferences.prf common.prf 
 do
-	[[ -f ${HOME}/.unison/${file} ]] && rm ${HOME}/.unison/${file}
-	ln -s /usr/local/share/${file} ${HOME}/.unison/${file}
+	[[ -f "${HOME}/.unison/${file}" ]] && rm "${HOME}/.unison/${file}"
+	ln -s "/usr/local/share/${file}" "${HOME}/.unison/${file}"
 done
 
-ensureDir ${HOME}/Library/LaunchAgents
+ensureDir "${HOME}/Library/LaunchAgents"
 
 for file in com.theclarkhome.prefsync.plist com.theclarkhome.logoutwatcher.plist
 do
-	[[ -f ${HOME}/Library/LaunchAgents/${file} ]] && rm ${HOME}/Library/LaunchAgents/${file}
-	ln -s /usr/local/share/${file} ${HOME}/Library/LaunchAgents/${file}
-	plutil -lint ${HOME}/Library/LaunchAgents/${file} && {
-		launchctl unload ${HOME}/Library/LaunchAgents/${file}
-                launchctl enable gui/$(id -u)/$(basename file .plist)
-		launchctl load ${HOME}/Library/LaunchAgents/${file}
+	[[ -f "${HOME}/Library/LaunchAgents/${file}" ]] && rm "${HOME}/Library/LaunchAgents/${file}"
+	ln -s "/usr/local/share/${file}" "${HOME}/Library/LaunchAgents/${file}"
+	plutil -lint "${HOME}/Library/LaunchAgents/${file}" && {
+		launchctl unload "${HOME}/Library/LaunchAgents/${file}"
+                launchctl enable gui/$(id -u)/$(basename "${file}" .plist)
+		launchctl load "${HOME}/Library/LaunchAgents/${file}"
 	}
 done
