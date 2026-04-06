83eabfe0b5b0ca8642784c55305243df5dbec1ed
mergwyn <gary@theclarkhome.com>
2026-04-06 17:18:54 +0100

    Update backup.pp

--- site-modules/profile/manifests/app/lxd/backup.pp
+++ site-modules/profile/manifests/app/lxd/backup.pp
@@ -1,20 +1,31 @@
 #
 #
-class profile::app::lxd::backup {
-  include profile::app::git
-  include profile::app::scripts
-  include cron
-
+class profile::app::lxd::backup (
+  Boolean $enable = false,
+) {
   $codedir = '/opt/lxdbackup'
 
+  if $enable {
+    include profile::app::git
+    include profile::app::scripts
+    include cron
+
+    $repo_ensure = 'latest'
+    $cron_ensure = 'present'
+  } else {
+    $repo_ensure = 'absent'
+    $cron_ensure = 'absent'
+  }
+
   vcsrepo { $codedir:
-    ensure   => latest,
+    ensure   => $repo_ensure,
     provider => git,
     source   => 'https://github.com/mergwyn/lxdbackup.git',
     revision => 'master',
   }
 
   cron::job { 'lxdbackup':
+    ensure      => $cron_ensure,
     environment => ['PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"'],
     command     => "${codedir}/lxdbackup",
     user        => 'gary',
