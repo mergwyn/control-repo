ec976fce8a1762017bdfb889345787d568ca6daa
mergwyn <gary@theclarkhome.com>
2026-04-11 15:51:31 +0100

    Rename MinIO secret keys to use underscores
    The secret keys in secrets.eyaml and their references in the MinIO
    profile
    have been renamed from `minio-root`/`minio-root-password` to
    `minio_root`/`minio_root_password` to use underscores consistently.

--- site-modules/profile/manifests/app/minio.pp
+++ site-modules/profile/manifests/app/minio.pp
@@ -1,8 +1,8 @@
 #
 class profile::app::minio (
   String $version = '2024-03-30T09-41-12Z',  # renovate: datasource=github-releases depName=minio/minio
-  String $root_user = "${lookup('secrets::minio-root')}",
-String $root_password = "${lookup('secrets::minio-root-password')}",
+  String $root_user = "${lookup('secrets::minio_root')}",
+String $root_password = "${lookup('secrets::minio_root_password')}",
   Stdlib::Absolutepath $data_dir = '/srv/minio',
   Integer $api_port = 9000,
   Integer $console_port = 9001,
