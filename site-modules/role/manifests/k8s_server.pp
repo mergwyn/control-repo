fd19820024d728d35d37c733e79ca0267678a01b
mergwyn <gary@theclarkhome.com>
2026-04-12 14:13:15 +0100

    Reorder includes in server role manifests for consistency

--- site-modules/role/manifests/k8s_server.pp
+++ site-modules/role/manifests/k8s_server.pp
@@ -2,14 +2,15 @@
 class role::k8s_server {
   include profile::platform::baseline  # All roles should have the base profile
 
-  include profile::app::sssd
   include profile::platform::baseline::debian::zfs
   include profile::platform::baseline::debian::virtual::kubernetes
   include profile::platform::baseline::debian::virtual::lxd
   include profile::platform::baseline::debian::nfs_server
 
-  include profile::app::unison
-  include profile::app::kopia
   include profile::app::k8s_tools
+  include profile::app::kopia
+  include profile::app::lxd::backup
+  include profile::app::sssd
+  include profile::app::unison
   include profile::app::velero
 }
--- site-modules/role/manifests/main_server.pp
+++ site-modules/role/manifests/main_server.pp
@@ -1,31 +1,27 @@
 #
 class role::main_server {
   include profile::platform::baseline
-  include profile::platform::baseline::debian::zfs
+  include profile::platform::baseline::debian::autofs
   include profile::platform::baseline::debian::nfs_server
+  include profile::platform::baseline::debian::virtual::kubernetes
+  include profile::platform::baseline::debian::zfs
+  include profile::platform::baseline::debian::virtual::lxd
 
   include profile::openvox::dev
 
-  include profile::app::sssd
-  #include profile::app::zabbix::agent
-  include profile::platform::baseline::debian::autofs
-  include profile::platform::baseline::debian::virtual::kubernetes
-  include profile::platform::baseline::debian::virtual::lxd
-  include profile::app::lxd::backup
-
-  #include profile::app::backuppc::client
-  #include profile::app::backuppc::server
   include profile::app::gpo::clean
   include profile::app::iptv
-  include profile::app::nginx::webdav
+  include profile::app::k8s_tools
+  include profile::app::kopia
+  include profile::app::lxd::backup
   include profile::app::nginx::phonebook
+  include profile::app::nginx::webdav
   include profile::app::odrive
   include profile::app::photos
   include profile::app::samba::shares
   include profile::app::speedtest
+  include profile::app::sssd
   include profile::app::timemachine
   include profile::app::transcoder
   include profile::app::unison
-  include profile::app::k8s_tools
-  include profile::app::kopia
 }
