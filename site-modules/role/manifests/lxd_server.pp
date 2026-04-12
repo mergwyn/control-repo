113b4dee3a0dd2c4229db1e6581ae767ffc1800a
mergwyn <gary@theclarkhome.com>
2026-04-12 13:57:36 +0100

    Remove commented profiles and reorder includes in server roles, adding lxd::backup where needed.

--- site-modules/role/manifests/lxd_server.pp
+++ site-modules/role/manifests/lxd_server.pp
@@ -2,14 +2,13 @@
 class role::lxd_server {
   include profile::platform::baseline  # All roles should have the base profile
   include profile::platform::baseline::debian::zfs
-  include profile::app::sssd
   include profile::platform::baseline::debian::virtual::kubernetes
   include profile::platform::baseline::debian::virtual::lxd
-  include profile::app::lxd::backup
 
-  #include profile::app::zabbix::agent
-  #include profile::app::backuppc::client
-  include profile::app::unison
   include profile::app::k8s_tools
   include profile::app::kopia
+  include profile::app::sssd
+  include profile::app::unison
+  include profile::app::lxd::backup
+
 }
