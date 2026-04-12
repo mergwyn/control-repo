05c837840bb6ba62bbb9cd10c5b4cdf4b63bf03d
mergwyn <gary@theclarkhome.com>
2026-04-12 14:40:55 +0100

    Fix lint errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -74,4 +74,4 @@ def ensure_module_defined(module_name)
 end
 
 # 'spec_overrides' from sync.yml will appear below this line
-{ 'facts' => { 'facter_version' => '4.5.2' } }
+{"facts"=>{"facter_version"=>"4.5.2"}}
