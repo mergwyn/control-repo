5be64a6c1ea2a98c2c14445fcc2d752e74a6fa68
mergwyn <gary@theclarkhome.com>
2026-04-13 18:16:28 +0100

    Enable MinIO service by default

--- site-modules/profile/manifests/app/minio.pp
+++ site-modules/profile/manifests/app/minio.pp
@@ -73,7 +73,7 @@ String $root_password = "${lookup('secrets::minio_root_password')}",
 
   # Systemd service
   systemd::unit_file { 'minio.service':
-    enable  => false,
+    enable  => true,
     active  => true,
     content => @("SERVICE"/L$),
       [Unit]
