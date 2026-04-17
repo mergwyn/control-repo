5fa545192ff17879952ab378999668ea44f1dbae
mergwyn <gary@theclarkhome.com>
2026-04-17 12:12:50 +0100

    Use tiered ZFS ARC sizes based on RAM
    Replace percentage-based parameters with fixed ARC max sizes
    determined by total system memory. Systems with <10GB RAM get
    1GB, <20GB get 2GB, and 32GB+ get 4GB. ARC min is set to 0.

--- site-modules/profile/manifests/platform/baseline/debian/zfs/kernelopts.pp
+++ site-modules/profile/manifests/platform/baseline/debian/zfs/kernelopts.pp
@@ -1,19 +1,32 @@
 # @summary kernel options (mainly tuning)
 #
-class profile::platform::baseline::debian::zfs::kernelopts (
-  Integer[0,100] $arc_max_percent = 0,
-  Integer[0,100] $arc_min_percent = 0,
-) {
+class profile::platform::baseline::debian::zfs::kernelopts {
+
+  # Calculate ARC max: ~12% of total RAM, but with sensible floors/caps
+  $mem_total_bytes = $facts['memory']['system']['total_bytes']
+
+  $zfs_arc_bytes = $mem_total_bytes ? {
+    # If < 10GB (8GB NUCs), set to 1GB
+    /^[0-9]{1,10}$/ if $mem_total_bytes < 10737418240 => '1073741824',
+
+    # If < 20GB (16GB NUCs), set to 2GB
+    /^[0-9]{1,11}$/ if $mem_total_bytes < 21474836480 => '2147483648',
+
+    # If 32GB or more, set to 4GB
+    default                                          => '4294967296',
+  }
+
+  if $mem_total_bytes >
   kmod::option { 'zfs_arc_max':
     module => 'zfs',
     option => 'zfs_arc_max',
-    value  => Integer($::facts['memory']['system']['total_bytes']*$arc_max_percent/100),
+    value  => $zfs_arc_bytes,
     notify => Exec['update_initramfs_all'],
   }
   kmod::option { 'zfs_arc_min':
     module => 'zfs',
     option => 'zfs_arc_min',
-    value  => Integer($::facts['memory']['system']['total_bytes']*$arc_min_percent/100),
+    value  => 0,
     notify => Exec['update_initramfs_all'],
   }
 #  kmod::option { 'zfs_vdev_scheduler':,
