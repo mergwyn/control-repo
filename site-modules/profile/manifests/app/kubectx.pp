7ebbb2da0354e6bca2025cb93ee3ba870d98f6d7
mergwyn <gary@theclarkhome.com>
2026-05-09 10:18:52 +0100

    Add kubens to kubectx profile

--- site-modules/profile/manifests/app/kubectx.pp
+++ site-modules/profile/manifests/app/kubectx.pp
@@ -1,4 +1,4 @@
-# @summary Install kubectx shell script with version stamping
+# @summary Install kubectx/kubensshell script with version stamping
 #
 class profile::app::kubectx (
   String $version = '013b6bc252ea6bbe7c8372ed64c327ad8a52f003', # renovate: datasource=github-commits depName=ahmetb/kubectx
@@ -10,4 +10,13 @@ class profile::app::kubectx (
     install_dir => '/usr/local/bin',
     stamp       => true,
   }
+
+  profile::app::binary_install { 'kubens':
+    version     => $version,
+    binary      => 'kubens',
+    url         => "https://github.com/ahmetb/kubens/raw/${version}/kubens",
+    install_dir => '/usr/local/bin',
+    stamp       => true,
+  }
+
 }
