cc807648bb75a4d1d38bcf036d7d1999217eb54a
mergwyn <gary@theclarkhome.com>
2026-05-10 17:46:17 +0100

    Move kubens to its own profile class

--- site-modules/profile/manifests/app/kubens.pp
+++ site-modules/profile/manifests/app/kubens.pp
@@ -3,10 +3,10 @@
 class profile::app::kubectx (
   String $version = '013b6bc252ea6bbe7c8372ed64c327ad8a52f003', # renovate: datasource=github-commits depName=ahmetb/kubectx
 ) {
-  profile::app::binary_install { 'kubectx':
+  profile::app::binary_install { 'kubens':
     version     => $version,
-    binary      => 'kubectx',
-    url         => "https://github.com/ahmetb/kubectx/raw/${version}/kubectx",
+    binary      => 'kubens',
+    url         => "https://github.com/ahmetb/kubectx/raw/${version}/kubens",
     install_dir => '/usr/local/bin',
     stamp       => true,
   }
