fac31842a587c1149df46fefbe84664a7d33981e
mergwyn <gary@theclarkhome.com>
2026-05-11 10:09:40 +0100

    Rename kubectx class to kubens

--- site-modules/profile/manifests/app/kubens.pp
+++ site-modules/profile/manifests/app/kubens.pp
@@ -1,6 +1,6 @@
 # @summary Install kubectx/kubensshell script with version stamping
 #
-class profile::app::kubectx (
+class profile::app::kubens (
   String $version = '013b6bc252ea6bbe7c8372ed64c327ad8a52f003', # renovate: datasource=github-commits depName=ahmetb/kubectx
 ) {
   profile::app::binary_install { 'kubens':
