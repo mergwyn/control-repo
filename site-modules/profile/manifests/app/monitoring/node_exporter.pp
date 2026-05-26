f18fcb2bf126bfc4ec696cb4c899ebd3e804791a
mergwyn <gary@theclarkhome.com>
2026-05-26 19:30:14 +0100

    Move node_exporter class to monitoring namespace

--- site-modules/profile/manifests/app/monitoring/node_exporter.pp
+++ site-modules/profile/manifests/app/monitoring/node_exporter.pp
@@ -1,25 +1,26 @@
-# profile/manifests/app/monitoring/node-exporter.pp
-class profile::app::node_exporter (
+# profile/manifests/app/monitoring/nodeexporter.pp
+class profile::app::monitoring::node_exporter (
     Stdlib::Port   $port             = 9100,
-  ) {
-    package { 'prometheus-node-exporter':
-      ensure => installed,
-    }
+) {
+  package { 'prometheus-node-exporter':
+    ensure => installed,
+  }
 
-    file { '/etc/default/prometheus-node-exporter':
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => @("EOT"),
-        # Managed by Puppet - do not edit
-        ARGS="--web.listen-address=:${port}"
-        | EOT
-      notify  => Service['prometheus-node-exporter'],
-    }
+  file { '/etc/default/prometheus-node-exporter':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => @("EOT"),
+      # Managed by Puppet - do not edit
+      ARGS="--web.listen-address=:${port}"
+      | EOT
+    notify  => Service['prometheus-node-exporter'],
+  }
 
-    service { 'prometheus-node-exporter':
-      ensure  => running,
-      enable  => true,
-      require => Package['prometheus-node-exporter'],
-    }  }
+  service { 'prometheus-node-exporter':
+    ensure  => running,
+    enable  => true,
+    require => Package['prometheus-node-exporter'],
+  }
+}
