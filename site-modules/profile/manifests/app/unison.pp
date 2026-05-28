b211d7222ccd63af9e3669dd85db25c19d952aa2
mergwyn <gary@theclarkhome.com>
2026-05-28 16:11:39 +0100

    Set tar_extract to bin/unison for Unison

--- site-modules/profile/manifests/app/unison.pp
+++ site-modules/profile/manifests/app/unison.pp
@@ -54,7 +54,7 @@ class profile::app::unison (
         binary      => 'unison',
         url         => "https://github.com/bcpierce00/unison/releases/download/v${version}/unison-${version}-${os}-${hardware}.tar.gz",
         tarball     => true,
-        tar_extract => 'linux-amd64/unison',
+        tar_extract => 'bin/unison',
         version_cmd => 'unison -version',
       }
 
