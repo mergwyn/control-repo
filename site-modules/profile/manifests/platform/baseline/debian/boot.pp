a18bdd37b29dae5dd4db57699413fb31e56d2d3a
mergwyn <gary@theclarkhome.com>
2026-07-29 21:00:38 +0100

    Add rEFInd support for Debian-family physical nodes (#321)
    * Add rEFInd support for Debian-family physical nodes
    
    - Introduced a new custom fact `has_refind` to check for the presence of
      a rEFInd configuration file.
    - Created a new class `platform::baseline::debian::boot` to manage
      boot-related configurations on Debian-family physical nodes, starting
      with rEFInd.
    - Added a class `platform::baseline::debian::boot::refind` to manage the
      rEFInd boot manager menu timeout, setting a default of 5 seconds and
      providing an option to override via Hiera.
    
    * Add boot profile class for Debian
    
    * Refactor class name to use "profile" prefix

---
+++ site-modules/profile/manifests/platform/baseline/debian/boot.pp
@@ -0,0 +1,9 @@
+# @summary
+#   Entry point for boot-related configuration on Debian-family physical nodes.
+#
+# Includes sub-classes managing boot loader and boot manager configuration.
+# Currently covers rEFInd; intended to be extended with further boot-related
+# baseline config (e.g. ZFSBootMenu specifics) as needed.
+class profile::platform::baseline::debian::boot {
+  include profile::platform::baseline::debian::boot::refind
+}
