349d20b292e5797730c5a063cc606051fa2160c2
mergwyn <gary@theclarkhome.com>
2026-07-31 17:39:52 +0100

    Add workaround for puppet crashing when started as a service

--- site-modules/profile/manifests/platform/baseline/darwin/puppet.pp
+++ site-modules/profile/manifests/platform/baseline/darwin/puppet.pp
@@ -1,6 +1,15 @@
 class profile::platform::baseline::darwin::puppet {
-  service { 'puppet':
-    ensure => running,
-    enable => true,
+  if $facts['os']['family'] == 'Darwin' {
+    # Only load/kickstart the daemon if the agent process isn't running at all
+    exec { 'ensure_puppet_daemon_running':
+      command => '/bin/launchctl bootstrap system /Library/LaunchDaemons/org.voxpupuli.puppet.plist',
+      unless  => '/bin/pgrep -f "puppet agent"',
+    }
+  } else {
+    service { 'puppet':
+      ensure => running,
+      enable => true,
+    }
   }
 }
+
