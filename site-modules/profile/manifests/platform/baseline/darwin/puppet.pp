10ec3918afed0fd875ee82949aeec38d367aa69b
mergwyn <gary@theclarkhome.com>
2026-07-31 17:34:19 +0100

    Add workaround for puppet crashing when started as a service

--- site-modules/profile/manifests/platform/baseline/darwin/puppet.pp
+++ site-modules/profile/manifests/platform/baseline/darwin/puppet.pp
@@ -1,6 +1,9 @@
 class profile::platform::baseline::darwin::puppet {
   service { 'puppet':
-    ensure => running,
-    enable => true,
+    ensure    => running,
+    enable    => true,
+    hasstatus => false, # Bypass launchctl status check bug on macOS
+    pattern   => 'puppet agent',
   }
 }
+
