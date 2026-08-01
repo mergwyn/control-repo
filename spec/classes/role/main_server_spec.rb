c44282f57c5cd5d3ff0a28e09fa7cea13a121628
mergwyn <gary@theclarkhome.com>
2026-08-01 17:44:32 +0100

    Add facts for reFind and ZFSBootMenu to main server role spec
    Merge additional facts into the `facts` hash for more comprehensive
    testing of the `main_server` role.

--- spec/classes/role/main_server_spec.rb
+++ spec/classes/role/main_server_spec.rb
@@ -9,7 +9,12 @@ describe 'role::main_server' do
       # Puppet::Util::Log.level = :debug
       # Puppet::Util::Log.newdestination(:console)
 
-      let(:facts) { os_facts }
+      let(:facts) do
+        os_facts.merge({
+          has_refind: true,
+          has_zfsbootmenu: true
+        })
+      end
       let(:trusted_facts) { { 'pp_role' => 'main_server' } }
       let(:node) { 'unittest.theclarkhome.com' }
       let(:pre_condition) do
