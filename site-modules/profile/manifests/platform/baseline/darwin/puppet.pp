611f9a1564e939971aacd4103dea0403de056232
mergwyn <gary@theclarkhome.com>
2026-08-11 16:07:48 +0100

    Remove conditional block for Darwin Puppet service configuration Move service definition to use 'org.voxpupuli.puppet' instead of 'puppet'

--- site-modules/profile/manifests/platform/baseline/darwin/puppet.pp
+++ site-modules/profile/manifests/platform/baseline/darwin/puppet.pp
@@ -10,51 +10,50 @@
 #   include profile::platform::baseline::darwin::puppet
 #
 class profile::platform::baseline::darwin::puppet {
-  if $facts['os']['family'] == 'Darwin' {
-    file { '/Library/LaunchDaemons/org.voxpupuli.puppet.plist':
-      ensure  => file,
-      owner   => 'root',
-      group   => 'wheel',
-      mode    => '0644',
-      content => @("PLIST"/L),
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-          "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
+  file { '/Library/LaunchDaemons/org.voxpupuli.puppet.plist':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'wheel',
+    mode    => '0644',
+    content => @("PLIST"/L),
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>org.voxpupuli.puppet</string>
+
+        <key>ProgramArguments</key>
+        <array>
+          <string>/opt/puppetlabs/bin/puppet</string>
+          <string>agent</string>
+          <string>--verbose</string>
+          <string>--no-daemonize</string>
+          <string>--logdest</string>
+          <string>console</string>
+        </array>
+
+        <key>RunAtLoad</key>
+        <true/>
+
+        <key>KeepAlive</key>
+        <true/>
+
+        <key>StandardOutPath</key>
+        <string>/var/log/puppetlabs/puppet/puppet.log</string>
+
+        <key>StandardErrorPath</key>
+        <string>/var/log/puppetlabs/puppet/puppet.log</string>
+
+        <key>EnvironmentVariables</key>
         <dict>
-          <key>Label</key>
-          <string>org.voxpupuli.puppet</string>
-
-          <key>ProgramArguments</key>
-          <array>
-            <string>/opt/puppetlabs/bin/puppet</string>
-            <string>agent</string>
-            <string>--verbose</string>
-            <string>--no-daemonize</string>
-            <string>--logdest</string>
-            <string>console</string>
-          </array>
-
-          <key>RunAtLoad</key>
-          <true/>
-
-          <key>KeepAlive</key>
-          <true/>
-
-          <key>StandardOutPath</key>
-          <string>/var/log/puppetlabs/puppet/puppet.log</string>
-
-          <key>StandardErrorPath</key>
-          <string>/var/log/puppetlabs/puppet/puppet.log</string>
-
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>PATH</key>
-            <string>/opt/puppetlabs/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-          </dict>
+          <key>PATH</key>
+          <string>/opt/puppetlabs/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
         </dict>
-        </plist>
-        PLIST
+      </dict>
+      </plist>
+      PLIST
     }
 
     exec { 'bootstrap puppet launchd':
@@ -63,17 +62,10 @@ class profile::platform::baseline::darwin::puppet {
       require => File['/Library/LaunchDaemons/org.voxpupuli.puppet.plist'],
     }
 
-    service { 'puppet':
+    service { 'org.voxpupuli.puppet':
       ensure   => running,
       enable   => true,
       provider => launchd,
       require  => Exec['bootstrap puppet launchd'],
     }
-  } else {
-    service { 'puppet':
-      ensure  => running,
-      enable  => true,
-      pattern => 'puppet agent',
-    }
-  }
 }
