4238f2f08b02ceed3cb41ea2f2d7454c93eb18ab
mergwyn <gary@theclarkhome.com>
2026-08-17 13:56:48 +0100

    Create directory structure for node_exporter textfile collector
    Add required directories for textfile collector
    Update service require to include textfile collector directory
    Fix owner and group for directories to 'prometheus'
    Update prometheus-node-exporter config to include textfile directory
    path

--- site-modules/profile/manifests/app/monitoring/node_exporter.pp
+++ site-modules/profile/manifests/app/monitoring/node_exporter.pp
@@ -13,6 +13,21 @@ class profile::app::monitoring::node_exporter (
     ensure => installed,
   }
 
+  file { '/var/lib/node_exporter':
+    ensure => directory,
+    owner  => 'prometheus',
+    group  => 'prometheus',
+    mode   => '0755',
+  }
+
+  file { '/var/lib/node_exporter/textfile_collector':
+    ensure  => directory,
+    owner   => 'prometheus',
+    group   => 'prometheus',
+    mode    => '0755',
+    require => File['/var/lib/node_exporter'],
+  }
+
   file { '/etc/default/prometheus-node-exporter':
     ensure  => file,
     owner   => 'root',
@@ -20,25 +35,17 @@ class profile::app::monitoring::node_exporter (
     mode    => '0644',
     content => @("EOT"),
       # Managed by Puppet - do not edit
-      ARGS="--web.listen-address=:${port}"
+      ARGS="--web.listen-address=:${port} --collector.textfile.directory=/var/lib/node_exporter/textfile_collector"
       | EOT
     notify  => Service['prometheus-node-exporter'],
-  }
-
-  file {
-    [
-      '/var/lib/node_exporter',
-      '/var/lib/node_exporter/textfile_collector',
-    ]:
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
 
   service { 'prometheus-node-exporter':
     ensure  => running,
     enable  => true,
-    require => Package['prometheus-node-exporter'],
+    require => [
+      Package['prometheus-node-exporter'],
+      File['/var/lib/node_exporter/textfile_collector'],
+    ],
   }
 }
