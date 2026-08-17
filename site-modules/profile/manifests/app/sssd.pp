5255a3159662b68808e7c353c720ccbe9bee7122
mergwyn <gary@theclarkhome.com>
2026-08-17 16:43:19 +0100

    Enable PAC support in SSSD by adding 'pac' to services and configuring PAC.

--- site-modules/profile/manifests/app/sssd.pp
+++ site-modules/profile/manifests/app/sssd.pp
@@ -11,7 +11,7 @@ class profile::app::sssd {
   class { 'sssd':
     main_config  => {
       'sssd'                    => {
-#        'services'            => ['nss', 'pam'],
+        'services'            => ['nss', 'pam', pac],
         'config_file_version' => 2,
         'domains'             => $trusted['domain'],
       },
@@ -38,6 +38,7 @@ class profile::app::sssd {
 # TODO workaorund https://bugs.launchpad.net/ubuntu/+source/sssd/+bug/1934997
         'ad_gpo_access_control'          => 'permissive',
       },
+      'pac'                     => {},
     },
     #require => Service[ 'SambaWinBind' ],
   }
